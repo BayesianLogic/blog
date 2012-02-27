df258f387298e7aee310af4736af3178ae534e5e
leili <leili@e5a44606-1236-43ac-9aa5-cdde1b33ff33>
2012-02-27 07:19:15 +0000

    git-svn-id: https://patmos.banatao.berkeley.edu:8889/svnbase/outbids/code/trunk@3 e5a44606-1236-43ac-9aa5-cdde1b33ff33

---
+++ src/test/TestMultivarGaussian.java
@@ -0,0 +1,28 @@
+package test;
+
+import blog.distrib.MultivarGaussian;
+import common.Util;
+import Jama.Matrix;
+
+/**
+ * a small program to generate samples from Multivariate Gaussian Distribution.
+ * @author leili
+ *
+ */
+
+public class TestMultivarGaussian {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Util.initRandom(true);
+		Matrix zero = new Matrix(5, 1);
+		Matrix unit = Matrix.identity(5,5);
+
+		MultivarGaussian mvg = new MultivarGaussian(zero, unit);
+
+		Matrix b = mvg.sampleVal();
+		System.out.println(b.toString());
+	}
+}
