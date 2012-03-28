1a80ed3ca2e35d0fc5a70a81c7cb46f1297f3381
leili <leili@e5a44606-1236-43ac-9aa5-cdde1b33ff33>
2012-03-28 23:34:12 +0000

    added agileJ class diagram
    git-svn-id: https://patmos.banatao.berkeley.edu:8889/svnbase/outbids/code/trunk@60 e5a44606-1236-43ac-9aa5-cdde1b33ff33

---
+++ src/test/blog/distrib/PoissonTest.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package test.blog.distrib;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import blog.distrib.Poisson;
+
+/**
+ * @author leili
+ *
+ */
+public class PoissonTest {
+
+	/**
+	 * Test method for {@link blog.distrib.Poisson#getLogProb(java.util.List, java.lang.Object)}.
+	 */
+	@Test
+	public void testGetLogProb() {
+		
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link blog.distrib.Poisson#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link blog.distrib.Poisson#getProb(java.util.List, java.lang.Object)}.
+	 */
+	@Test
+	public void testGetProb() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link blog.distrib.Poisson#sampleVal(java.util.List, blog.Type)}.
+	 */
+	@Test
+	public void testSampleVal() {
+		fail("Not yet implemented");
+	}
+
+}
