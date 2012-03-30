38c95a412c291d09c16970b9065d2a55e979dfc3
leili <leili@e5a44606-1236-43ac-9aa5-cdde1b33ff33>
2012-03-30 01:15:48 +0000

    added blog.sampling.modular
    git-svn-id: https://patmos.banatao.berkeley.edu:8889/svnbase/outbids/code/trunk@64 e5a44606-1236-43ac-9aa5-cdde1b33ff33

---
+++ src/blog/sampling/modular/BlockInstantiatingEvalContextImpl.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package blog.sampling.modular;
+
+import blog.ClassicInstantiatingEvalContext;
+import blog.PartialWorld;
+
+/**
+ * @author leili
+ *
+ */
+public class BlockInstantiatingEvalContextImpl extends
+		ClassicInstantiatingEvalContext implements BlockInstantiatingEvalContext {
+
+	/**
+	 * @param world
+	 */
+	public BlockInstantiatingEvalContextImpl(PartialWorld world) {
+		super(world);
+		// TODO Auto-generated constructor stub
+	}
+
+}
