dd41c39661b4c182482c6355d3ad0b7cd9d9abaf
lileicc <lileicc@gmail.com>
2012-04-10 07:02:57 +0000

    added list construction expression
    git-svn-id: https://patmos.banatao.berkeley.edu:8889/svnbase/outbids/code/trunk@90 e5a44606-1236-43ac-9aa5-cdde1b33ff33

--- src/blog/absyn/NilExp.java
+++
@@ -1,5 +0,0 @@
-package blog.absyn;
-import Symbol.Symbol;
-public class NilExp extends Stmt {
-  public NilExp(int p) {pos=p;}
-}
--- src/blog/absyn/RecordTy.java
+++
@@ -1,6 +0,0 @@
-package blog.absyn;
-import Symbol.Symbol;
-public class RecordTy extends Ty {
-   public FieldList fields;
-   public RecordTy(int p, FieldList f) {pos=p; fields=f;}
-}   
