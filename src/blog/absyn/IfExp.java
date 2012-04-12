d8eb9611d0b60852afde9c608d70ff9497374230
lileicc <lileicc@gmail.com>
2012-04-12 04:34:42 +0000

    git-svn-id: https://patmos.banatao.berkeley.edu:8889/svnbase/outbids/code/trunk@96 e5a44606-1236-43ac-9aa5-cdde1b33ff33

--- src/blog/absyn/IfExp.java
+++
@@ -1,9 +0,0 @@
-package blog.absyn;
-import blog.symbol.Symbol;
-public class IfExp extends Expr {
-   public Stmt test;
-   public Stmt thenclause;
-   public Stmt elseclause; /* optional */
-   public IfExp(int p, Stmt x, Stmt y) {pos=p; test=x; thenclause=y; elseclause=null;}
-   public IfExp(int p, Stmt x, Stmt y, Stmt z) {pos=p; test=x; thenclause=y; elseclause=z;}
-}
