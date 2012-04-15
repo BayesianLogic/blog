1942a9bb5f9e46fbedf5112bec8049750e2b19f2
lileicc <lileicc@gmail.com>
2012-04-15 11:20:26 +0000

    git-svn-id: https://patmos.banatao.berkeley.edu:8889/svnbase/outbids/code/trunk@107 e5a44606-1236-43ac-9aa5-cdde1b33ff33

---
+++ src/test/blog/parse/TestParse.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package test.blog.parse;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author leili
+ * @date Apr 15, 2012
+ * 
+ */
+public class TestParse {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		// TO-DO
+	}
+
+}
