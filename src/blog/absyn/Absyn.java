5132905dc656a625930168fab0ea05ebee037d27
leili <leili@e5a44606-1236-43ac-9aa5-cdde1b33ff33>
2012-04-18 23:04:57 +0000

    add ErrorMsg
    git-svn-id: https://patmos.banatao.berkeley.edu:8889/svnbase/outbids/code/trunk@117 e5a44606-1236-43ac-9aa5-cdde1b33ff33

--- src/blog/absyn/Absyn.java
+++ src/blog/absyn/Absyn.java
@@ -1,7 +1,14 @@
 package blog.absyn;
 
 abstract public class Absyn {
-	public int pos;
+	protected int pos;
+	
+	/**
+	 * @return position of this abstract syntax tree in the input file
+	 */
+	public int getPos() {
+		return pos;
+	}
 
 	abstract void printTree(Printer pr, int d);
 }
