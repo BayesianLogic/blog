8f5ecbe49a06ee99746ece86958e8e568ee55c10
lileicc <lileicc@gmail.com>
2012-04-18 23:04:57 +0000

    add ErrorMsg
    git-svn-id: https://patmos.banatao.berkeley.edu:8889/svnbase/outbids/code/trunk@117 e5a44606-1236-43ac-9aa5-cdde1b33ff33

--- src/blog/semant/Semant.java
+++ src/blog/semant/Semant.java
@@ -3,12 +3,52 @@
  */
 package blog.semant;
 
+import blog.absyn.Dec;
+import blog.absyn.Expr;
+import blog.absyn.TypeDec;
+import blog.model.*;
+
+
 /**
  * @author leili
  *
  */
 public class Semant {
 	
+	private Model model;
+	private Evidence evidence;
 	
-
+	public Semant() {
+		model = new Model();
+		evidence = new Evidence();
+	}
+	
+	public Semant(Model m, Evidence e) {
+		model = m;
+		evidence = e;
+	}
+	
+	void transStmt(blog.absyn.Stmt e) {
+		if (e instanceof Dec) {
+			transDec((Dec) e);
+		}
+	}
+	
+	void transDec(Dec e) {
+		if (e instanceof TypeDec) {
+			transDec((TypeDec) e);
+		}
+	}
+	
+	void transDec(TypeDec e) {
+		String name = e.name.toString();
+		if (model.getType(name) != null) 
+		model.addType(name);
+	}
+	
+	void transExpr(Expr e) {
+		
+	}
+	
+	
 }
