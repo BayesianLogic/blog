23ee2894a5b68f5d6f73f1370d7581334585e46e
lileicc <lileicc@gmail.com>
2012-04-19 04:19:57 +0000

    git-svn-id: https://patmos.banatao.berkeley.edu:8889/svnbase/outbids/code/trunk@125 e5a44606-1236-43ac-9aa5-cdde1b33ff33

--- src/blog/semant/Semant.java
+++ src/blog/semant/Semant.java
@@ -4,7 +4,13 @@
 package blog.semant;
 
 import blog.absyn.Dec;
+import blog.absyn.DistinctSymbolDec;
+import blog.absyn.DistributionDec;
 import blog.absyn.Expr;
+import blog.absyn.FixedFuncDec;
+import blog.absyn.NameTy;
+import blog.absyn.SymbolArrayList;
+import blog.absyn.Ty;
 import blog.absyn.TypeDec;
 import blog.model.*;
 import blog.msg.ErrorMsg;
@@ -39,9 +45,26 @@ public class Semant {
 	void transDec(Dec e) {
 		if (e instanceof TypeDec) {
 			transDec((TypeDec) e);
+		} else if (e instanceof DistinctSymbolDec) {
+			transDec((DistinctSymbolDec) e);
+		} else if (e instanceof DistributionDec) {
+			//TO-DO
+		} else if (e instanceof FixedFuncDec) {
+			transDec((FixedFuncDec) e);
 		}
 	}
 	
+	void transDec(FixedFuncDec e) {
+		Type type = getType(e.result);
+		
+	}
+	
+	
+	
+	/**
+	 * add the declared type to model
+	 * @param e
+	 */
 	void transDec(TypeDec e) {
 		String name = e.name.toString();
 		if (model.getType(name) != null) {
@@ -51,9 +74,60 @@ public class Semant {
 		}
 	}
 	
+	
+	/**
+	 * translate the Distinct symbol declaration to internal model representation
+	 * @param e
+	 */
+	void transDec(DistinctSymbolDec e) {
+			Type type = getNameType(e.type);			
+			for (SymbolArrayList sa=e.symbols; sa != null; sa = sa.next) {
+				if (sa.head == null) {
+					error(sa.head.line, sa.head.col, "Symbol mistake!");
+				} else {
+					int sz = sa.head.size;
+					String name = sa.head.name.toString();
+					if (sz == 1) {
+						model.addEnumeratedObject(name, type);
+					} else {
+						for (int i=1; i <= sz; i++) {
+							model.addEnumeratedObject(name + i, type);
+						}
+					}
+				}
+			}
+	}
+	
+	Type getNameType(Ty type) {
+		Type ty = null;
+		if (type instanceof NameTy) {
+		String name = ((NameTy) type).name.toString();
+		ty = model.getType(name);
+		if (ty == null) {
+			error(type.line, type.col, "Type " + name +  " undefined!");
+		}
+		}
+		else {
+			error(type.line, type.col, "Type not allowed!");			
+		}
+		return ty;
+	}
+	
+	Type getType(Ty type) {
+//	TO-DO
+		return null;
+	}
+	
+	
+	
+	
+	
+	
 	void transExpr(Expr e) {
 		
 	}
+	
+	
 	
 	void error(int line, int col, String msg) {
 		errorMsg.error(line, col, msg);
