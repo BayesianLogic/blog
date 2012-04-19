a1e68092f9528d60e7d32e63d08df15b916d93f7
leili <leili@e5a44606-1236-43ac-9aa5-cdde1b33ff33>
2012-04-19 00:51:31 +0000

    add Parse class
    git-svn-id: https://patmos.banatao.berkeley.edu:8889/svnbase/outbids/code/trunk@121 e5a44606-1236-43ac-9aa5-cdde1b33ff33

---
+++ src/blog/parse/Parse.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package blog.parse;
+
+import blog.absyn.Absyn;
+
+/**
+ * @author leili
+ * @since Apr 18, 2012
+ * 
+ */
+public class Parse {
+
+	protected blog.msg.ErrorMsg errorMsg;
+	protected Absyn absyn;
+
+	public Parse(String filename) {
+		errorMsg = new blog.msg.ErrorMsg(filename);
+
+		java.io.InputStream inp;
+		BLOGParser parser;
+		try {
+			inp = new java.io.FileInputStream(filename);
+		} catch (java.io.FileNotFoundException e) {
+			throw new Error("File not found: " + filename);
+		}
+		try {
+			parser = new BLOGParser(new BLOGLexer(inp, errorMsg), errorMsg);
+			/* open input files, etc. here */
+
+			parser. /* debug_ */parse();
+			absyn = parser.parseResult;
+		} catch (Throwable e) {
+			e.printStackTrace();
+			throw new Error(e.toString());
+		} finally {
+			try {
+				inp.close();
+			} catch (java.io.IOException e) {
+			}
+		}
+
+	}
+	
+	public Absyn getResult() {
+		return absyn;
+	}
+	
+}
