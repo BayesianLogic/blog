5d7e2ce7a3770b7329d12d94caea9675f8aceecf
lileicc <lileicc@gmail.com>
2012-04-21 18:29:36 +0000

    rename blog.absyn.Evidence to blog.absyn.EvidenceStmt rename blog.absyn.Query to blog.absyn.QueryStmt
    git-svn-id: https://patmos.banatao.berkeley.edu:8889/svnbase/outbids/code/trunk@146 e5a44606-1236-43ac-9aa5-cdde1b33ff33

--- src/blog/absyn/QueryStmt.java
+++ src/blog/absyn/QueryStmt.java
@@ -1,9 +1,9 @@
 package blog.absyn;
 
-public class Query extends Stmt {
+public class QueryStmt extends Stmt {
 	public Expr query;
 
-	public Query(int p, Expr q) {
+	public QueryStmt(int p, Expr q) {
 		pos = p;
 		query = q;
 	}
