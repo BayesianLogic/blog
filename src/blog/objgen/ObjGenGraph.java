de64c3b8324bdb74f288fd6a50e663ed4553cdd1
leili <leili@e5a44606-1236-43ac-9aa5-cdde1b33ff33>
2012-04-23 21:16:18 +0000

    move ObjGenGraph$SatisfierIterator to a separate file
    git-svn-id: https://patmos.banatao.berkeley.edu:8889/svnbase/outbids/code/trunk@167 e5a44606-1236-43ac-9aa5-cdde1b33ff33

--- src/blog/objgen/ObjGenGraph.java
+++ src/blog/objgen/ObjGenGraph.java
@@ -37,7 +37,6 @@ package blog.objgen;
 
 import java.util.*;
 
-import blog.AbstractObjectIterator;
 import blog.ObjectIterator;
 import blog.common.AbstractDGraph;
 import blog.common.DGraph;
@@ -170,7 +169,7 @@ public class ObjGenGraph extends AbstractDGraph {
 	 */
 	public ObjectIterator iterator(EvalContext context,
 			Set externallyDistinguished, boolean returnPOPApps) {
-		return new SatisfierIterator(context, externallyDistinguished,
+		return new SatisfierIterator(this, context, externallyDistinguished,
 				returnPOPApps);
 	}
 
@@ -330,7 +329,7 @@ public class ObjGenGraph extends AbstractDGraph {
 			OriginFunction[] popGenFuncs = pop.originFuncs();
 			if (Arrays.asList(popGenFuncs).containsAll(nonNullGenFuncs)) {
 				// this POP uses all the non-null gen funcs
-				Node popNode = getPOPNode(pop, subject, originFuncConstraints);
+				Node popNode = createPOPNode(pop, subject, originFuncConstraints);
 
 				// popNode could be null, e.g., if there is a
 				// constraint saying one of its gen funcs is null
@@ -343,7 +342,7 @@ public class ObjGenGraph extends AbstractDGraph {
 		return typeNode;
 	}
 
-	private Node getPOPNode(POP pop, Term subject, List constraints) {
+	private Node createPOPNode(POP pop, Term subject, List constraints) {
 		List parents = new ArrayList();
 		for (int i = 0; i < pop.originFuncs().length; ++i) {
 			OriginFunction g = pop.originFuncs()[i];
@@ -491,170 +490,10 @@ public class ObjGenGraph extends AbstractDGraph {
 		return false;
 	}
 
-	private class SatisfierIterator extends AbstractObjectIterator {
-		SatisfierIterator(EvalContext context, Set externallyDistinguished,
-				boolean returnPOPApps) {
-			this.context = context;
-			this.externallyDistinguished = externallyDistinguished;
-			this.returnPOPAppsForTarget = returnPOPApps;
-
-			// For each POPNode, add its parents to the POP parent maps,
-			// associated with empty lists
-			for (Iterator nodeIter = nodes.iterator(); nodeIter.hasNext();) {
-				Node node = (Node) nodeIter.next();
-				if (node instanceof POPNode) {
-					for (Iterator parentIter = node.getParents().iterator(); parentIter
-							.hasNext();) {
-						Node parent = (Node) parentIter.next();
-						popParentCurRoundObjs.put(parent, new ArrayList());
-						popParentPrevRoundObjs.put(parent, new ArrayList());
-						popParentEarlierRoundObjs.put(parent, new ArrayList());
-					}
-				}
-			}
-
-			targetIter = getNodeIterator(targetNode, returnPOPAppsForTarget);
-		}
-
-		protected int skipAfterNext() {
-			return targetIter.skipIndistinguishable();
-		}
-
-		protected Object findNext() {
-			// If the target iterator has no more elements, we need to
-			// finish this round and start a new one. Do this
-			// repeatedly until the target node iterator has a next
-			// element, or until we complete a round where no objects
-			// are added for any POP parents.
-			while (!targetIter.hasNext()) {
-				if (!targetIter.canDetermineNext()) {
-					canDetermineNext = false;
-					// System.out.println("Iter for target node " + targetNode
-					// + " can't determine next.");
-					return null;
-				}
-
-				// Process all the POP parents in this round -- except
-				// the target node, whose curRoundObjs were collected over
-				// the course of this round.
-				//
-				// Note that within a round, the order in which nodes
-				// are processed doesn't matter.
-				for (Iterator iter = popParentCurRoundObjs.keySet().iterator(); iter
-						.hasNext();) {
-					Node popParent = (Node) iter.next();
-					if (popParent != targetNode) {
-						if (!processPOPParent(popParent)) {
-							return null;
-						}
-					}
-				}
-
-				// Try to start new round.
-				if (!startNewRound()) {
-					return null;
-				}
-
-				// Don't include guaranteed objects in new iterators after
-				// the first round
-				includeGuaranteed = false;
-
-				// We will process the target node first in the new round
-				targetIter = getNodeIterator(targetNode, returnPOPAppsForTarget);
-			}
-
-			// Continue processing the target node in the current round
-			Object newObj = targetIter.next();
-			if (popParentCurRoundObjs.containsKey(targetNode)) {
-				((List) popParentCurRoundObjs.get(targetNode)).add(newObj);
-			}
-			return newObj;
-		}
-
-		private boolean startNewRound() {
-			// TODO: limit ourselves to parents of POPs that are still
-			// active, i.e., POPs that don't have a permanently empty
-			// parent. A node is permanently empty if it has no
-			// objects and none of its ancestors generated any objects
-			// in the current round.
-
-			boolean haveNewObjs = false;
-			for (Iterator iter = popParentCurRoundObjs.entrySet().iterator(); iter
-					.hasNext();) {
-				Map.Entry entry = (Map.Entry) iter.next();
-				Node popParent = (Node) entry.getKey();
-				List curRoundObjs = (List) entry.getValue();
-				if (!curRoundObjs.isEmpty()) {
-					haveNewObjs = true;
-				}
-
-				List prevRoundObjs = (List) popParentPrevRoundObjs.get(popParent);
-				List earlierRoundObjs = (List) popParentEarlierRoundObjs.get(popParent);
-				earlierRoundObjs.addAll(prevRoundObjs);
-
-				prevRoundObjs.clear();
-				prevRoundObjs.addAll(curRoundObjs);
-				curRoundObjs.clear();
-			}
-
-			return haveNewObjs;
-		}
-
-		private boolean processPOPParent(Node node) {
-			List curRoundObjs = (List) popParentCurRoundObjs.get(node);
-			ObjectIterator iter = getNodeIterator(node, false);
-			if (node.isFinite()) {
-				// Get all objects from iterator
-				while (iter.hasNext()) {
-					curRoundObjs.add(iter.next());
-				}
-			} else {
-				// Get only one object from iterator
-				if (iter.hasNext()) {
-					curRoundObjs.add(iter.next());
-				}
-			}
-
-			if (!iter.canDetermineNext()) {
-				canDetermineNext = false;
-				// System.out.println("POP parent node " + node
-				// + " can't determine next.");
-				return false;
-			}
-			return true;
-		}
-
-		private ObjectIterator getNodeIterator(Node node, boolean returnPOPApps) {
-			if (rootIters.containsKey(node)) {
-				return (ObjectIterator) rootIters.get(node);
-			}
-
-			ObjectIterator iter = node.iterator(context, externallyDistinguished,
-					returnPOPApps, popParentPrevRoundObjs, popParentEarlierRoundObjs,
-					includeGuaranteed);
-			if (node.getParents().isEmpty()) {
-				rootIters.put(node, iter);
-			}
-			return iter;
-		}
-
-		EvalContext context;
-		Set externallyDistinguished;
-		boolean returnPOPAppsForTarget;
-
-		boolean includeGuaranteed = true;
-		Map popParentCurRoundObjs = new HashMap(); // from Node to List
-		Map popParentPrevRoundObjs = new HashMap(); // from Node to List
-		Map popParentEarlierRoundObjs = new HashMap(); // from Node to List
-		Map rootIters = new HashMap(); // from Node to ObjectIterator
-
-		ObjectIterator targetIter;
-	}
-
-	private Set nodes = new HashSet(); // of Node
+	Set nodes = new HashSet(); // of Node
 	private Map unconstrainedNodes = new HashMap(); // from Type to Node
 
-	private Node targetNode;
+	Node targetNode;
 
 	private LogicalVar var;
 	private Set freeVars;
---
+++ src/blog/objgen/SatisfierIterator.java
@@ -0,0 +1,181 @@
+/**
+ * 
+ */
+package blog.objgen;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import blog.AbstractObjectIterator;
+import blog.ObjectIterator;
+import blog.sample.EvalContext;
+
+class SatisfierIterator extends AbstractObjectIterator {
+	/**
+	 * 
+	 */
+	private final ObjGenGraph objGenGraph;
+
+	SatisfierIterator(ObjGenGraph objGenGraph, EvalContext context, Set externallyDistinguished,
+			boolean returnPOPApps) {
+		this.objGenGraph = objGenGraph;
+		this.context = context;
+		this.externallyDistinguished = externallyDistinguished;
+		this.returnPOPAppsForTarget = returnPOPApps;
+
+		// For each POPNode, add its parents to the POP parent maps,
+		// associated with empty lists
+		for (Iterator nodeIter = this.objGenGraph.nodes.iterator(); nodeIter.hasNext();) {
+			Node node = (Node) nodeIter.next();
+			if (node instanceof POPNode) {
+				for (Iterator parentIter = node.getParents().iterator(); parentIter
+						.hasNext();) {
+					Node parent = (Node) parentIter.next();
+					popParentCurRoundObjs.put(parent, new ArrayList());
+					popParentPrevRoundObjs.put(parent, new ArrayList());
+					popParentEarlierRoundObjs.put(parent, new ArrayList());
+				}
+			}
+		}
+
+		targetIter = getNodeIterator(this.objGenGraph.targetNode, returnPOPAppsForTarget);
+	}
+
+	protected int skipAfterNext() {
+		return targetIter.skipIndistinguishable();
+	}
+
+	protected Object findNext() {
+		// If the target iterator has no more elements, we need to
+		// finish this round and start a new one. Do this
+		// repeatedly until the target node iterator has a next
+		// element, or until we complete a round where no objects
+		// are added for any POP parents.
+		while (!targetIter.hasNext()) {
+			if (!targetIter.canDetermineNext()) {
+				canDetermineNext = false;
+				// System.out.println("Iter for target node " + targetNode
+				// + " can't determine next.");
+				return null;
+			}
+
+			// Process all the POP parents in this round -- except
+			// the target node, whose curRoundObjs were collected over
+			// the course of this round.
+			//
+			// Note that within a round, the order in which nodes
+			// are processed doesn't matter.
+			for (Iterator iter = popParentCurRoundObjs.keySet().iterator(); iter
+					.hasNext();) {
+				Node popParent = (Node) iter.next();
+				if (popParent != this.objGenGraph.targetNode) {
+					if (!processPOPParent(popParent)) {
+						return null;
+					}
+				}
+			}
+
+			// Try to start new round.
+			if (!startNewRound()) {
+				return null;
+			}
+
+			// Don't include guaranteed objects in new iterators after
+			// the first round
+			includeGuaranteed = false;
+
+			// We will process the target node first in the new round
+			targetIter = getNodeIterator(this.objGenGraph.targetNode, returnPOPAppsForTarget);
+		}
+
+		// Continue processing the target node in the current round
+		Object newObj = targetIter.next();
+		if (popParentCurRoundObjs.containsKey(this.objGenGraph.targetNode)) {
+			((List) popParentCurRoundObjs.get(this.objGenGraph.targetNode)).add(newObj);
+		}
+		return newObj;
+	}
+
+	private boolean startNewRound() {
+		// TODO: limit ourselves to parents of POPs that are still
+		// active, i.e., POPs that don't have a permanently empty
+		// parent. A node is permanently empty if it has no
+		// objects and none of its ancestors generated any objects
+		// in the current round.
+
+		boolean haveNewObjs = false;
+		for (Iterator iter = popParentCurRoundObjs.entrySet().iterator(); iter
+				.hasNext();) {
+			Map.Entry entry = (Map.Entry) iter.next();
+			Node popParent = (Node) entry.getKey();
+			List curRoundObjs = (List) entry.getValue();
+			if (!curRoundObjs.isEmpty()) {
+				haveNewObjs = true;
+			}
+
+			List prevRoundObjs = (List) popParentPrevRoundObjs.get(popParent);
+			List earlierRoundObjs = (List) popParentEarlierRoundObjs.get(popParent);
+			earlierRoundObjs.addAll(prevRoundObjs);
+
+			prevRoundObjs.clear();
+			prevRoundObjs.addAll(curRoundObjs);
+			curRoundObjs.clear();
+		}
+
+		return haveNewObjs;
+	}
+
+	private boolean processPOPParent(Node node) {
+		List curRoundObjs = (List) popParentCurRoundObjs.get(node);
+		ObjectIterator iter = getNodeIterator(node, false);
+		if (node.isFinite()) {
+			// Get all objects from iterator
+			while (iter.hasNext()) {
+				curRoundObjs.add(iter.next());
+			}
+		} else {
+			// Get only one object from iterator
+			if (iter.hasNext()) {
+				curRoundObjs.add(iter.next());
+			}
+		}
+
+		if (!iter.canDetermineNext()) {
+			canDetermineNext = false;
+			// System.out.println("POP parent node " + node
+			// + " can't determine next.");
+			return false;
+		}
+		return true;
+	}
+
+	private ObjectIterator getNodeIterator(Node node, boolean returnPOPApps) {
+		if (rootIters.containsKey(node)) {
+			return (ObjectIterator) rootIters.get(node);
+		}
+
+		ObjectIterator iter = node.iterator(context, externallyDistinguished,
+				returnPOPApps, popParentPrevRoundObjs, popParentEarlierRoundObjs,
+				includeGuaranteed);
+		if (node.getParents().isEmpty()) {
+			rootIters.put(node, iter);
+		}
+		return iter;
+	}
+
+	EvalContext context;
+	Set externallyDistinguished;
+	boolean returnPOPAppsForTarget;
+
+	boolean includeGuaranteed = true;
+	Map popParentCurRoundObjs = new HashMap(); // from Node to List
+	Map popParentPrevRoundObjs = new HashMap(); // from Node to List
+	Map popParentEarlierRoundObjs = new HashMap(); // from Node to List
+	Map rootIters = new HashMap(); // from Node to ObjectIterator
+
+	ObjectIterator targetIter;
+}
