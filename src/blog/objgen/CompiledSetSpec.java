0199025379234bf4b90ab7bb134c3b1c410437e4
lileicc <lileicc@gmail.com>
2012-04-23 18:53:43 +0000

    extract private classes (ElementSet & UnfilteredIterator) of CompiledSetSpec to be in package of blog.objgen
    git-svn-id: https://patmos.banatao.berkeley.edu:8889/svnbase/outbids/code/trunk@166 e5a44606-1236-43ac-9aa5-cdde1b33ff33

--- src/blog/objgen/CompiledSetSpec.java
+++ src/blog/objgen/CompiledSetSpec.java
@@ -35,19 +35,10 @@
 
 package blog.objgen;
 
-import java.util.*;
+import java.util.List;
 
-import blog.AbstractObjectIterator;
-import blog.AbstractObjectSet;
-import blog.GenericObject;
-import blog.GenericPOPAppSatisfier;
 import blog.ObjectIterator;
 import blog.ObjectSet;
-import blog.POPAppBasedSet;
-import blog.bn.NumberVar;
-import blog.common.HashMultiMap;
-import blog.common.MultiMap;
-import blog.common.Util;
 import blog.model.ConjFormula;
 import blog.model.Formula;
 import blog.model.LogicalVar;
@@ -80,6 +71,7 @@ public class CompiledSetSpec {
 		disjuncts = phi.getPropDNF().getDisjuncts();
 		objGenGraphs = new ObjGenGraph[disjuncts.size()];
 		for (int i = 0; i < disjuncts.size(); ++i) {
+			// TODO leili: List may be inefficient for get(i), use ArrayList instead.
 			ConjFormula disjunct = (ConjFormula) disjuncts.get(i);
 			objGenGraphs[i] = new ObjGenGraph(var.getType(), var,
 					disjunct.getConjuncts());
@@ -99,7 +91,7 @@ public class CompiledSetSpec {
 	 * <code>var</code>, make <code>orgFormula</code> true in the given context.
 	 */
 	public ObjectSet elementSet(EvalContext context) {
-		return new ElementSet(context);
+		return new ElementSet(this, context);
 	}
 
 	/**
@@ -108,7 +100,7 @@ public class CompiledSetSpec {
 	 * set specification, but it may return other objects as well.
 	 */
 	public ObjectIterator unfilteredIterator(EvalContext context) {
-		return new UnfilteredIterator(context, context.getLogicalVarValues());
+		return new UnfilteredIterator(this, context, context.getLogicalVarValues());
 	}
 
 	/**
@@ -144,7 +136,7 @@ public class CompiledSetSpec {
 		return buf.toString();
 	}
 
-	private Boolean isFirstSatisfiedDisjunct(EvalContext context, Object obj,
+	Boolean isFirstSatisfiedDisjunct(EvalContext context, Object obj,
 			int index) {
 		context.assign(var, obj);
 		Boolean result;
@@ -174,336 +166,9 @@ public class CompiledSetSpec {
 		return result;
 	}
 
-	private class ElementSet extends AbstractObjectSet {
-		ElementSet(EvalContext context) {
-			this.context = context;
-		}
+	LogicalVar var;
+	Formula origFormula;
 
-		protected Boolean isEmptyInternal() {
-			ObjectIterator iter = iterator(context.getLogicalVarValues());
-			return (iter.canDetermineNext() ? Boolean.valueOf(!iter.hasNext()) : null);
-		}
-
-		protected Integer sizeInternal() {
-			int size = 0;
-			ObjectIterator iter = iterator(context.getLogicalVarValues());
-			while (iter.hasNext()) {
-				iter.next();
-				++size;
-				size += iter.skipIndistinguishable();
-			}
-			if (!iter.canDetermineNext()) {
-				return null;
-			}
-			return new Integer(size);
-		}
-
-		public Boolean containsInternal(Object obj) {
-			context.assign(var, obj);
-
-			Boolean result = Boolean.FALSE;
-			for (Iterator iter = disjuncts.iterator(); iter.hasNext();) {
-				Formula disj = (Formula) iter.next();
-				Boolean disjValue = (Boolean) disj.evaluate(context);
-				if (disjValue == null) {
-					result = null;
-					break;
-				}
-				if (disjValue.booleanValue()) {
-					// object satisfies this disjunct, so short-circuit
-					result = Boolean.TRUE;
-					break;
-				}
-			}
-
-			context.unassign(var);
-			return result;
-		}
-
-		public ObjectIterator iterator(Set externallyDistinguished) {
-			externallyDistinguished = new HashSet(externallyDistinguished);
-			externallyDistinguished.addAll(context.getLogicalVarValues());
-			return new SatisfierIterator(externallyDistinguished);
-		}
-
-		public ObjectSet getExplicitVersion() {
-			POPAppBasedSet set = new POPAppBasedSet();
-			MultiMap popAppExceptions = null;
-
-			// See if we can get the satisfiers explicitly regardless of
-			// the POP application
-			GenericObject genericObj = new GenericObject(var.getType());
-			Set satisfiers = origFormula.getSatisfiersIfExplicit(context, var,
-					genericObj);
-			if (satisfiers == Formula.ALL_OBJECTS) {
-				// we know each POP app is included with no exceptions
-				popAppExceptions = MultiMap.EMPTY_MULTI_MAP;
-			} else if (satisfiers != null) {
-				for (Iterator iter = satisfiers.iterator(); iter.hasNext();) {
-					Object obj = iter.next();
-					set.addIndividual(obj, context.getPOPAppSatisfied(obj));
-				}
-				return set;
-			} else {
-				// See if we can find the non-satisfiers explicitly.
-				// If so, we'll still enumerate the POP apps, but we'll know
-				// the exceptions for each one.
-				Set nonSatisfiers = origFormula.getNonSatisfiersIfExplicit(context,
-						var, genericObj);
-				if (nonSatisfiers == Formula.ALL_OBJECTS) {
-					return set; // which is currently empty
-				}
-				if (nonSatisfiers != null) {
-					popAppExceptions = new HashMultiMap();
-					for (Iterator iter = nonSatisfiers.iterator(); iter.hasNext();) {
-						Object obj = iter.next();
-						popAppExceptions.add(context.getPOPAppSatisfied(obj), obj);
-					}
-				}
-			}
-
-			// Get ready to enumerate individuals and POP apps
-			ObjectIterator[] graphIters = new ObjectIterator[objGenGraphs.length];
-			BitSet active = new BitSet();
-			for (int i = 0; i < objGenGraphs.length; ++i) {
-				graphIters[i] = objGenGraphs[i].iterator(context,
-						context.getLogicalVarValues(), true);
-				active.set(i);
-			}
-
-			int nextDisjunctIndex = 0;
-			while (active.cardinality() > 0) {
-				if (!graphIters[nextDisjunctIndex].canDetermineNext()) {
-					return null;
-				}
-
-				if (graphIters[nextDisjunctIndex].hasNext()) {
-					Object nextObj = graphIters[nextDisjunctIndex].next();
-					if (!maybeAddToSet(set, popAppExceptions, nextObj, nextDisjunctIndex)) {
-						return null;
-					}
-					// continue round-robin over disjuncts
-				} else {
-					active.clear(nextDisjunctIndex);
-				}
-
-				nextDisjunctIndex = (nextDisjunctIndex + 1) % graphIters.length;
-			}
-
-			return set;
-		}
-
-		public Object sample(int n) {
-			ObjectSet explicit = getExplicitVersion();
-			if (explicit == null) {
-				return null;
-			}
-			return explicit.sample(n);
-		}
-
-		public int indexOf(Object o) {
-			ObjectSet explicit = getExplicitVersion();
-			if (explicit == null) {
-				return -1;
-			}
-			return explicit.indexOf(o);
-		}
-
-		public String toString() {
-			return (CompiledSetSpec.this.toString() + " " + context
-					.getAssignmentStr());
-		}
-
-		private boolean maybeAddToSet(POPAppBasedSet set,
-				MultiMap popAppExceptions, Object obj, int disjunctIndex) {
-			if (obj instanceof NumberVar) {
-				return maybeAddPOPApp(set, popAppExceptions, (NumberVar) obj,
-						disjunctIndex);
-			}
-
-			Boolean isFirst = isFirstSatisfiedDisjunct(context, obj, disjunctIndex);
-			if (isFirst == null) {
-				return false;
-			}
-			if (isFirst.booleanValue()) {
-				NumberVar popApp = context.getPOPAppSatisfied(obj);
-				set.addIndividual(obj, popApp);
-			}
-			return true; // successfully determined whether to add or not
-		}
-
-		private boolean maybeAddPOPApp(POPAppBasedSet set,
-				MultiMap popAppExceptions, NumberVar popApp, int disjunctIndex) {
-			ObjectSet satisfiers = context.getSatisfiers(popApp);
-			if (popAppExceptions != null) {
-				// We were able to enumerate exceptions explicitly
-				// without even considering the generating functions
-				set.addSatisfiers(popApp, satisfiers,
-						(Set) popAppExceptions.get(popApp));
-				return true;
-			}
-
-			// Try finding explicit representation by taking
-			// generating functions into account
-			GenericObject genericObj = new GenericPOPAppSatisfier(popApp.pop(),
-					popApp.args());
-			Set explicitSatisfiers = origFormula.getSatisfiersIfExplicit(context,
-					var, genericObj);
-			if (explicitSatisfiers == Formula.ALL_OBJECTS) {
-				// this POP app is included with no exceptions
-				set.addSatisfiers(popApp, satisfiers, Collections.EMPTY_SET);
-				return true;
-			} else if (explicitSatisfiers != null) {
-				for (Iterator iter = explicitSatisfiers.iterator(); iter.hasNext();) {
-					set.addIndividual(iter.next(), popApp);
-				}
-				return true;
-			} else {
-				// See if we can find the non-satisfiers explicitly.
-				Set nonSatisfiers = origFormula.getNonSatisfiersIfExplicit(context,
-						var, genericObj);
-				if (nonSatisfiers == Formula.ALL_OBJECTS) {
-					return true; // don't add anything
-				}
-				if (nonSatisfiers != null) {
-					set.addSatisfiers(popApp, satisfiers, nonSatisfiers);
-					return true;
-				}
-			}
-
-			// If we get here, then we actually have to iterate over objects
-
-			Set in = new LinkedHashSet();
-			Set out = new HashSet();
-			Boolean mostlyIncluded = null;
-
-			for (ObjectIterator iter = satisfiers.iterator(context
-					.getLogicalVarValues()); iter.hasNext();) {
-				Object obj = iter.next();
-				Boolean isFirst = isFirstSatisfiedDisjunct(context, obj, disjunctIndex);
-				if (isFirst == null) {
-					return false;
-				}
-				if (isFirst.booleanValue()) {
-					in.add(obj);
-				} else {
-					out.add(obj);
-				}
-
-				if (mostlyIncluded == null) {
-					if (iter.skipIndistinguishable() > 0) {
-						mostlyIncluded = isFirst;
-					}
-				}
-			}
-
-			if ((mostlyIncluded != null) && mostlyIncluded.booleanValue()) {
-				set.addSatisfiers(popApp, satisfiers, out);
-			} else {
-				for (Iterator iter = in.iterator(); iter.hasNext();) {
-					set.addIndividual(iter.next(), popApp);
-				}
-			}
-
-			return true;
-		}
-
-		private class SatisfierIterator extends AbstractObjectIterator {
-			SatisfierIterator(Set externallyDistinguished) {
-				graphIters = new ObjectIterator[objGenGraphs.length];
-				for (int i = 0; i < objGenGraphs.length; ++i) {
-					graphIters[i] = objGenGraphs[i].iterator(context,
-							externallyDistinguished, false);
-					active.set(i);
-				}
-			}
-
-			protected int skipAfterNext() {
-				return graphIters[nextDisjunctIndex].skipIndistinguishable();
-			}
-
-			protected Object findNext() {
-				while (active.cardinality() > 0) {
-					nextDisjunctIndex = (nextDisjunctIndex + 1) % graphIters.length;
-
-					if (!graphIters[nextDisjunctIndex].canDetermineNext()) {
-						canDetermineNext = false;
-						return null;
-					}
-
-					if (graphIters[nextDisjunctIndex].hasNext()) {
-						Object nextObj = graphIters[nextDisjunctIndex].next();
-						Boolean isFirst = isFirstSatisfiedDisjunct(context, nextObj,
-								nextDisjunctIndex);
-
-						if (isFirst == null) {
-							canDetermineNext = false;
-							return null;
-						}
-						if (isFirst.booleanValue()) {
-							return nextObj;
-						}
-
-						// current value of nextObj is not in this set
-						graphIters[nextDisjunctIndex].skipIndistinguishable();
-						// continue round-robin over disjuncts
-					} else {
-						active.clear(nextDisjunctIndex);
-					}
-				}
-
-				return null;
-			}
-
-			private ObjectIterator[] graphIters;
-			private BitSet active = new BitSet();
-			private int nextDisjunctIndex = -1;
-		}
-
-		private EvalContext context;
-	}
-
-	private class UnfilteredIterator extends AbstractObjectIterator {
-		UnfilteredIterator(EvalContext context, Set externallyDistinguished) {
-			graphIters = new ObjectIterator[objGenGraphs.length];
-			for (int i = 0; i < objGenGraphs.length; ++i) {
-				graphIters[i] = objGenGraphs[i].iterator(context,
-						externallyDistinguished, false);
-				active.set(i);
-			}
-		}
-
-		protected int skipAfterNext() {
-			return graphIters[nextDisjunctIndex].skipIndistinguishable();
-		}
-
-		protected Object findNext() {
-			while (active.cardinality() > 0) {
-				nextDisjunctIndex = (nextDisjunctIndex + 1) % graphIters.length;
-
-				if (!graphIters[nextDisjunctIndex].canDetermineNext()) {
-					canDetermineNext = false;
-					return null;
-				}
-
-				if (graphIters[nextDisjunctIndex].hasNext()) {
-					return graphIters[nextDisjunctIndex].next();
-				} else {
-					active.clear(nextDisjunctIndex);
-				}
-			}
-
-			return null;
-		}
-
-		private ObjectIterator[] graphIters;
-		private BitSet active = new BitSet();
-		private int nextDisjunctIndex = -1;
-	}
-
-	private LogicalVar var;
-	private Formula origFormula;
-
-	private List disjuncts; // of ConjFormula
-	private ObjGenGraph[] objGenGraphs;
+	List disjuncts; // of ConjFormula
+	ObjGenGraph[] objGenGraphs;
 }
