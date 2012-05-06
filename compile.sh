b514161dbce69a5b13f8a1364160a299417ba95c
leili <leili@e5a44606-1236-43ac-9aa5-cdde1b33ff33>
2012-05-06 00:19:53 +0000

    added compile.sh and readme
    git-svn-id: https://patmos.banatao.berkeley.edu:8889/svnbase/outbids/code/trunk@215 e5a44606-1236-43ac-9aa5-cdde1b33ff33

--- compile.sh
+++ compile.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+#script for compiling blog
+
 mkdir -p bin
 
 find src -name \*.java -print >javafiles
 
-javac  -verbose -cp lib/java_cup.jar:lib/JFlex-1.4.3.jar:lib/commons-math3-3.0.jar:lib/commons-math-2.2.jar:lib/junit-4.10.jar:bin/ -d bin/ @javafiles
+javac  -verbose -cp lib/java_cup.jar:lib/JFlex-1.4.3.jar:lib/commons-math3-3.0.jar:lib/commons-math-2.2.jar:lib/junit-4.10.jar:lib/Jama.jar:bin/ -d bin/ @javafiles
 
 rm javafiles
