90f099f2174b27293fb86328a08d4e60e7814109
lileicc <lileicc@gmail.com>
2012-05-06 00:15:38 +0000

    git-svn-id: https://patmos.banatao.berkeley.edu:8889/svnbase/outbids/code/trunk@214 e5a44606-1236-43ac-9aa5-cdde1b33ff33

---
+++ compile.sh
@@ -0,0 +1,7 @@
+mkdir -p bin
+
+find src -name \*.java -print >javafiles
+
+javac  -verbose -cp lib/java_cup.jar:lib/JFlex-1.4.3.jar:lib/commons-math3-3.0.jar:lib/commons-math-2.2.jar:lib/junit-4.10.jar:bin/ -d bin/ @javafiles
+
+rm javafiles
