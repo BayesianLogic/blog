3f5124700b33a278193720f3deaaab1a256da195
aki <aki@e5a44606-1236-43ac-9aa5-cdde1b33ff33>
2012-05-15 17:08:28 +0000

    some tests before implementing if
    git-svn-id: https://patmos.banatao.berkeley.edu:8889/svnbase/outbids/code/trunk@248 e5a44606-1236-43ac-9aa5-cdde1b33ff33

---
+++ src/test/blog/parse/TestObs.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package test.blog.parse;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import blog.absyn.BooleanExpr;
+import blog.absyn.DistinctSymbolDec;
+import blog.absyn.DoubleExpr;
+import blog.absyn.FieldList;
+import blog.absyn.FixedFuncDec;
+import blog.absyn.FuncCallExpr;
+import blog.absyn.NameTy;
+import blog.absyn.OpExpr;
+import blog.absyn.Stmt;
+import blog.absyn.StringExpr;
+import blog.absyn.SymbolArray;
+import blog.absyn.SymbolArrayList;
+import blog.symbol.Symbol;
+
+/**
+ * @author amatsukawa
+ * @date Apr 19, 2012
+ */
+
+//TODO: These still need testing, but this helps me see the tree structure
+
+public class TestObs extends TestParse {
+
+	@Test
+	public void testSimpleDecl() {
+		String toParse = "obs ObsColor(Draw1) = Blue;";
+		String parsed = TestParse.parsedStringRepr(toParse);
+		System.out.println(parsed);
+	}
+
+}
