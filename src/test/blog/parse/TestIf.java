94da35bfde7c550be93b1e192af095c03a5d9199
Akihiro Matsukawa <amatsukawa@am-desktop.(none)>
2012-05-17 04:11:40 -0700

    Right before FuncCallExpr change, really just a commit for travel

--- src/test/blog/parse/TestIf.java
+++ src/test/blog/parse/TestIf.java
@@ -30,11 +30,11 @@ public class TestIf extends TestParse {
 
 	@Test
 	public void testIfDecl() {
-		String parsed = TestParse.parsedStringRepr("Random Color test if 1 == 2 & 1 == 3 then ~ Poisson(5);");
 		String toParse = "random Real test " +
 				"if 1 == 2 then ~ Poisson(5) " + 
 				"else if 1 == 3 then ~ Poisson(4);";
-		parsed = TestParse.parsedStringRepr(toParse);
+		String parsed = TestParse.parsedStringRepr(toParse);
+		parsed = TestParse.parsedStringRepr("Random Color test(some d) if Ball(d) == 2 & 1 == 3 then ~ Poisson(5.0);");
 		System.out.println(parsed);
 	}
 	
