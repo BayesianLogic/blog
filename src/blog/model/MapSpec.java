6dcd1073d824e41be621414b96c6d62433b31acc
Akihiro Matsukawa <amatsukawa@am-desktop.(none)>
2012-06-10 22:16:29 -0700

    initial MapSpec.java file

---
+++ src/blog/model/MapSpec.java
@@ -0,0 +1,18 @@
+package blog.model;
+
+import java.util.*;
+
+/** ArgSpec that represents a map statement, such as 
+ * {Blue -> 0.5, Green -> 0.5}.
+ *  For now, only supports fixed parameters. 
+ *
+ *  @author amatsukawa
+ */
+
+public class MapSpec extends ArgSpec {
+
+
+
+    public MapSpec() {
+    }
+}
