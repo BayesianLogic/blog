660ec7848cda098a377ebe2427e1638272dc7c8b
awong <fictitiousgains@gmail.com>
2012-07-15 00:40:52 -0700

    1) Constructed test infrastructure (run with test-ex.sh, subclass from test.example.ExampleTest) 2) Added regression test for poisson-ball.blog 3) Deleted models in old syntax (new syntax versions exist with extension .dblog)

---
+++ test-ex.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BLOG_HOME="."
+export BLOG_HOME
+
+J_PATH_SEP=`${BLOG_HOME}/path_sep.sh`
+
+# Add additional test classes to this list
+for CLASS in test.example.TestPoissonBall test.example.TestSimpleAircraftB
+do
+    java -cp "${BLOG_HOME}/lib/commons-math3-3.0.jar${J_PATH_SEP}${BLOG_HOME}/bin${J_PATH_SEP}${BLOG_HOME}/lib/java_cup.jar${J_PATH_SEP}${BLOG_HOME}/lib/JFlex-1.4.3.jar$" $CLASS
+    RET=$?
+    if [ $RET != 0 ]
+    then
+	break
+    fi
+done
