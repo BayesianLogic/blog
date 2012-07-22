dfebfe437e5cb04457b3a306c729f87112190f92
awong <fictitiousgains@gmail.com>
2012-07-21 22:06:07 -0700

    Corrected timestamps on new test classes

--- src/test/example/TestHurricane.java
+++ src/test/example/TestHurricane.java
@@ -4,53 +4,53 @@ import java.util.HashMap;
 
 /**
  * Regression test for hurricane.blog
- * (correct as of <date>)
- * 
+ * (correct as of July 19, 2012)
+ *
  * @author akanata
  * @date July 19, 2012
  *
  */
 public class TestHurricane extends ExampleTest {
-	protected static final String DEF_ENGINE = "blog.engine.SamplingEngine";
-	protected static final int NUM_SAMPLES = 50000;
-	protected static final int NUM_BURNIN = 0;
-	protected static final String DEF_SAMPLER = "blog.sample.LWSampler";
-	protected static final String DEF_PROPOSER = "blog.GenericProposer";
-	protected static final int STAT_INTERVAL = 2500;
-	
-	private static final double ERR_BOUND = 5e-4;
+        protected static final String DEF_ENGINE = "blog.engine.SamplingEngine";
+        protected static final int NUM_SAMPLES = 50000;
+        protected static final int NUM_BURNIN = 0;
+        protected static final String DEF_SAMPLER = "blog.sample.LWSampler";
+        protected static final String DEF_PROPOSER = "blog.GenericProposer";
+        protected static final int STAT_INTERVAL = 2500;
 
-	public static void main(String[] args) throws IncorrectProbException {
-		TestHurricane test = new TestHurricane();
-		test.runTest();
-	}
-	
-	public TestHurricane() {
-		super(ERR_BOUND);
-	}
+        private static final double ERR_BOUND = 5e-4;
 
-	@Override
-	public void setInputModel() {
-		pathsSource.add("example/hurricane.blog");
-	}
+        public static void main(String[] args) throws IncorrectProbException {
+                TestHurricane test = new TestHurricane();
+                test.runTest();
+        }
 
-	@Override
-	public void setInferenceProps() {
-		inferenceProps.setProperty("engineClass", DEF_ENGINE);
-		inferenceProps.setProperty("numSamples", "" + NUM_SAMPLES);
-		inferenceProps.setProperty("burnIn", "" + NUM_BURNIN);
-		inferenceProps.setProperty("samplerClass", DEF_SAMPLER);
-		inferenceProps.setProperty("proposerClass", DEF_PROPOSER);
-		inferenceProps.setProperty("reportInterval", "" + STAT_INTERVAL);
-	}
+        public TestHurricane() {
+                super(ERR_BOUND);
+        }
 
-	@Override
-	public void referenceProbs() {
-		HashMap<Object, Double> damage = refProbs.get(0);
-		damage.put(model.getConstantValue("Mild"), 0.74);
-		damage.put(model.getConstantValue("Severe"), 0.26);
-		
-		HashMap<Object, Double> secondProb = refProbs.get(1);
-		secondProb.put(model.getConstantValue("B"), 1.0);
-	}
+        @Override
+        public void setInputModel() {
+                pathsSource.add("example/hurricane.blog");
+        }
+
+        @Override
+        public void setInferenceProps() {
+                inferenceProps.setProperty("engineClass", DEF_ENGINE);
+                inferenceProps.setProperty("numSamples", "" + NUM_SAMPLES);
+                inferenceProps.setProperty("burnIn", "" + NUM_BURNIN);
+                inferenceProps.setProperty("samplerClass", DEF_SAMPLER);
+                inferenceProps.setProperty("proposerClass", DEF_PROPOSER);
+                inferenceProps.setProperty("reportInterval", "" + STAT_INTERVAL);
+        }
+
+        @Override
+        public void referenceProbs() {
+                HashMap<Object, Double> damage = refProbs.get(0);
+                damage.put(model.getConstantValue("Mild"), 0.74);
+                damage.put(model.getConstantValue("Severe"), 0.26);
+
+                HashMap<Object, Double> secondProb = refProbs.get(1);
+                secondProb.put(model.getConstantValue("B"), 1.0);
+        }
 }
