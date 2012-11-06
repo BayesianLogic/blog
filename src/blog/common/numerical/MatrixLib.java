c1bb471b38e01994fb388b4358843f53971b7cd2
awong <fictitiousgains@gmail.com>
2012-11-06 10:23:13 -0800

    First revision of MatrixSpec, using Jama.Matrix library
    Note that a different matrix library may be used, by implementing
    the MatrixLib interface and instantiating the implemented class
    in MatrixSpec.compile().
    
    No functions are supported yet; this will come in the next commit.

---
+++ src/blog/common/numerical/MatrixLib.java
@@ -0,0 +1,20 @@
+package blog.common.numerical;
+
+/**
+ * Exposes different matrix libraries to BLOG using a consistent set of
+ * methods.  Different libraries may be used with BLOG without significant
+ * code modifications.
+ * 
+ * @author awong
+ * @date November 5, 2012
+ */
+public interface MatrixLib {	
+	/**
+	 * Gives the value of an element of this matrix
+	 * 
+	 * @param x the x-index
+	 * @param y the y-index
+	 * @return 
+	 */
+	public double elementAt(int x, int y);
+}
