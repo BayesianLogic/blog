9b26eceffd819726163b3a93315a2a111c92787c
Dan Wang <d.wang1991@gmail.com>
2012-12-25 01:26:45 -0800

    Clean up JS + HTML

--- web/static/blog/blog.js
+++ web/static/blog/blog.js
@@ -1,29 +1,40 @@
-CodeMirror.defineMode("diff", function() {
+$(function() {
+  CodeMirror.defineMode("diff", function() {
+    var keywords = wordRegexp(['extern','import','fixed','func','distinct','random','origin','param','type', 'obs', 'query']);
+    var types = wordRegexp(['Integer', 'Real', 'Boolean', 'NaturalNum', 'String', 'List', 'Map', 'TabularCPD','Categorical']);
 
-	var keywords = wordRegexp(['extern','import','fixed','func','distinct','random','origin','param','type', 'obs', 'query']);
-	var types = wordRegexp(['Integer', 'Real', 'Boolean', 'NaturalNum', 'String', 'List', 'Map', 'TabularCPD','Categorical']);
-
-	function wordRegexp(words) {
-        return new RegExp("^((" + words.join(")|(") + "))\\b");
+    function wordRegexp(words) {
+      return new RegExp("^((" + words.join(")|(") + "))\\b");
     }
-	
-  	return {
-  		token: function(stream){
-  			
-  			console.log(stream.current());
-  			// Remove all space
-			if (stream.eatSpace()) return null;
-			
-			if (stream.match(keywords)) return 'keyword';
-			
-			if (stream.match(types)) return 'tag';
-			
-			if (stream.match(/[:=~]/)) return 'atom';	
-			
-			stream.next();
-			return null;
-		}
-  };
-});
 
-CodeMirror.defineMIME("text/x-diff", "diff");
+    return {
+      token: function(stream){
+        if (stream.eatSpace()) return null;
+        if (stream.match(keywords)) return 'keyword';
+        if (stream.match(types)) return 'tag';
+        if (stream.match(/[:=~]/)) return 'atom';
+        stream.next();
+        return null;
+      }
+    };
+  });
+
+  CodeMirror.defineMIME("text/x-diff", "diff");
+
+  var editor = CodeMirror.fromTextArea($('#textfield')[0], {
+    lineNumbers: true,
+    matchBrackets: true
+  });
+
+  $(".button").click(function() {
+    var input_string = editor.getValue();
+    $.ajax({
+      type: "POST",
+      data: {textfield : input_string},
+      success: function(data) {
+        $('#text_result').text(data).hide().fadeIn(1500);
+      },
+    });
+    return false;
+  });
+});
