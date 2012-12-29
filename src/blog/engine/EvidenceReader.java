70c293e4a39cdfb3b92305ce9bb428314e2b248b
Cheng <x.cheng@berkeley.edu>
2012-12-28 16:34:49 -0800

    deleted old stuff

---
+++ src/blog/engine/EvidenceReader.java
@@ -0,0 +1,13 @@
+package blog.engine;
+
+import java.io.BufferedReader;
+import java.io.Reader;
+
+public class EvidenceReader extends BufferedReader {
+
+	public EvidenceReader(Reader in) {
+		super(in);
+		// TODO Auto-generated constructor stub
+	}
+
+}
