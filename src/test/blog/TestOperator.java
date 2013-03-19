6c48e9e9769af1cec0897621530ccc9ed0fe12c1
awong <fictitiousgains@gmail.com>
2013-03-18 22:00:39 -0700

    Added tests for comparison operators, refactored min/max/round built-in function tests

--- src/test/blog/TestOperator.java
+++ src/test/blog/TestOperator.java
@@ -69,31 +69,4 @@ public class TestOperator {
     Main.runFromString(code, args);
   }
   
-  @Test
-  public void testMin() {
-    String code = "fixed Integer small = min({Integer k: k > 3 & k < 7});\n"
-        + "query small;";
-    Main.runFromString(code, args);
-  }
-
-  @Test
-  public void testMax() {
-    String code = "fixed Integer large = max({Integer k: k > 3 & k < 7});\n"
-        + "query large;";
-    Main.runFromString(code, args);
-  }
-
-  @Test
-  public void testRoundLower() {
-	String code = "fixed Integer rounded = round(1.4);\n"
-        + "query rounded;";
-	Main.runFromString(code, args);
-  }
-  
-  @Test
-  public void testRoundHigher() {
-	String code = "fixed Integer rounded = round(1.5);\n"
-        + "query rounded;";
-	Main.runFromString(code, args);
-  }
 }
