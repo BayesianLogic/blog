e5ae48d939949eecbc9521236d881b3e1efba3ad
awong <fictitiousgains@gmail.com>
2013-03-31 17:04:28 -0700

    Tweaked test for hurricane.blog for new queries

--- src/test/example/TestHurricane.java
+++ src/test/example/TestHurricane.java
@@ -46,7 +46,19 @@ public class TestHurricane extends ExampleTest {
 
         @Override
         public void referenceProbs() {
-                HashMap<Object, Double> damage = refProbs.get(0);
+        		HashMap<Object, Double> first = refProbs.get(0);
+        		first.put(model.getConstantValue("A"), 0.5);
+        		first.put(model.getConstantValue("B"), 0.5);
+        		
+        		HashMap<Object, Double> damageA = refProbs.get(1);
+        		damageA.put(model.getConstantValue("Severe"), 0.63);
+        		damageA.put(model.getConstantValue("Mild"), 0.37);
+        		
+        		HashMap<Object, Double> damageB = refProbs.get(2);
+        		damageB.put(model.getConstantValue("Severe"), 0.63);
+        		damageB.put(model.getConstantValue("Mild"), 0.37);
+        	
+                HashMap<Object, Double> damage = refProbs.get(3);
                 damage.put(model.getConstantValue("Mild"), 0.74);
                 damage.put(model.getConstantValue("Severe"), 0.26);
         }
