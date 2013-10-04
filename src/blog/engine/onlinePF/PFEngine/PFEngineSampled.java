31ab0ab9b88f3db5ff21afc629dc128cda927d8e
Cheng <x.cheng@berkeley.edu>
2013-10-04 15:02:22 -0700

    fixed obs log

--- src/blog/engine/onlinePF/PFEngine/PFEngineSampled.java
+++ src/blog/engine/onlinePF/PFEngine/PFEngineSampled.java
@@ -79,7 +79,6 @@ public class PFEngineSampled extends PFEngineOnline{
 		if (ev.compile()!=0)
 			System.exit(1);
 
-		UBT.obsOutput.printInput(evs);
 		for (Particle o : particles){
 			TimedParticle p = (TimedParticle) o;
 			p.unInstantiateObservables(selectedOS);
@@ -146,7 +145,7 @@ public class PFEngineSampled extends PFEngineOnline{
 		if (ev.compile()!=0)
 			System.exit(1);
 
-		
+		UBT.obsOutput.printInput(ev.toString());
 		for (Particle o : particles){
 			TimedParticle p = (TimedParticle) o;
 			p.unInstantiateObservables(selectedOS);
