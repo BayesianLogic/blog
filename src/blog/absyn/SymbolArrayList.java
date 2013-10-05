56eacb0730b9cfa6e3727a8583c0a08cd6b32af5
William Cushing <william.cushing@gmail.com>
2013-10-04 19:22:02 -0700

    Added iteration support
    Expanded deprecation comment
    
    Added comment regarding Iterator implementation.
    
    Added iteration syntax support
    
    Refactored names, added a bunch of utility methods to make reading
    test-cases easier.

--- src/blog/absyn/SymbolArrayList.java
+++ src/blog/absyn/SymbolArrayList.java
@@ -4,7 +4,7 @@ package blog.absyn;
  * @author leili
  * @date Apr 22, 2012
  */
-public class SymbolArrayList extends Absyn {
+public class SymbolArrayList extends Absyn implements Iterable<SymbolArray> {
 	public SymbolArray head;
 	public SymbolArrayList next;
 
@@ -23,5 +23,21 @@ public class SymbolArrayList extends Absyn {
 				next.printTree(pr, d);
 		}
 	}
+	
+	/**
+	 * @see StmtList.Iterator
+	 */
+	public class Iterator implements java.util.Iterator<SymbolArray> {
+		SymbolArrayList curr=null;
+		public Iterator(SymbolArrayList SymbolArrayList) { curr = SymbolArrayList; }
+		public boolean hasNext() { return curr != null; }
+		public SymbolArray next() {
+			SymbolArray o = curr.head;
+			curr = curr.next;
+			return o;
+		}
+		public void remove() { throw new UnsupportedOperationException(); }
+	}
+	public Iterator iterator() { return new Iterator(this);}
 
 }
--- src/test/blog/parse/TestParse.java
+++ src/test/blog/parse/TestParse.java
@@ -4,54 +4,78 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import static org.junit.Assert.*;
 
 import blog.absyn.*;
-import blog.symbol.Symbol;
 import blog.parse.Parse;
+import blog.symbol.Symbol;
+
 
 /**
  * @author leili, amatsukawa
  * @date Apr 15, 2012
- * 
+ * @author William Cushing
+ * @date 2013/10/6
  */
 public class TestParse {
 
+	protected TestParse() {}
+	
 	public static ByteArrayOutputStream out = new ByteArrayOutputStream();
-	public static Printer pr = new Printer(new PrintStream(out));
+	public static IndentingPrinter pr = new IndentingPrinter(new PrintStream(out));
 
-	/**
-	 * @throws java.lang.Exception
-	 */
 	@After
-	public void tearDown() throws Exception {
-		TestParse.out.reset();
-	}
-	
-	public static String parsedStringRepr(String s) {
-		Parse tester = Parse.parseString(s);
-		Absyn parsedTree = tester.getParseResult();
-		parsedTree.printTree(pr, 0);
-		String parsed = out.toString();
+	public static void reset() {
 		out.reset();
-		return parsed;
+		pr.reset();
 	}
 	
-	public static String shouldBeRepr(Stmt[] stmts){
-		Absyn shouldBeTree = makeStmtList(stmts);
-		shouldBeTree.printTree(pr, 0);
-		String shouldBe = out.toString();
-		return shouldBe;
+	public static String parseToRepr(String s) {
+		Parse tester = Parse.parseString(s);
+		Absyn parsedTree = tester.getResult();
+		parsedTree.printSyntax(pr);
+		return out.toString();
 	}
 	
-	public static StmtList makeStmtList(Stmt[] stmts) {
-		StmtList head = null;
-		for(int i = stmts.length-1; i > -1; i--) {
-			head = new StmtList(stmts[i], head);
-		}
-		return head;
+	public static String toRepr(Stmt... stmts){
+		return toRepr(Stmts(stmts));
+	}
+	public static String toRepr(Absyn x){
+		x.printSyntax(pr);
+		return out.toString();
 	}
 
+	public static SymbolArrayList Symbols(Symbol... xs) { 
+		SymbolArray[] ys = new SymbolArray[xs.length];
+		for (int i=0; i<xs.length; ++i)
+			ys[i] = new SymbolArray(xs[i]);
+		return SymbolArrayList.SymbolArrayList(ys);
+	}
+	public static StmtList Stmts(Stmt... xs) 
+	{ return StmtList.StmtList(xs);}
+	public static FieldList Fields(Field... xs) 
+	{ return FieldList.FieldList(xs);}
+	public static ExprList Exprs(Expr... xs) 
+	{ return ExprList.ExprList(xs);}
+
+	public static Symbol Symbol(String n) 
+	{ return Symbol.Symbol(n);}
+	public static Field Field(Symbol n, Ty t) 
+	{ return new Field(n,t);}
+	public static NameTy Type(Symbol n) 
+	{ return new NameTy(n);}
+	
+	public static OpExpr Expr(int code, Expr l, Expr r)
+	{ return new OpExpr(l,code,r); }
+	public static DoubleExpr Expr(double x)
+	{ return new DoubleExpr(x); }
+	public static IntExpr Expr(int x)
+	{ return new IntExpr(x); }
+	public static BooleanExpr Expr(boolean x)
+	{ return new BooleanExpr(x); }
+	public static StringExpr Expr(String x)
+	{ return new StringExpr(x); }
+	public static SymbolExpr Expr(Symbol x)
+	{ return new SymbolExpr(x); }
 }
+
+
