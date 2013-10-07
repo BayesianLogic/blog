948f023a83ece06481d2f028bd38784a605f37d4
lileicc <lileicc@gmail.com>
2013-10-07 13:58:53 -0700

    formatting the source

--- src/blog/absyn/Absyn.java
+++ src/blog/absyn/Absyn.java
@@ -20,148 +20,163 @@ import java.util.ArrayList;
  */
 
 abstract public class Absyn {
-	public int col;
-	public int line;
+  public int col;
+  public int line;
 
-	/**
-	 * @param line
-	 * @param col
-	 */
-	public Absyn(int line, int col) {
-		this.line = line;
-		this.col = col;
-	}
+  /**
+   * @param line
+   * @param col
+   */
+  public Absyn(int line, int col) {
+    this.line = line;
+    this.col = col;
+  }
 
-	/**
-	 * @deprecated
-	 */
-	public Absyn() {
-		line = 0;
-		col = 0;
-	}
+  /**
+   * @deprecated
+   */
+  public Absyn() {
+    line = 0;
+    col = 0;
+  }
 
-	/**
-	 * @deprecated
-	 * @param p
-	 */
-	public Absyn(int p) {
-		this(0, p);
-	}
+  /**
+   * @deprecated
+   * @param p
+   */
+  public Absyn(int p) {
+    this(0, p);
+  }
 
-	/**
-	 * @return position of this abstract syntax tree in the input file
-	 */
-	public int getCol() {
-		return col;
-	}
+  /**
+   * @return position of this abstract syntax tree in the input file
+   */
+  public int getCol() {
+    return col;
+  }
 
-	/**
-	 * should directly use .line
-	 * 
-	 * @return
-	 */
-	public int getLine() {
-		return line;
-	}
+  /**
+   * should directly use .line
+   * 
+   * @return
+   */
+  public int getLine() {
+    return line;
+  }
 
-	/**
-	 * Prints the syntax tree, given a Printer class.
-	 * 
-	 * @param pr Printer, where to print
-	 * @param d depth of the node in the tree
-	 * 
-	 * @deprecated Use {@link #MISSING()} instead.  In a child, consider deleting/deprecating printTree.
-	 */
-	public void printTree(Printer pr, int d) { 
-		throw new UnsupportedOperationException();
-	}
+  /**
+   * Prints the syntax tree, given a Printer class.
+   * 
+   * @param pr
+   *          Printer, where to print
+   * @param d
+   *          depth of the node in the tree
+   * 
+   * @deprecated Use {@link #MISSING()} instead. In a child, consider
+   *             deleting/deprecating printTree.
+   */
+  public void printTree(Printer pr, int d) {
+    throw new UnsupportedOperationException();
+  }
 
-	/**
-	 * Filters out the source location fields, as well as class-level fields.
-	 * 
-	 * @param fields result of getFields() 
-	 * 	(or getDeclaredFields())
-	 * @return new ArrayList omitting "line", "col", and static fields
-	 * @see Class#getFields()
-	 * @see Class#getDeclaredFields()
-	 * @see Modifier
-	 * @see #line
-	 * @see #col
-	 */
-	protected static ArrayList<Field> pruneFields(Field[] fields) {
-		ArrayList<Field> ret = new ArrayList<Field>(fields.length-2);
-		for (Field f : fields) {
-			if (shouldPrune(f)) continue;
-			ret.add(f);
-		}
-		return ret;
-	}
+  /**
+   * Filters out the source location fields, as well as class-level fields.
+   * 
+   * @param fields
+   *          result of getFields()
+   *          (or getDeclaredFields())
+   * @return new ArrayList omitting "line", "col", and static fields
+   * @see Class#getFields()
+   * @see Class#getDeclaredFields()
+   * @see Modifier
+   * @see #line
+   * @see #col
+   */
+  protected static ArrayList<Field> pruneFields(Field[] fields) {
+    ArrayList<Field> ret = new ArrayList<Field>(fields.length - 2);
+    for (Field f : fields) {
+      if (shouldPrune(f))
+        continue;
+      ret.add(f);
+    }
+    return ret;
+  }
 
-	/**
-	 * Helper for pruneFields; tests to see whether a field ought to be pruned.
-	 * 
-	 * "Ought to be pruned" means that the field tracks source locations,
-	 *  or that the "field" is a class-level, rather than instance-level, field.
-	 *  
-	 * @param f field to test
-	 * @return true if it should be squelched
-	 * @see #pruneFields(Field[])
-	 */
-	protected static boolean shouldPrune(Field f) {
-		if (f.getName() == "line") return true;
-		if (f.getName() == "col") return true;
-		if (Modifier.isStatic(f.getModifiers())) return true;
-		return false;
-	}
+  /**
+   * Helper for pruneFields; tests to see whether a field ought to be pruned.
+   * 
+   * "Ought to be pruned" means that the field tracks source locations,
+   * or that the "field" is a class-level, rather than instance-level, field.
+   * 
+   * @param f
+   *          field to test
+   * @return true if it should be squelched
+   * @see #pruneFields(Field[])
+   */
+  protected static boolean shouldPrune(Field f) {
+    if (f.getName() == "line")
+      return true;
+    if (f.getName() == "col")
+      return true;
+    if (Modifier.isStatic(f.getModifiers()))
+      return true;
+    return false;
+  }
 
-	/**
-	 * Helper for printValue; looks up the value of a field reflectively, squelches any exceptions.
-	 * <p>
-	 * There shouldn't be any exceptions generated, as long as this method is called with a Field object in fact representing a Field of this object.
-	 * The method is protected (private might be better) to help ensure that it is called correctly.
-	 * 
-	 * @param f a Field object representing some field of this object
-	 * @return its value
-	 * @see java.lang.reflect.Field
-	 * @see #MISSING()
-	 */
-	protected Object getValue(Field f) {
-		Object value = null;
-		try {
-			value = f.get(this);
-		} catch (IllegalArgumentException e) {
-			// "can't" happen
-		} catch (IllegalAccessException e) {
-			// "can't" happen
-		}
-		return value;
-	}
-	/**
-	 * Helper for printValue; for parsimony with getValue: returns the name of a given field.
-	 * 
-	 * @param f a Field reflectively representing a field of this object
-	 * @return its name
-	 * @see java.lang.reflect.Field
-	 * @see #MISSING()
-	 * @see #getValue(Field)
-	 */
-	protected String getName(Field f) {
-		return f.getName();
-	}
+  /**
+   * Helper for printValue; looks up the value of a field reflectively,
+   * squelches any exceptions.
+   * <p>
+   * There shouldn't be any exceptions generated, as long as this method is
+   * called with a Field object in fact representing a Field of this object. The
+   * method is protected (private might be better) to help ensure that it is
+   * called correctly.
+   * 
+   * @param f
+   *          a Field object representing some field of this object
+   * @return its value
+   * @see java.lang.reflect.Field
+   * @see #MISSING()
+   */
+  protected Object getValue(Field f) {
+    Object value = null;
+    try {
+      value = f.get(this);
+    } catch (IllegalArgumentException e) {
+      // "can't" happen
+    } catch (IllegalAccessException e) {
+      // "can't" happen
+    }
+    return value;
+  }
 
+  /**
+   * Helper for printValue; for parsimony with getValue: returns the name of a
+   * given field.
+   * 
+   * @param f
+   *          a Field reflectively representing a field of this object
+   * @return its name
+   * @see java.lang.reflect.Field
+   * @see #MISSING()
+   * @see #getValue(Field)
+   */
+  protected String getName(Field f) {
+    return f.getName();
+  }
 
-	/**
-	 * Uses printSyntax to deliver the S-expression rendering of the tree.
-	 * 
-	 * @see java.lang.Object#toString()
-	 * @see #MISSING()
-	 * @see PrettyPrinter#printSourceLocations
-	 */
-	public String toString() {
-		ByteArrayOutputStream str = new ByteArrayOutputStream();
-		PrettyPrinter pr = new PrettyPrinter(str);
-		pr.printSyntax(this);
-		return str.toString();
-	}
+  /**
+   * Uses printSyntax to deliver the S-expression rendering of the tree.
+   * 
+   * @see java.lang.Object#toString()
+   * @see #MISSING()
+   * @see PrettyPrinter#printSourceLocations
+   */
+  public String toString() {
+    ByteArrayOutputStream str = new ByteArrayOutputStream();
+    PrettyPrinter pr = new PrettyPrinter(str);
+    pr.printSyntax(this);
+    return str.toString();
+  }
 
 }
--- src/blog/absyn/ExprList.java
+++ src/blog/absyn/ExprList.java
@@ -1,64 +1,76 @@
 package blog.absyn;
 
-
 /**
  * @author leili
  * @date Apr 22, 2012
  * 
  */
 public class ExprList extends Absyn implements Iterable<Expr> {
-	public Expr head;
-	public ExprList next;
+  public Expr head;
+  public ExprList next;
 
-	public ExprList(Expr h, ExprList t) {
-		super();
-		if (h != null) {
-			this.head = h;
-			this.line = h.line;
-			this.col = h.col;
-		}
-		next = t;
-	}
+  public ExprList(Expr h, ExprList t) {
+    super();
+    if (h != null) {
+      this.head = h;
+      this.line = h.line;
+      this.col = h.col;
+    }
+    next = t;
+  }
 
-	@Override
-	public void printTree(Printer pr, int d) {
-		pr.indent(d);
-		pr.say("ExprList(");
-		if (head != null) {
-			pr.sayln("");
-			head.printTree(pr, d + 1);
-			if (next != null) {
-				pr.sayln(",");
-				next.printTree(pr, d + 1);
-			}
-		}
-		pr.say(")");
-	}
+  @Override
+  public void printTree(Printer pr, int d) {
+    pr.indent(d);
+    pr.say("ExprList(");
+    if (head != null) {
+      pr.sayln("");
+      head.printTree(pr, d + 1);
+      if (next != null) {
+        pr.sayln(",");
+        next.printTree(pr, d + 1);
+      }
+    }
+    pr.say(")");
+  }
 
-	/**
-	 * @see StmtList.Iterator
-	 */
-	public class Iterator implements java.util.Iterator<Expr> {
-		ExprList curr=null;
-		public Iterator(ExprList ExprList) { curr = ExprList; }
-		public boolean hasNext() { return curr != null; }
-		public Expr next() {
-			Expr o = curr.head;
-			curr = curr.next;
-			return o;
-		}
-		public void remove() { throw new UnsupportedOperationException(); }
-	}
-	public Iterator iterator() { return new Iterator(this);}
-	
-	/**
-	 * @see StmtList#StmtList(Stmt...)
-	 */
-	public static ExprList ExprList(Expr... xs) {
-		ExprList head = null;
-		for(int i = xs.length-1; i > -1; --i)
-			head = new ExprList(xs[i], head);
-		return head;
-	}
+  /**
+   * @see StmtList.Iterator
+   */
+  public class Iterator implements java.util.Iterator<Expr> {
+    ExprList curr = null;
+
+    public Iterator(ExprList ExprList) {
+      curr = ExprList;
+    }
+
+    public boolean hasNext() {
+      return curr != null;
+    }
+
+    public Expr next() {
+      Expr o = curr.head;
+      curr = curr.next;
+      return o;
+    }
+
+    public void remove() {
+      throw new UnsupportedOperationException();
+    }
+  }
+
+  public Iterator iterator() {
+    return new Iterator(this);
+  }
+
+  /**
+   * @see StmtList#StmtList(Stmt...)
+   */
+  public static ExprList ExprList(Expr... xs) {
+    ExprList head = null;
+    for (int i = xs.length - 1; i > -1; --i)
+      head = new ExprList(xs[i], head);
+    return head;
+  }
 
 }
