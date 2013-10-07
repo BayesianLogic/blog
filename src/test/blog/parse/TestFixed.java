12c304c3c4d494155968ff93851732beac94c9f8
William Cushing <william.cushing@gmail.com>
2013-10-07 12:55:19 -0700

    Test cases for parser pass.
    Semantic checks in other test suits seem to be broken, but that appears
    to be true prior to any changes I made to parsing.

--- src/test/blog/parse/TestFixed.java
+++ src/test/blog/parse/TestFixed.java
@@ -24,23 +24,23 @@ public class TestFixed extends TestParse {
 	public void testFixedConstantDeclaration() {
 		{
 			String parsed = parseToRepr("fixed Real a = 1.0;");
-			String generated = toRepr(							
+			String generated = toRepr(Stmts(					
 					new FixedFuncDec(
 							Symbol("a"), 
 							Type(Symbol("Real")), 
 							Expr(1.0))
-					);
+					));
 			assertEquals(generated, parsed);
 		}
 		reset();
 		{
 			String parsed = parseToRepr("fixed Boolean a = true;");
-			String generated = toRepr(
+			String generated = toRepr(Stmts(
 					new FixedFuncDec(
 						Symbol("a"), 
 						Type(Symbol("Boolean")), 
 						Expr(true))
-					);
+					));
 			assertEquals(generated, parsed);
 		}
 	}
@@ -48,49 +48,53 @@ public class TestFixed extends TestParse {
 	@Test
 	public void testFixedConstantWithExpr() {
 		String parsed = parseToRepr("fixed Real a = 1.0 + b;");
-		String generated = toRepr( 
+		String generated = toRepr(Stmts( 
 				new FixedFuncDec(Symbol("a"), 
 						Type(Symbol("Real")), 
 						Expr(	OpExpr.PLUS, 
 								Expr(1.0),
 								Expr(Symbol("b"))
-								)));
+								))
+				));
 		assertEquals(generated, parsed);
 	}
 
 	@Test
 	public void testBuiltinDistinct_String() {
 		String parsed = parseToRepr("fixed String a = \"hello\";");
-		String generated = toRepr( 
+		String generated = toRepr(Stmts( 
 				new FixedFuncDec(Symbol("a"), 
 						Type(Symbol("String")), 
-						Expr("hello")));
+						Expr("hello"))
+				));
 		assertEquals(generated, parsed);
 	}
 
 	@Test
 	public void testDistinct() {
 		String parsed = parseToRepr("distinct Real a, b;");
-		String generated = toRepr( 
+		String generated = toRepr(Stmts( 
 				new DistinctSymbolDec(
 						Type(Symbol("Real")), 
 						Symbols(Symbol("a"), 
 								Symbol("b")) 
-								));
+								)
+				));
 		assertEquals(generated, parsed);
 	}
 
 	@Test
 	public void testFixedFunction() {
 		String parsed = parseToRepr("fixed Real add(Real a, Real b) = a + b;");
-		String generated = toRepr( 
+		String generated = toRepr(Stmts( 
 				new FixedFuncDec(Symbol("add"), 
 						Fields(	Field(Symbol("a"),Type(Symbol("Real"))),
 								Field(Symbol("b"),Type(Symbol("Real"))) ),
 						Type(Symbol("Real")), 
 						Expr(	OpExpr.PLUS,
 								Expr(Symbol("a")),
-								Expr(Symbol("b")))));
+								Expr(Symbol("b"))))
+				));
 		assertEquals(generated, parsed);
 	}
 
