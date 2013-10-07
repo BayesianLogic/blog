163b43f2a15c8de91ba08a48bf4f5fadf7856b39
William Cushing <william.cushing@gmail.com>
2013-10-06 18:47:22 -0700

    Pulled Field out of FieldList.
    Long-term goal: replace all the List subclasses of Absyn with generic
    containers...the lack of multiple inheritance in Java makes doing that
    rather more awkward than in other languages...

--- src/blog/absyn/FieldList.java
+++ src/blog/absyn/FieldList.java
@@ -1,5 +1,11 @@
 package blog.absyn;
 
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections;
+
+import blog.absyn.StmtList.Iterator;
 import blog.symbol.Symbol;
 
 /**
@@ -7,27 +13,28 @@ import blog.symbol.Symbol;
  * @date Apr 22, 2012
  * 
  */
-public class FieldList extends Absyn {
-	public Symbol var;
-	public Ty typ;
+public class FieldList extends Absyn implements Iterable<Field> {
+	public Field head;
 	public FieldList next;
 
-	public FieldList(Symbol n, Ty t, FieldList x) {
-		super(t.getLine(), t.getCol());
-		var = n;
-		typ = t;
-		next = x;
+	public FieldList(Symbol name, Ty type, FieldList tail) {
+		this(new Field(name,type),tail);
 	}
-
+	public FieldList(Field field, FieldList tail) {
+		super(field.line,field.col);
+		head=field;
+		next=tail;
+	}
+	
 	@Override
 	public void printTree(Printer pr, int d) {
 		pr.indent(d);
 		pr.sayln("FieldList(");
-		typ.printTree(pr, d + 1);
-		if (var != null) {
+		head.typ.printTree(pr, d + 1);
+		if (head.var != null) {
 			pr.sayln(",");
 			pr.indent(d + 1);
-			pr.say(var.toString());
+			pr.say(head.var.toString());
 		}
 		if (next != null) {
 			pr.sayln(",");
@@ -35,4 +42,32 @@ public class FieldList extends Absyn {
 		}
 		pr.say(")");
 	}
+	
+	/**
+	 * @see StmtList.Iterator
+	 */
+	public class Iterator implements java.util.Iterator<Field> {
+		FieldList curr=null;
+		public Iterator(FieldList FieldList) { curr = FieldList; }
+		public boolean hasNext() { return curr != null; }
+		public Field next() {
+			Field o = curr.head;
+			curr = curr.next;
+			return o;
+		}
+		public void remove() { throw new UnsupportedOperationException(); }
+	}
+	public Iterator iterator() { return new Iterator(this);}
+
+	/**
+	 * @see StmtList#StmtList(Stmt...)
+	 */
+	public static FieldList FieldList(Field... xs) {
+		FieldList head = null;
+		for(int i = xs.length-1; i > -1; --i)
+			head = new FieldList(xs[i], head);
+		return head;
+	}
+
+
 }
