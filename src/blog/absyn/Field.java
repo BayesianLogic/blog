16dd32da6f676cbf7dc97a9559bc14b0f6e0e8e9
lileicc <lileicc@gmail.com>
2013-10-07 13:58:53 -0700

    formatting the source

--- src/blog/absyn/Field.java
+++ src/blog/absyn/Field.java
@@ -3,17 +3,21 @@ package blog.absyn;
 import blog.symbol.Symbol;
 
 public class Field extends Absyn {
-	public Symbol var;
-	public Ty typ;
+  public Symbol var;
+  public Ty typ;
 
-	public Field(int line, int col, Symbol var, Ty typ) {
-		super(line, col);
-		this.var = var;
-		this.typ = typ;
-	}
+  public Field(int line, int col, Symbol var, Ty typ) {
+    super(line, col);
+    this.var = var;
+    this.typ = typ;
+  }
 
-	public Field(int p, Symbol var, Ty typ) {this(0,p,var,typ);}
+  public Field(int p, Symbol var, Ty typ) {
+    this(0, p, var, typ);
+  }
 
-	public Field(Symbol var, Ty typ) {this(typ.line,typ.col,var,typ);}
+  public Field(Symbol var, Ty typ) {
+    this(typ.line, typ.col, var, typ);
+  }
 
 }
--- src/blog/absyn/FieldList.java
+++ src/blog/absyn/FieldList.java
@@ -8,61 +8,74 @@ import blog.symbol.Symbol;
  * 
  */
 public class FieldList extends Absyn implements Iterable<Field> {
-	public Field head;
-	public FieldList next;
+  public Field head;
+  public FieldList next;
 
-	public FieldList(Symbol name, Ty type, FieldList tail) {
-		this(new Field(name,type),tail);
-	}
-	public FieldList(Field field, FieldList tail) {
-		super(field.line,field.col);
-		head=field;
-		next=tail;
-	}
+  public FieldList(Symbol name, Ty type, FieldList tail) {
+    this(new Field(name, type), tail);
+  }
 
-	@Override
-	public void printTree(Printer pr, int d) {
-		pr.indent(d);
-		pr.sayln("FieldList(");
-		head.typ.printTree(pr, d + 1);
-		if (head.var != null) {
-			pr.sayln(",");
-			pr.indent(d + 1);
-			pr.say(head.var.toString());
-		}
-		if (next != null) {
-			pr.sayln(",");
-			next.printTree(pr, d + 1);
-		}
-		pr.say(")");
-	}
+  public FieldList(Field field, FieldList tail) {
+    super(field.line, field.col);
+    head = field;
+    next = tail;
+  }
 
-	/**
-	 * @see StmtList.Iterator
-	 */
-	public class Iterator implements java.util.Iterator<Field> {
-		FieldList curr=null;
-		public Iterator(FieldList FieldList) { curr = FieldList; }
-		public boolean hasNext() { return curr != null; }
-		public Field next() {
-			Field o = curr.head;
-			curr = curr.next;
-			return o;
-		}
-		public void remove() { throw new UnsupportedOperationException(); }
-	}
-	public Iterator iterator() { return new Iterator(this);}
+  @Override
+  public void printTree(Printer pr, int d) {
+    pr.indent(d);
+    pr.sayln("FieldList(");
+    head.typ.printTree(pr, d + 1);
+    if (head.var != null) {
+      pr.sayln(",");
+      pr.indent(d + 1);
+      pr.say(head.var.toString());
+    }
+    if (next != null) {
+      pr.sayln(",");
+      next.printTree(pr, d + 1);
+    }
+    pr.say(")");
+  }
 
-	/**
-	 * @see StmtList#StmtList(Stmt...)
-	 */
-	public static FieldList FieldList(Field... xs) {
-		FieldList head = null;
-		for(int i = xs.length-1; i > -1; --i) {
-			head = new FieldList(xs[i], head);
-		}
-		return head;
-	}
+  /**
+   * @see StmtList.Iterator
+   */
+  public class Iterator implements java.util.Iterator<Field> {
+    FieldList curr = null;
 
+    public Iterator(FieldList FieldList) {
+      curr = FieldList;
+    }
+
+    public boolean hasNext() {
+      return curr != null;
+    }
+
+    public Field next() {
+      Field o = curr.head;
+      curr = curr.next;
+      return o;
+    }
+
+    public void remove() {
+      throw new UnsupportedOperationException();
+    }
+  }
+
+  public Iterator iterator() {
+    return new Iterator(this);
+  }
+
+  /**
+   * @see StmtList#StmtList(Stmt...)
+   */
+  public static FieldList FieldList(Field... xs) {
+    FieldList head = null;
+    for (int i = xs.length - 1; i > -1; --i) {
+      head = new FieldList(xs[i], head);
+    }
+    return head;
+  }
 
 }
