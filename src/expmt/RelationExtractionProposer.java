9ba8ba619d9a75552a734d8efa8064ad38292ca5
lileicc <lileicc@gmail.com>
2014-01-07 21:00:13 -0800

    fix the compilation error

--- src/expmt/RelationExtractionProposer.java
+++ src/expmt/RelationExtractionProposer.java
@@ -35,19 +35,16 @@
 
 package expmt;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
 
-import blog.ObjectIdentifier;
-import blog.bn.BasicVar;
-import blog.bn.NumberVar;
-import blog.bn.RandFuncAppVar;
 import blog.bn.BayesNetVar;
-import blog.common.Timer;
-import blog.common.Util;
+import blog.bn.RandFuncAppVar;
 import blog.model.Evidence;
 import blog.model.FunctionSignature;
 import blog.model.Model;
-import blog.model.POP;
 import blog.model.RandomFunction;
 import blog.model.Type;
 import blog.sample.Proposer;
@@ -60,155 +57,165 @@ import blog.world.PartialWorldDiff;
  */
 public class RelationExtractionProposer implements Proposer {
 
-	// Instance variables
-	private Model model;
-	private Evidence evidence;
-	private List queries; // of Query
+  // Instance variables
+  private Model model;
+  private Evidence evidence;
+  private List queries; // of Query
 
-	private Type relType;		// Relation
-	private Type entType;		// Entity
-	private Type factType;		// Fact
-	private Type trigType;		// Trigger
-	private Type sentType;		// Sentence
+  private Type relType; // Relation
+  private Type entType; // Entity
+  private Type factType; // Fact
+  private Type trigType; // Trigger
+  private Type sentType; // Sentence
 
-	//private POP relPOP; // To be implemented in a later version
+  // private POP relPOP; // To be implemented in a later version
 
-	// random variable functions as described by the model
-	private RandomFunction sparsity;
-	private RandomFunction holds;
-	private RandomFunction theta;
-	private RandomFunction sourceFact;
-	private RandomFunction subject;
-	private RandomFunction object;
-	private RandomFunction triggerID;
-	private RandomFunction verb;
+  // random variable functions as described by the model
+  private RandomFunction sparsity;
+  private RandomFunction holds;
+  private RandomFunction theta;
+  private RandomFunction sourceFact;
+  private RandomFunction subject;
+  private RandomFunction object;
+  private RandomFunction triggerID;
+  private RandomFunction verb;
 
+  /**
+   * Creates a new RelationExtractionProposer object for the given model.
+   * 
+   * Set the model, types, and random functions
+   */
+  public RelationExtractionProposer(Model model, Properties properties) {
 
-	/**
-	 * Creates a new RelationExtractionProposer object for the given model.
-	 *
-	 * Set the model, types, and random functions
-	 */
-	public RelationExtractionProposer(Model model, Properties properties) {
-		
-		// Set model
-		this.model = model;
+    // Set model
+    this.model = model;
 
-		// Set types
-		relType = Type.getType("Relation");
-		entType = Type.getType("Entity");
-		factType = Type.getType("Fact");
-		trigType = Type.getType("Trigger");
-		sentType = Type.getType("Sentence");
+    // Set types
+    relType = Type.getType("Relation");
+    entType = Type.getType("Entity");
+    factType = Type.getType("Fact");
+    trigType = Type.getType("Trigger");
+    sentType = Type.getType("Sentence");
 
-		// Set Random Functions
-		sparsity = (RandomFunction) model.getFunction(new FunctionSignature("Sparsity", relType));
-		holds = (RandomFunction) model.getFunction(new FunctionSignature("Holds", factType));
-		theta = (RandomFunction) model.getFunction(new FunctionSignature("Theta", relType));
-		sourceFact = (RandomFunction) model.getFunction(new FunctionSignature("SourceFact", sentType));
-		subject = (RandomFunction) model.getFunction(new FunctionSignature("Subject", sentType));
-		object = (RandomFunction) model.getFunction(new FunctionSignature("Object", sentType));
-		triggerID = (RandomFunction) model.getFunction(new FunctionSignature("TriggerID", sentType));
-		verb = (RandomFunction) model.getFunction(new FunctionSignature("Verb", sentType));
+    // Set Random Functions
+    sparsity = (RandomFunction) model.getFunction(new FunctionSignature(
+        "Sparsity", relType));
+    holds = (RandomFunction) model.getFunction(new FunctionSignature("Holds",
+        factType));
+    theta = (RandomFunction) model.getFunction(new FunctionSignature("Theta",
+        relType));
+    sourceFact = (RandomFunction) model.getFunction(new FunctionSignature(
+        "SourceFact", sentType));
+    subject = (RandomFunction) model.getFunction(new FunctionSignature(
+        "Subject", sentType));
+    object = (RandomFunction) model.getFunction(new FunctionSignature("Object",
+        sentType));
+    triggerID = (RandomFunction) model.getFunction(new FunctionSignature(
+        "TriggerID", sentType));
+    verb = (RandomFunction) model.getFunction(new FunctionSignature("Verb",
+        sentType));
 
-	}
+  }
 
-	/**
-	 * Initialization:
-	 *  - Set all observed variables
-	 *	- Choose SourceFact for all observed sentences uniformly from all facts such that the argument pair matches
-	 *  - Set the Holds(f) variable for this fact to be True
-	 *  - Set Dirichlet parameter (alpha) size to be number of triggers
-	 *  - Set TriggerID for observed sentences such that the observed Verb matches the TriggerID
-	 *  - Sample everything else
-	 */
-	public PartialWorldDiff initialize(Evidence evidence, List queries) {
+  /**
+   * Initialization:
+   * - Set all observed variables
+   * - Choose SourceFact for all observed sentences uniformly from all facts
+   * such that the argument pair matches
+   * - Set the Holds(f) variable for this fact to be True
+   * - Set Dirichlet parameter (alpha) size to be number of triggers
+   * - Set TriggerID for observed sentences such that the observed Verb matches
+   * the TriggerID
+   * - Sample everything else
+   */
+  public PartialWorldDiff initialize(Evidence evidence, List queries) {
 
-		// Set evidence and queries
-		this.evidence = evidence;
-		this.queries = queries;
+    // Set evidence and queries
+    this.evidence = evidence;
+    this.queries = queries;
 
-		PartialWorld underlying = new DefaultPartialWorld(Collections.singleton(relType));
-		PartialWorldDiff world = new PartialWorldDiff(underlying);
+    PartialWorld underlying = new DefaultPartialWorld(
+        Collections.singleton(relType));
+    PartialWorldDiff world = new PartialWorldDiff(underlying);
 
-		// Set observed variables
-		for (Iterator iter = evidence.getEvidenceVars().iterator(); iter.hasNext();) {
-			RandFuncAppVar var = (RandFuncAppVar) iter.next();
-			
-		}
+    // Set observed variables
+    for (Iterator iter = evidence.getEvidenceVars().iterator(); iter.hasNext();) {
+      RandFuncAppVar var = (RandFuncAppVar) iter.next();
 
-	}
-
-	/**
-	 * Proposes a next state for the Markov chain given the current state. The
-	 * world argument is a PartialWorldDiff that the proposer can modify to create
-	 * the proposal; the saved version of this PartialWorldDiff is the state
-	 * before the proposal. Returns the log proposal ratio: log (q(x | x') / q(x'
-	 * | x))
-	 * 
-	 * The details of the proposal has been arranged nicely in a PDF file.
-	 * Please look to that file for details.
-	 */
-	public double proposeNextState(PartialWorldDiff proposedWorld) {
-
-	}
-
-	/**
-	 * Method for performing sourceFact(s) switch
-	 */
-	private double sourceFactSwitch(PartialWorldDiff proposedWorld) {
-
-	}
-
-	/**
-	 * Method for performing Holds(f) switch
-	 */
-	private double holdsSwitch(PartialWorldDiff proposedWorld) {
-
-	}
-
-	/**
-	 * Method for performing sparsity sampling (this is Gibbs)
-	 */
-	private double sparsitySample(PartialWorldDiff proposedWorld) {
-		return 0;
-	}
-
-	/**
-	 * Method for performing theta sampling (this is Gibbs)
-	 */
-	private double thetaSampling(PartialWorldDiff proposedWorld) {
-		return 0;
-	}
-
-
-
-
-	/**
-	 * These methods will not be used, but must be implemented to satisfy the Proposer interface...
-	 */
-
-	public void updateStats(boolean accepted) {
-		// no stats maintained
-	}
-
-	public void printStats() {
-		// no stats to print
-	}
-
-    public PartialWorldDiff reduceToCore(PartialWorld curWorld, BayesNetVar var) {
-        return null;
     }
+    return world;
 
-    public double proposeNextState(PartialWorldDiff proposedWorld, 
-                            BayesNetVar var, int i) {
-        return 0;
-    }
+  }
 
-    public double proposeNextState(PartialWorldDiff proposedWorld, BayesNetVar var) {
-        return 0;
-    }
+  /**
+   * Proposes a next state for the Markov chain given the current state. The
+   * world argument is a PartialWorldDiff that the proposer can modify to create
+   * the proposal; the saved version of this PartialWorldDiff is the state
+   * before the proposal. Returns the log proposal ratio: log (q(x | x') / q(x'
+   * | x))
+   * 
+   * The details of the proposal has been arranged nicely in a PDF file.
+   * Please look to that file for details.
+   */
+  public double proposeNextState(PartialWorldDiff proposedWorld) {
+    return 0;
 
+  }
+
+  /**
+   * Method for performing sourceFact(s) switch
+   */
+  private double sourceFactSwitch(PartialWorldDiff proposedWorld) {
+    return 0;
+
+  }
+
+  /**
+   * Method for performing Holds(f) switch
+   */
+  private double holdsSwitch(PartialWorldDiff proposedWorld) {
+    return 0;
+
+  }
+
+  /**
+   * Method for performing sparsity sampling (this is Gibbs)
+   */
+  private double sparsitySample(PartialWorldDiff proposedWorld) {
+    return 0;
+  }
+
+  /**
+   * Method for performing theta sampling (this is Gibbs)
+   */
+  private double thetaSampling(PartialWorldDiff proposedWorld) {
+    return 0;
+  }
+
+  /**
+   * These methods will not be used, but must be implemented to satisfy the
+   * Proposer interface...
+   */
+
+  public void updateStats(boolean accepted) {
+    // no stats maintained
+  }
+
+  public void printStats() {
+    // no stats to print
+  }
+
+  public PartialWorldDiff reduceToCore(PartialWorld curWorld, BayesNetVar var) {
+    return null;
+  }
+
+  public double proposeNextState(PartialWorldDiff proposedWorld,
+      BayesNetVar var, int i) {
+    return 0;
+  }
+
+  public double proposeNextState(PartialWorldDiff proposedWorld, BayesNetVar var) {
+    return 0;
+  }
 
 }
-
