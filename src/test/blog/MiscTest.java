1327a0669c480b0c423b7d3ee91b3b2f0e520459
Constantin Berzan <cberzan@gmail.com>
2014-01-16 15:50:11 -0800

    add test for logSum() and junit runner script

--- src/test/blog/MiscTest.java
+++ src/test/blog/MiscTest.java
@@ -70,4 +70,34 @@ public class MiscTest extends TestCase {
 				query.getProb(Util.multiset(Util.list(true), Util.list(true),
 						Util.list(true))), 0.1);
 	}
+
+	public void testLogSum() {
+		double got = Util.logSum(-2000, -2000);
+		double expected = -2000 + java.lang.Math.log(2);
+		assertTrue(java.lang.Math.abs(got - expected) < 1e-10);
+
+		got = Util.logSum(-2000, -2010);
+		expected = -1999.99995460110085332417;
+		assertTrue(java.lang.Math.abs(got - expected) < 1e-10);
+
+		got = Util.logSum(-2000, -1000);
+		expected = -1000;
+		assertTrue(java.lang.Math.abs(got - expected) < 1e-10);
+
+		got = Util.logSum(-1000, -2000);
+		expected = -1000;
+		assertTrue(java.lang.Math.abs(got - expected) < 1e-10);
+
+		got = Util.logSum(-1000, Double.NEGATIVE_INFINITY);
+		expected = -1000;
+		assertTrue(java.lang.Math.abs(got - expected) < 1e-10);
+
+		got = Util.logSum(Double.NEGATIVE_INFINITY, -1000);
+		expected = -1000;
+		assertTrue(java.lang.Math.abs(got - expected) < 1e-10);
+
+		got = Util.logSum(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY);
+		expected = Double.NEGATIVE_INFINITY;
+		assertEquals(got, expected);
+	}
 }
