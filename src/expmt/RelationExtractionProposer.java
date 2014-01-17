3c82efc45a45131aee4f8beabe14fecb25839e5a
chrisdxie <chrisdxie@gmail.com>
2014-01-17 15:20:52 -0800

    Finished initialize method for RelationExtractionProposer

--- src/expmt/RelationExtractionProposer.java
+++ src/expmt/RelationExtractionProposer.java
@@ -35,20 +35,34 @@
 
 package expmt;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
+import java.util.Set;
+import java.util.Random;
 
+import blog.BLOGUtil;
 import blog.bn.BayesNetVar;
 import blog.bn.RandFuncAppVar;
 import blog.bn.NumberVar;
+import blog.bn.VarWithDistrib;
+import blog.common.Util;
+import blog.distrib.Beta;
+import blog.distrib.Dirichlet;
 import blog.model.Evidence;
 import blog.model.FunctionSignature;
 import blog.model.Model;
+import blog.model.NonGuaranteedObject;
+import blog.model.OriginFunction;
 import blog.model.RandomFunction;
 import blog.model.Type;
 import blog.model.POP;
+import blog.objgen.ObjectSet;
+import blog.sample.ClassicInstantiatingEvalContext;
 import blog.sample.Proposer;
 import blog.world.DefaultPartialWorld;
 import blog.world.PartialWorld;
@@ -75,6 +89,11 @@ public class RelationExtractionProposer implements Proposer {
   private POP factPOP;
   private POP trigPOP;
   private POP sentPOP;
+  
+  // parameters
+  private Integer alpha;
+  private Integer beta;
+  private Double dir_alpha;
 
   // random variable functions as described by the model
   private RandomFunction sparsityFunc;
@@ -85,6 +104,14 @@ public class RelationExtractionProposer implements Proposer {
   private RandomFunction objectFunc;
   private RandomFunction triggerIDFunc;
   private RandomFunction verbFunc;
+  
+  // Origin functions
+  private OriginFunction relFunc;
+  private OriginFunction arg1Func;
+  private OriginFunction arg2Func;
+  
+  // HashMap for facts
+  private HashMap<String, Object> factMap;
 
   /**
    * Creates a new RelationExtractionProposer object for the given model.
@@ -93,6 +120,7 @@ public class RelationExtractionProposer implements Proposer {
    */
   public RelationExtractionProposer(Model model, Properties properties) {
 
+	  
     // Set model
     this.model = model;
 
@@ -120,25 +148,36 @@ public class RelationExtractionProposer implements Proposer {
         "TriggerID", sentType));
     verbFunc = (RandomFunction) model.getFunction(new FunctionSignature("Verb",
         sentType));
-
+    
+    // Set Origin Functions
+    relFunc = (OriginFunction) model.getFunction(new FunctionSignature("Rel",
+    	factType));
+    arg1Func = (OriginFunction) model.getFunction(new FunctionSignature("Arg1",
+    	factType));
+    arg2Func = (OriginFunction) model.getFunction(new FunctionSignature("Arg2", 
+    	factType));
+    
     // Set POPs
-    relPOP = (POP) relType.getPOPs().iterator().next();
-    entPOP = (POP) entType.getPOPs().iterator().next();
-    //factPOP = (POP) factType.getPOPs().iterator().next(); // Not needed..?
-    trigPOP = (POP) trigType.getPOPs().iterator().next();
-    sentPOP = (POP) sentType.getPOPs().iterator().next();
+    factPOP = (POP) factType.getPOPs().iterator().next();
+    
+    // Set parameter constants
+    alpha = (Integer) model.getConstantValue("alpha");
+    beta = (Integer) model.getConstantValue("beta");
+    dir_alpha = (Double) model.getConstantValue("dir_alpha");
 
   }
 
   /**
    * Initialization:
-   * - Set all observed variables (Subject, Object, Verb, TriggerID)
-   * - Choose SourceFact for all observed sentences uniformly from all facts
-   * such that the argument pair matches
-   * - Set the Holds(f) variable for this fact to be True
-   * - Set TriggerID for observed sentences such that the observed Verb matches
-   * the TriggerID
-   * - Sample everything else
+   * 1) Set all observed variables (Subject, Object, Verb, TriggerID)
+   * 2) Choose SourceFact for all observed sentences uniformly from all facts
+   *     such that the argument pair matches
+   * 3) Set the Holds(f) variable for this fact to be True
+   * 4) Sample everything else
+   *    a) #Fact variables which are set to 1
+   * 	b) Sparsity
+   * 	c) Theta
+   * 	d) Holds
    */
   public PartialWorldDiff initialize(Evidence evidence, List queries) {
 
@@ -146,35 +185,112 @@ public class RelationExtractionProposer implements Proposer {
     this.evidence = evidence;
     this.queries = queries;
 
-    PartialWorld underlying = new DefaultPartialWorld(
-        Collections.singleton(relType));
+    // Instantiate the world
+    PartialWorld underlying = new DefaultPartialWorld(getTypes());
     PartialWorldDiff world = new PartialWorldDiff(underlying);
-
-    // Set number variables: Assume #Triggers, #Sentences, #Relations, #Entities are specified in model
-    NumberVar nRels = new NumberVar(relPOP, Collections.EMPTY_LIST);
-    world.setValue(nRels, relType.getGuaranteedObjects().size());
-    NumberVar nEnts = new NumberVar(entPOP, Collections.EMPTY_LIST);
-    world.setValue(nEnts, entType.getGuaranteedObjects().size());
-    //NumberVar nFacts = new NumberVar(factPOP, Collections.EMPTY_LIST);
-    //world.setValue(nFacts, factType.getGuaranteedObjects());          // Not needed because it's already set..?
-    NumberVar nTrigs = new NumberVar(trigPOP, Collections.EMPTY_LIST);
-    world.setValue(nTrigs, trigType.getGuaranteedObjects().size());
-    NumberVar nSents = new NumberVar(sentPOP, Collections.EMPTY_LIST);
-    world.setValue(nSents, sentType.getGuaranteedObjects().size());
-
-    // Set observed variables
+    
+    // 1) Set observed variables
     for (Iterator iter = evidence.getEvidenceVars().iterator(); iter.hasNext();) {
     	RandFuncAppVar var = (RandFuncAppVar) iter.next();
     	RandomFunction varFunc = var.func();
+    	
     	if (varFunc == verbFunc) {
-    		Object sentence = var.args()[0];
-    		int triggerIndex = trigType.getGuaranteedObjIndex(evidence.getObservedValue(var)); // Assumes trigger Type knows all triggers
+    		Object sentence = var.args()[0]; // Get the sentence s
+    		int triggerIndex = trigType.getGuaranteedObjIndex(evidence.getObservedValue(var)); // Assumes triggers are Distinct
     		RandFuncAppVar trigFuncVar = new RandFuncAppVar(triggerIDFunc, Collections.singletonList(sentence));
     		world.setValue(trigFuncVar, triggerIndex);
-    	} 
+    	}
+    	
     	world.setValue(var, evidence.getObservedValue(var));
     }
-    System.out.println(world.toString());
+
+    // Create hashmap where key is a unique relation/arg pair, value is a fact
+    factMap = new HashMap<String, Object>(); 
+    
+    for (Object rel : relType.getGuaranteedObjects()) {
+    	for (Object arg1 : entType.getGuaranteedObjects()) {
+    		for (Object arg2 : entType.getGuaranteedObjects()) {
+    			
+    			// The key
+    			String key = generateKey(rel, arg1, arg2);
+    			
+    			// Use a NumberVar to grab the fact
+    			Object[] factArgs = {rel, arg1, arg2};
+    		    NumberVar nFacts = new NumberVar(factPOP, factArgs);
+    		    
+    		    // 4a) Instantiate the number variable, by definition of the model
+    		    world.setValue(nFacts, 1);
+    		    
+    			Object value = world.getSatisfiers(nFacts).iterator().next(); // Should only be one fact that satisfies that number statement
+    			
+    			factMap.put(key, value);
+    		}
+    	}
+    }
+    
+    // 2) For each observed sentence, choose SourceFact (basically randomly choose a relation
+    // to go with the argument pair). This assumes sentences are distinct.
+    // 3) Also, set the holds variable for this fact to be true
+    Random rng = new Random();
+    for (Object sentence : sentType.getGuaranteedObjects()) {
+    	
+    	// Choose relation randomly from all relations
+    	int relNum = rng.nextInt(relType.getGuaranteedObjects().size());
+    	Object rel = relType.getGuaranteedObject(relNum);
+    	Object arg1 = world.getValue(new RandFuncAppVar(subjectFunc, Collections.singletonList(sentence)));
+    	Object arg2 = world.getValue(new RandFuncAppVar(objectFunc, Collections.singletonList(sentence)));
+    	
+    	// 2) Set the sourceFact value in the world
+    	Object fact = factMap.get(generateKey(rel, arg1, arg2));
+    	world.setValue(new RandFuncAppVar(sourceFactFunc, Collections.singletonList(sentence)), fact);
+    	    	
+    	// 3) Set the Holds value in the world
+    	world.setValue(new RandFuncAppVar(holdsFunc, Collections.singletonList(fact)), true); // I hope this is correct, and there isn't some class for BLOG booleans
+    }
+    
+    // 4b and c) Sample Sparsity(r) and Theta(r) for each relation r
+    Integer[] params = {alpha, beta};
+    Beta sparsitySampler = new Beta(Arrays.asList(params));
+    Dirichlet thetaSampler = new Dirichlet(trigType.getGuaranteedObjects().size(), dir_alpha); // This needs to change, need to implement a new dirichlet dist
+    for (Object rel : relType.getGuaranteedObjects()) {
+    	
+    	// 4b) Sparsity(r)
+    	RandFuncAppVar sparsity = new RandFuncAppVar(sparsityFunc, Collections.singletonList(rel));
+    	world.setValue(sparsity, sparsitySampler.sampleVal(Collections.EMPTY_LIST, relType));
+    	
+    	// 4c) Theta(r)
+    	RandFuncAppVar theta = new RandFuncAppVar(thetaFunc, Collections.singletonList(rel));
+    	world.setValue(theta, thetaSampler.sampleVal(Collections.EMPTY_LIST, relType)); 
+    	    	
+    }
+    
+    
+    // 4d) Sample all holds(f) values if they haven't been instantiated
+    for (Object fact: factMap.values()) {
+    	
+    	RandFuncAppVar holds = new RandFuncAppVar(holdsFunc, Collections.singletonList(fact));
+    	if (!world.isInstantiated(holds)) {
+    		
+    		Object rel = ((NonGuaranteedObject) fact).getOriginFuncValue(relFunc);
+    		if (rng.nextDouble() < (Double) world.getValue(new RandFuncAppVar(sparsityFunc, Collections.singletonList(rel)))) {
+    			world.setValue(holds, true);
+    		} else {
+    			world.setValue(holds, false);
+    		}	
+    	}
+    }
+    
+    // Debug
+    if (Util.verbose()) {
+	    for (Object fact : factMap.values()) {
+	    	
+	    	Object holds = world.getValue(new RandFuncAppVar(holdsFunc, Collections.singletonList(fact)));
+	    	System.out.println("Fact: " + fact + ", Holds: " + holds);
+	    	
+	    }
+	    System.out.println(world.toString());
+    }
+    
     return world;
 
   }
@@ -249,5 +365,21 @@ public class RelationExtractionProposer implements Proposer {
   public double proposeNextState(PartialWorldDiff proposedWorld, BayesNetVar var) {
     return 0;
   }
+  
+  // A method to create the key specific to the factMap (HashMap)
+  private String generateKey(Object rel, Object arg1, Object arg2) {
+	  return "Rel: " + rel.toString() + ", Arg1: " + arg1.toString() + ", Arg2: " + arg2.toString();
+  }
+  
+  private Set getTypes() {
+      Set idTypes = new HashSet();
+      idTypes.add(relType); 
+      idTypes.add(entType);
+      //idTypes.add(factType); // If commented, Fact objects result in NonGuaranteedObjects
+      idTypes.add(trigType);
+      idTypes.add(sentType);
+      return idTypes;
+  }
+
 
 }
