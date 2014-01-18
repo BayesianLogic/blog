e7c9ace090bb63d08309a8c6d885b992f391e308
Constantin Berzan <cberzan@gmail.com>
2014-01-17 16:22:14 -0800

    example: inferring parameters of a UnivarGaussian
    For 1000 observations, the sample weights are all very small. Before,
    we would get empty distributions as output. With the new log computations,
    this works properly.

---
+++ example/gaussian-params/example_gen.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+"""
+Use this script to generate a model with a given number of samples:
+
+./example_gen.py 20 >example-20.blog
+./example_gen.py 1000 >example-1000.blog
+"""
+
+
+from collections import namedtuple
+from jinja2 import Environment
+from jinja2 import FileSystemLoader
+import numpy as np
+import sys
+
+
+Observation = namedtuple('Observation', ['timestep', 'value'])
+
+
+def generate_model(mean, variance, num_samples):
+    """
+    Return the generated model as a string.
+    """
+    # Observations:
+    observations = []
+    for timestep in xrange(num_samples):
+        value = np.sqrt(variance) * np.random.randn() + mean
+        observations.append(Observation(timestep, value))
+
+    # Generate the model.
+    env = Environment(loader=FileSystemLoader('.'))
+    template = env.get_template('example.blog.template')
+    return template.render(observations=observations)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print >>sys.stderr, \
+            "Usage: {} num_observations >model.blog".format(sys.argv[0])
+        sys.exit(1)
+    num_samples = int(sys.argv[1])
+    mean = 0
+    variance = 5
+    model = generate_model(mean, variance, num_samples)
+    print model
