d576b00f52b2edfa20c5ee741806527b754e5610
Constantin Berzan <cberzan@gmail.com>
2014-01-27 15:57:42 -0800

    draw interactive viz in world coords

--- example/ppaml-quadcopter/automobile_viz.py
+++ example/ppaml-quadcopter/automobile_viz.py
@@ -9,55 +9,74 @@ from automobile_data import INTENSITY_COLS
 from automobile_data import INTENSITY_MAX
 from automobile_data import LASER_COLS
 from automobile_data import LASER_MAX
+from automobile_data import LATITUDE_MIN
+from automobile_data import LATITUDE_MAX
+from automobile_data import LONGITUDE_MIN
+from automobile_data import LONGITUDE_MAX
 from automobile_data import read_data
+from automobile_data import read_metadata
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def draw_readings(ax, readings, index):
+def draw_readings(ax, readings, index, properties, true_obstacles):
     """
     Draw the laser and intensity data at the given index.
 
     If no data is available for that index, look at previous indices.
     """
     ax.clear()
+    ax.set_xlim(LONGITUDE_MIN - 1, LONGITUDE_MAX + 1)
+    ax.set_ylim(LATITUDE_MIN - 1, LATITUDE_MAX + 1)
+    true_obst_xs, true_obst_ys = zip(*true_obstacles)
+    ax.scatter(true_obst_xs, true_obst_ys, c='yellow')
 
     # Find nearest (present or past) indices where data is available:
     assert 0 <= index < len(readings)
+    gps_index = index
+    while gps_index >= 0 and not readings[gps_index].gps_latitude:
+        gps_index -= 1
     laser_index = index
     while laser_index >= 0 and not readings[laser_index].laser:
         laser_index -= 1
     intensity_index = index
     while intensity_index >= 0 and not readings[intensity_index].intensity:
         intensity_index -= 1
-    if laser_index == -1 or intensity_index == -1:
+    if gps_index == -1 or laser_index == -1 or intensity_index == -1:
         ax.set_title("no data")
         plt.draw()
         return
 
-    # Draw laser and intensity data.
-    ax.set_title("index {} ({}), laser {} ({}), intensity {} ({})".format(
-        index, readings[index].time,
-        laser_index, readings[laser_index].time,
-        intensity_index, readings[intensity_index].time))
-    xs = []
-    ys = []
-    cs = []
+    # Draw laser and intensity data based on latest GPS location.
+    ax.set_title(
+        "index {} ({}), gps {} ({}), laser {} ({}), intensity {} ({})"
+        .format(
+            index, readings[index].time,
+            gps_index, readings[gps_index].time,
+            laser_index, readings[laser_index].time,
+            intensity_index, readings[intensity_index].time))
+    agent_x = readings[gps_index].gps_longitude
+    agent_y = readings[gps_index].gps_latitude
+    ax.scatter([agent_x], [agent_y], c='red')
+    obst_xs = []
+    obst_ys = []
+    obst_cs = []
     assert LASER_COLS == INTENSITY_COLS == 361
     for i in xrange(LASER_COLS):
         distance = readings[laser_index].laser[i]
         if np.abs(distance - LASER_MAX) < 1e-6:
             continue  # no obstacles within laser range
         intensity = readings[intensity_index].intensity[i]
-        radians = 0.5 * i * np.pi / 180
-        xs.append(distance * np.cos(radians))
-        ys.append(distance * np.sin(radians))
-        cs.append(intensity / INTENSITY_MAX)
+        radians = (-0.5 * i + 90) * np.pi / 180
+        obst_x = agent_x + distance * np.cos(radians)
+        obst_y = agent_y + distance * np.sin(radians)
+        obst_c = intensity / INTENSITY_MAX
+        obst_xs.append(obst_x)
+        obst_ys.append(obst_y)
+        obst_cs.append(obst_c)
 
-    ax.scatter(xs, ys, c=cs)
-    ax.set_xlim(-5, 5)
-    ax.set_ylim(-1, 10)
+    ax.scatter(obst_xs, obst_ys, c=obst_cs)
     plt.draw()
 
 
@@ -95,11 +114,12 @@ class Zipper(object):
 if __name__ == "__main__":
     data_dir = "./data/automobile/1_straight/data/ground/"
     readings = read_data(data_dir)
+    properties, obstacles = read_metadata(data_dir)
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
     def redraw(index):
-        draw_readings(ax, readings, index)
+        draw_readings(ax, readings, index, properties, obstacles)
 
     zipper = Zipper(0, len(readings) - 1, redraw)
     fig.canvas.mpl_connect('key_press_event', zipper.on_key_press)
