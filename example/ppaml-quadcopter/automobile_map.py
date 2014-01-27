350dd4f17e53916494cf5d26fd5958d8db3c8437
Constantin Berzan <cberzan@gmail.com>
2014-01-27 15:58:28 -0800

    transparency to intuitively show certainty (obstacles seen again and again)

--- example/ppaml-quadcopter/automobile_map.py
+++ example/ppaml-quadcopter/automobile_map.py
@@ -55,7 +55,7 @@ def draw_map(ax, readings, properties, true_obstacles):
     print "Have {} agent locations and {} obstacle locations.".format(
         len(agent_xs), len(obst_xs))
     ax.scatter(agent_xs, agent_ys, c='red', linewidths=0)
-    ax.scatter(obst_xs, obst_ys, c=obst_cs, linewidths=0)
+    ax.scatter(obst_xs, obst_ys, c=obst_cs, linewidths=0, alpha=0.01)
     true_obst_xs, true_obst_ys = zip(*true_obstacles)
     ax.scatter(true_obst_xs, true_obst_ys, c='yellow')
     ax.set_xlim(LONGITUDE_MIN - 1, LONGITUDE_MAX + 1)
