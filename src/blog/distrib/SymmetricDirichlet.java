9c595f217b85aa81fe99040a971cdd4dca8f5965
chrisdxie <chrisdxie@gmail.com>
2014-02-06 01:45:32 -0800

    Created SymmetricDirichlet class

---
+++ src/blog/distrib/SymmetricDirichlet.java
@@ -0,0 +1,17 @@
+package blog.distrib;
+
+import java.util.List;
+
+public class SymmetricDirichlet extends Dirichlet {
+
+  public SymmetricDirichlet(List<Double> params) {
+    super(params);
+    // TODO Auto-generated constructor stub
+  }
+
+  public SymmetricDirichlet(int dimension, double paramVal) {
+    super(dimension, paramVal);
+    // TODO Auto-generated constructor stub
+  }
+
+}
