25b0b53a3ded1c46733d1450d313969ccc74c4a0
Constantin Berzan <cberzan@gmail.com>
2014-03-05 12:02:52 -0800

    port laser eqn solving to java

---
+++ example/ppaml-quadcopter/LaserLogic.java
@@ -0,0 +1,65 @@
+package ppaml_quadcopter;
+
+
+/**
+ * Computes ground-truth laser readings given known pose and obstacles.
+ *
+ * This class is deliberately decoupled from BLOG, so that we can test it
+ * independently. We plug it into BLOG using the LaserInterp class.
+ */
+public class LaserLogic {
+
+  /**
+   * Solve a quadratic equation.
+   *
+   * Returns two solutions in sorted order
+   * Returns two nulls if there are no solutions.
+   */
+  public static Double[] solveQuadraticEquation(double a, double b, double c) {
+    Double[] solutions = new Double[2];
+    double delta = b * b - 4 * a * c;
+    if (delta < 0) {
+      solutions[0] = null;
+      solutions[1] = null;
+    } else {
+      solutions[0] = (-b - Math.sqrt(delta)) / (2 * a);
+      solutions[1] = (-b + Math.sqrt(delta)) / (2 * a);
+    }
+    return solutions;
+  }
+
+  /**
+   * Return ground-truth laser readings given pose and obstacle.
+   *
+   * Laser sensor is at laserX, laserY, with orientation laserTheta.
+   * Laser sensor shines a laser ray at each angle in laserAngles.
+   * If the laser ray encounters no obstacle, its reading is laserMaxRange.
+   *
+   * Obstacle is a cylinder at obstacleX, obstacleY, of radius obstacleR.
+   */
+  public static double[] readingsForObstacle(
+          double laserX, double laserY, double laserTheta,
+          double[] laserAngles, double laserMaxRange,
+          double obstacleX, double obstacleY, double obstacleR) {
+    double[] readings = new double[laserAngles.length];
+    for (int i = 0; i < laserAngles.length; i++) {
+      final double angle = laserAngles[i];
+      final double dx = laserX - obstacleX;
+      final double dy = laserY - obstacleY;
+      double a = 1;
+      double b = ((2 * dx * Math.cos(laserTheta + angle)) +
+                  (2 * dy * Math.sin(laserTheta + angle)));
+      double c = dx * dx + dy * dy - obstacleR * obstacleR;
+      Double[] solutions = solveQuadraticEquation(a, b, c);
+      if (solutions[0] == null || solutions[1] < 0) {
+        // Does not intersect ray.
+        readings[i] = laserMaxRange;
+      } else if (solutions[0] < 0) {
+        readings[i] = solutions[1];
+      } else {
+        readings[i] = solutions[0];
+      }
+    }
+    return readings;
+  }
+};
---
+++ example/ppaml-quadcopter/TestLaserLogic.java
@@ -0,0 +1,50 @@
+package ppaml_quadcopter;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import ppaml_quadcopter.LaserLogic;
+
+
+@RunWith(JUnit4.class)
+public class TestLaserLogic {
+
+  @Test
+  public void testSolveQuadraticEquation() {
+    Double[] solutions = LaserLogic.solveQuadraticEquation(123, 45, 67);
+    assertEquals(solutions[0], null);
+    assertEquals(solutions[1], null);
+
+    solutions = LaserLogic.solveQuadraticEquation(123, 45, -67);
+    assertEquals(solutions[0], -0.94330678167407689, 1e-10);
+    assertEquals(solutions[1], 0.57745312313749153, 1e-10);
+  }
+
+  @Test
+  public void testReadingsForObstacle() {
+    double laserX = 2;
+    double laserY = 3;
+    double laserTheta = 0.3;
+    double[] laserAngles = new double[]{
+      0.1, 0.3, 0.5, 0.7, 0.9, 1.1, 1.3, 1.5, 1.7};
+    double laserMaxRange = 10;
+    double obstacleX = 7;
+    double obstacleY = 9;
+    double obstacleR = 2;
+    double[] referenceReadings = new double[]{
+      10, 10, 5.8777472, 5.99884367, 10, 10, 10, 10, 10};
+    double readings[] = LaserLogic.readingsForObstacle(
+      laserX, laserY, laserTheta,
+      laserAngles, laserMaxRange,
+      obstacleX, obstacleY, obstacleR);
+    assertEquals(laserAngles.length, referenceReadings.length);
+    assertEquals(readings.length, referenceReadings.length);
+    for (int i = 0; i < laserAngles.length; i++) {
+      assertEquals(readings[i], referenceReadings[i], 1e-7);
+    }
+  }
+}
