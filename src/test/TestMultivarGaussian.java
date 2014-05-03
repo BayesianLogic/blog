905b714cc8f431d09fa6a27f8a5525f7372586b3
Constantin Berzan <cberzan@gmail.com>
2014-05-02 17:18:35 -0700

    convert test/TestMultivarGaussian.java to JUnit test and make it pass

--- src/test/TestMultivarGaussian.java
+++ src/test/TestMultivarGaussian.java
@@ -1,38 +1,39 @@
 package test;
 
+import Jama.Matrix;
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
 import blog.common.Util;
 import blog.common.numerical.JamaMatrixLib;
 import blog.common.numerical.MatrixLib;
 import blog.distrib.MultivarGaussian;
-import Jama.Matrix;
+
 
 /**
- * a small program to generate samples from Multivariate Gaussian Distribution.
- * 
- * @author leili
- * 
+ * Unit tests for MultivarGaussian.
  */
-
+@RunWith(JUnit4.class)
 public class TestMultivarGaussian {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Util.initRandom(true);
-		
-		double[][] zeroTmp = new double[5][1];
-		MatrixLib zero = new JamaMatrixLib(zeroTmp);
-		
-		double[][] identityTmp = new double[5][5];
-		for (int i = 0; i < 5; i++) {
-			identityTmp[i][i] = 1;
-		}
-		MatrixLib unit = new JamaMatrixLib(identityTmp);
+  @Test
+  public void testSample() {
+    Util.initRandom(true);
 
-		MultivarGaussian mvg = new MultivarGaussian(zero, unit);
+    double[][] zeroTmp = new double[5][1];
+    MatrixLib zero = new JamaMatrixLib(zeroTmp);
 
-		MatrixLib b = mvg.sampleVal();
-		System.out.println(b.toString());
-	}
+    double[][] identityTmp = new double[5][5];
+    for (int i = 0; i < 5; i++) {
+      identityTmp[i][i] = 1;
+    }
+    MatrixLib unit = new JamaMatrixLib(identityTmp);
+
+    MultivarGaussian mvg = new MultivarGaussian(zero, unit);
+
+    MatrixLib b = mvg.sampleVal();
+    // System.out.println(b.toString());
+  }
 }
