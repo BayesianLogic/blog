5882555ec07ff392ec20e3841e3a94798ee3124d
lileicc <lileicc@gmail.com>
2014-05-08 21:01:18 -0700

    update the python lexer \n1. adding new distributions;\n2. adding the buildin function;\n3. fix the rule for function name(now allowing underscore and interleaving numbers)

--- tools/blog_py_lexer/blog/lexer.py
+++ tools/blog_py_lexer/blog/lexer.py
@@ -8,7 +8,11 @@ class BlogLexer(RegexLexer):
   operators = ['\\-\\>','=','~',':', '\\+', '\\-', '\\*', '/', '\\[', ']', 
          '\\{', '}', '!', '\\<', '\\>', '\\<=', '\\>=', '==', '!=', 
          '&', '\\|', '=\\>', '#', '\\^', '%', '@']
-  wordops = ['Prev', 'IsEmptyString']
+  wordops = ['Prev', 'IsEmptyString', 'Succ', 'Pred',
+             'Prev', 'inv', 'det', 'min', 'max', 
+             'round', 'transpose', 'sin', 'cos', 'tan',
+             'atan2', 'sum', 'vstack', 'eye', 'zeros', 
+             'ones', 'toInt', 'toReal']
   deliminators = [',', ';', '\\(', '\\)']
   keywords = ['extern','import','fixed','distinct','random','origin',
         'param','type', 'forall', 'exists', 'obs', 'query', 
@@ -18,11 +22,11 @@ class BlogLexer(RegexLexer):
              'UniformChoice', 'MultivarGaussian', 'Poisson',
              'Bernoulli', 'BooleanDistrib', 'Binomial', 'Beta', 'BoundedGenometric',
              'Categorical', 'Dirichlet', 'EqualsCPD', 'Gamma', 'Geometric', 'Iota',
-             'LinearGaussian', 'MixtureDistrib', 'NegativeBinamial', 
-             'RoundedLogNormal', 'TabularInterp',
+             'LinearGaussian', 'MixtureDistrib', 'Multinomial',
+             'NegativeBinamial', 'RoundedLogNormal', 'TabularInterp',
              'UniformVector', 'UnivarGaussian', 
              'Exponential', 'UniformInt', 'UniformReal']
-  idname_reg = '[a-zA-Z_]\w*'
+  idname_reg = '[a-zA-Z_]\\w*'
 
   def gen_regex(ops):
     return "|".join(ops)
@@ -31,14 +35,14 @@ class BlogLexer(RegexLexer):
     'root' : [
       (r'//.*?\n', Comment.Single),
       (r'(?s)/\*.*?\*/', Comment.Multiline),
-      (r'('+idname_reg+')(\()', bygroups(Name.Function, Punctuation)),
-      (r'('+gen_regex(types)+')\\b', Keyword.Type),
-      (r'('+gen_regex(distribs)+')\\b', Name.Class),
-      (r'('+gen_regex(keywords)+')\\b', Keyword),
+      ('('+idname_reg+')(\\()', bygroups(Name.Function, Punctuation)),
+      ('('+gen_regex(types)+')\\b', Keyword.Type),
+      ('('+gen_regex(distribs)+')\\b', Name.Class),
+      ('('+gen_regex(keywords)+')\\b', Keyword),
       (gen_regex(operators), Operator),
-      (r'(' + gen_regex(wordops) +')\\b', Operator.Word),
-      (r'(true|false|null)\b', Keyword.Constant),
-      (r'('+idname_reg+')\b', Name),
+      ('(' + gen_regex(wordops) +')\\b', Operator.Word),
+      ('(true|false|null)\\b', Keyword.Constant),
+      ('('+idname_reg+')\\b', Name),
       (r'"(\\\\|\\"|[^"])*"', String),
       (gen_regex(deliminators), Punctuation),
       (r'\d*\.\d+', Number.Float),
@@ -51,7 +55,7 @@ def run_tests():
   tests = [
     "type Person;",
     "distinct Person Alice, Bob, P[100];",
-    "random Real x ~ Gaussian(0, 1);\nrandom Real y ~ Gaussian(x, 1);",
+    "random Real x1_x2x3 ~ Gaussian(0, 1);\nrandom Real y ~ Gaussian(x, 1);",
     "random type0 funcname(type1 x) =expression;\nrandom type0 funcname(type1 x) dependency-expression;",
     "random NaturalNum x ~ Poisson(a);",
     "param Real a: 0 < a & a < 10 ;"
