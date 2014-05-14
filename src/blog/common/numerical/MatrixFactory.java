3011ec0d33eae80c16817bd4b96af77a7d0ef691
lileicc <lileicc@gmail.com>
2014-05-13 17:45:42 -0700

    make sure the format is correct. Tab should be turned into spaces.

--- src/blog/common/numerical/MatrixFactory.java
+++ src/blog/common/numerical/MatrixFactory.java
@@ -1,11 +1,9 @@
 package blog.common.numerical;
 
-import blog.common.numerical.JamaMatrixLib;
-import blog.common.numerical.MatrixLib;
 
 /**
  * Creates MatrixLib objects.
- *
+ * 
  * All code should use this class to create new MatrixLib objects, instead
  * of importing JamaMatrixLib directly. This allows us to easily change the
  * underlying implementation in the future.
