2a86edd61b13a3902a4ea289793bf5c47394e341
Yusuf Erol <yberol@Yusuf-Erols-MacBook-Pro.local>
2014-05-20 00:08:46 -0700

    new bird model files

---
+++ example/bird_concise/Birds1000On10by10/LocToInteger.m
@@ -0,0 +1,19 @@
+fid = fopen('LocToInteger.blog','w');
+
+fprintf(fid,'// defining the locations \n');
+fprintf(fid,'type Location; \n');
+fprintf(fid,'distinct Location l[%d]; \n',GridSize);
+fprintf(fid,'// the map from loc to integer \n');
+fprintf(fid,'random Integer loc_to_int(Location loc) { \n');
+for i=1:GridSize;
+   if(i==1)
+       fprintf(fid,'    if loc == l[%d] then = %d \n',i-1,i-1);
+   elseif(i<=GridSize)
+       fprintf(fid,'    else if loc == l[%d] then = %d \n',i-1,i-1);
+   else
+       fprintf(fid,'    else = -1\n');
+   end
+    
+end
+fprintf(fid,'};\n');
+fclose(fid);
