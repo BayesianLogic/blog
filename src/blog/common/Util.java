6c70b619b978f93077568810060ebfaef09c7324
Constantin Berzan <cberzan@gmail.com>
2014-05-20 18:31:41 -0700

    remove instrumentation

--- src/blog/common/Util.java
+++ src/blog/common/Util.java
@@ -2006,6 +2006,4 @@ public class Util {
 	private static Random rand;
 	private static boolean verbose = false;
 	private static boolean print = false;
-
-  public static boolean braindump = false;
 }
