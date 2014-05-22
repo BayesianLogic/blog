e45c2889f8d8f781815e627f972e69d69e7b97e8
cgioia <cgioia@cgioia-VirtualBox.(none)>
2014-05-21 18:43:37 -0700

    test case for mvg; case 1 and 2

--- src/test/TestMultivarGaussian.java
+++ src/test/TestMultivarGaussian.java
@@ -2,14 +2,15 @@ package test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import blog.common.Util;
 import blog.common.numerical.MatrixFactory;
 import blog.common.numerical.MatrixLib;
 import blog.distrib.MultivarGaussian;
@@ -19,46 +20,58 @@ import blog.distrib.MultivarGaussian;
  */
 @RunWith(JUnit4.class)
 public class TestMultivarGaussian {
-
-  @Test
-  public void setUp() {
-
-  }
-
-  @Test
-  public void testSample() {
-    Util.initRandom(true);
-
-    MatrixLib mean = MatrixFactory.zeros(5, 1);
-    MatrixLib sigma = MatrixFactory.eye(5);
-    MultivarGaussian mvg = new MultivarGaussian(mean, sigma);
-
-    MatrixLib b = mvg.sampleVal();
-    // System.out.println(b.toString());
-  }
+  private final double ERROR_BOUND = 1e-10;
+  private MatrixLib mean;
+  private MatrixLib variance;
+  private HashMap<MatrixLib, Double> probVals;
 
   @Test
   public void testGetProb() {
-    // Case 1 -- Mean and Variance are fixed
-    List constructParams = new LinkedList();
-    MatrixLib mean = MatrixFactory.zeros(2, 1);
-    mean.setElement(1, 0, 1);
-    mean.setElement(0, 0, 3);
+    // Assign the mean and variance
+    mean = MatrixFactory.zeros(2, 1);
+    mean.setElement(0, 0, 1);
+    mean.setElement(1, 0, 3);
 
-    MatrixLib variance = MatrixFactory.eye(2);
+    variance = MatrixFactory.eye(2);
     variance.setElement(0, 0, 4.25);
     variance.setElement(0, 1, 3.1);
     variance.setElement(1, 0, 3.1);
     variance.setElement(1, 1, 9.64);
 
-    List params = new LinkedList();
-    params.add(mean);
-    params.add(variance);
-    MultivarGaussian mvg = new MultivarGaussian(params);
+    probVals = new HashMap<MatrixLib, Double>();
+    probVals.put(MatrixFactory.fromArray(new double[][] { { 0 }, { 0 } }),
+        0.017816329802283811);
+    probVals.put(MatrixFactory.fromArray(new double[][] { { 0.5 }, { 0.7 } }),
+        0.021411078817775459);
+    probVals.put(MatrixFactory.fromArray(new double[][] { { 1.0 }, { 3.0 } }),
+        0.028420525552124164);
+    probVals.put(MatrixFactory.fromArray(new double[][] { { 5.0 }, { 5.0 } }),
+        0.0040865387874093571);
+
+    // Case 1 -- Mean and Variance are fixed
+    List constructParams = new LinkedList();
+    List args = new LinkedList();
+    constructParams.add(mean);
+    constructParams.add(variance);
+    MultivarGaussian mvg = new MultivarGaussian(constructParams);
+    testGaussian(mvg, args);
 
     // Case 2 -- Mean varies, Variance fixed
+    constructParams = new LinkedList();
+    constructParams.add(variance);
+    args = new LinkedList();
+    args.add(mean);
+    mvg = new MultivarGaussian(constructParams);
+    testGaussian(mvg, args);
 
     // Case 3 -- Mean varies, Variance varies
+  }
+
+  public void testGaussian(MultivarGaussian mvg, List args) {
+    Set<MatrixLib> points = probVals.keySet();
+    for (MatrixLib point : points) {
+      assertEquals(probVals.get(point), mvg.getProb(args, point), ERROR_BOUND);
+    }
   }
 
   /**
