5c777fcc5418763f23d206496801c88a01f1a5d1
Yusuf Erol <yberol@dhcp-45-87.eecs.berkeley.edu>
2014-05-23 13:26:52 -0700

    minor changes, adding a json output for cberzan to work with

--- example/ppaml-birdmigration/Birds1000_On10by10Grid/matlab/bird_queries.m
+++ example/ppaml-birdmigration/Birds1000_On10by10Grid/matlab/bird_queries.m
@@ -11,14 +11,18 @@ fid = fopen('../bird_queries.blog','w');
 % fprintf(fid,'\n');
 for t=1:T;
     for i=1:GridSize;
-        fprintf(fid,'query birds(l[%d],@%d);',i-1,t-1);
+%         fprintf(fid,'query birds(l[%d],@%d);',i-1,t-1);
+%         fprintf(fid,'\n');
+        
+        fprintf(fid,'query outflow_vector(l[%d],@%d);',i-1,t-1);
         fprintf(fid,'\n');
     end
 end
 
-fprintf(fid,'query beta1;\n');
-fprintf(fid,'query beta2;\n');
-fprintf(fid,'query beta3;\n');
-fprintf(fid,'query beta4;\n');
+% fprintf(fid,'query beta1;\n');
+% fprintf(fid,'query beta2;\n');
+% fprintf(fid,'query beta3;\n');
+% fprintf(fid,'query beta4;\n');
+fprintf(fid,'query beta(@60);\n');
 
 fclose(fid);
