116889aa2068a0ddfcf2c350b336c36edefe8f2a
Constantin Berzan <cberzan@gmail.com>
2014-05-22 17:33:28 -0700

    found the problem: car-laser.blog only 1-2 particles survive in each timestep

--- src/blog/engine/ParticleFilter.java
+++ src/blog/engine/ParticleFilter.java
@@ -207,6 +207,8 @@ public class ParticleFilter extends InferenceEngine {
         }
       }
 
+      System.out.println("after processing timestep " + timestepIndex);
+
       if (timestepIndex >= 0) {
         List<Query> currentQueries = slicedQueries.get(Timestep
             .at(timestepIndex));
@@ -355,14 +357,17 @@ public class ParticleFilter extends InferenceEngine {
       normalizedWeights[i] = Math.exp(logWeights[i] - logSumWeights);
     }
 
+    int survived = 0;
     for (int i = 0; i < numParticles; i++) {
       int selection = Util.sampleWithProbs(normalizedWeights);
       if (!alreadySampled[selection]) {
         newParticles.add(particles.get(selection));
         alreadySampled[selection] = true;
+        survived++;
       } else
         newParticles.add(((Particle) particles.get(selection)).copy());
     }
+    System.out.println(survived + " particles survived");
 
     particles = newParticles;
   }
