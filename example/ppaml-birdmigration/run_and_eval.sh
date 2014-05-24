927a2774b60cccc145048484fdcabdfd03052b5b
Yusuf Erol <yberol@dhcp-45-87.eecs.berkeley.edu>
2014-05-23 19:47:48 -0700

    added evaluation scripts

---
+++ example/ppaml-birdmigration/run_and_eval.sh
@@ -0,0 +1,28 @@
+mkdir cps
+mkdir eval_results
+
+cd ./cps
+mkdir dataset1
+mkdir dataset2
+mkdir dataset3
+
+cd ./Birds1_On4by4Grid/
+#./run_bird.sh
+python convert_output.py out.json estimation
+
+cd ..
+
+cd ./Birds1000_On10by10Grid/
+#./run_bird.sh
+python convert_output.py out.json estimation
+python convert_output.py out.json reconstruction
+python convert_output.py out.json prediction
+
+cd ./Birds1M_On10by10Grid/
+#./run_bird.sh
+python convert_output.py out.json estimation
+python convert_output.py out.json reconstruction
+python convert_output.py out.json prediction
+
+
+
