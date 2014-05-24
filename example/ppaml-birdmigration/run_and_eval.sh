92ded35501ecb0dca66a029010ef1ea8b2ed6585
Yusuf Erol <yberol@dhcp-45-87.eecs.berkeley.edu>
2014-05-23 20:02:56 -0700

    minor

--- example/ppaml-birdmigration/run_and_eval.sh
+++ example/ppaml-birdmigration/run_and_eval.sh
@@ -9,19 +9,19 @@ mkdir dataset3
 cd ..
 
 cd ./Birds1_On4by4Grid/
-#./run_bird.sh
+./run_bird.sh
 python convert_output.py out.json estimation
 
 cd ..
 
 cd ./Birds1000_On10by10Grid/
-#./run_bird.sh
+./run_bird.sh
 python convert_output.py out.json estimation
 python convert_output.py out.json reconstruction
 python convert_output.py out.json prediction
 
 cd ./Birds1M_On10by10Grid/
-#./run_bird.sh
+./run_bird.sh
 python convert_output.py out.json estimation
 python convert_output.py out.json reconstruction
 python convert_output.py out.json prediction
