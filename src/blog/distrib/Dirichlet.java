d73cc9a6b6b476548b7d982b95fefe1ede64f7ad
Constantin Berzan <cberzan@gmail.com>
2014-05-28 19:08:14 -0700

    re-add old distributions in distrib

---
+++ src/blog/distrib/Dirichlet.java
@@ -0,0 +1,208 @@
+/*
+ * Copyright (c) 2012, Regents of the University of California
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in
+ *   the documentation and/or other materials provided with the
+ *   distribution.  
+ *
+ * * Neither the name of the University of California, Berkeley nor
+ *   the names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior 
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package blog.distrib;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import blog.common.numerical.MatrixFactory;
+import blog.common.numerical.MatrixLib;
+
+/**
+ * A Dirichlet distribution with shape parameter vector a, defined by
+ * f(x1, x2, ... xn) = 1/Z * (x1^(a1-1) * x2^(a2-1) * ... * xn^(an-1),
+ * where \sum(xi) = 1 and Z = \prod(Gamma(ai)) / Gamma(\sum(ai)).
+ * 
+ * Note that this is a generalization of the Beta distribution: while
+ * the Beta generates a parameter for the Bernoulli process (Bernoulli
+ * and Binomial distributions), the Dirichlet generates parameters for
+ * the Categorical process (Categorical and Multinomial distributions).
+ */
+public class Dirichlet extends AbstractCondProbDistrib {
+	
+	private Double[] alpha;
+	private Gamma[] gammas;
+	
+	/**
+	 * Constructs a Dirichlet distribution with the given parameters.
+	 * 
+	 * @param a list of parameters for the distribution
+	 */
+	public Dirichlet(List<Double> params) {
+		alpha = new Double[params.size()];
+		params.toArray(alpha);
+		initGammas();
+	}
+	
+	/**
+	 * Constructs a Dirichlet distribution with the given dimension and
+	 * parameter value for all dimensions
+	 * 
+	 * @param the dimension of the distribution
+	 * @param the value for all parameters of this distribution
+	 */
+	public Dirichlet(int dimension, double paramVal) {
+		alpha = new Double[dimension];
+		Arrays.fill(alpha, paramVal);
+		initGammas();
+	}
+	
+	/**
+	 * Helper for constructor to construct needed gamma functions
+	 */
+	private void initGammas() {
+		int numParams = alpha.length;
+		gammas = new Gamma[numParams];
+		for (int i = 0; i < numParams; i++) {
+			gammas[i] = new Gamma(alpha[i], 1);
+		}
+	}
+	
+	/**
+	 * Helper for sampleVal() to reconstruct gamma functions
+	 */
+	private void reconstructGammas(int vec_size) {
+		int numParams = vec_size;
+		gammas = new Gamma[numParams];
+		for (int i = 0; i < numParams; i++) {
+			gammas[i] = new Gamma(alpha[0], 1);
+		}
+	}
+
+
+	/**
+	 * Returns the probability of a vector of values from this distribution.
+	 */
+	public double getProb(List args, Object childValue) {
+		if (!(childValue instanceof MatrixLib)) {
+			throw new IllegalArgumentException("Dirichlet distribution" +
+					"requires MatrixLib as argument, not " + childValue.getClass() + ".");
+		}
+		
+		MatrixLib mat = (MatrixLib)childValue;
+		if (mat.numRows() != 1 || mat.numCols() == 0) {
+			throw new IllegalArgumentException("Dirichlet distribution" +
+					"requires nonempty vector of Numbers as argument.");
+		}
+		
+		double prob = 1.0;
+		for (int i = 0; i < mat.numCols(); i++) {
+			double count_term = (args.size() != 0) ? alpha[0] : alpha[i];
+			double x = mat.elementAt(0, i);
+			prob *= Math.pow(x, count_term - 1);
+		}
+		prob /= normalize(alpha);
+		
+		return prob;
+	}
+	
+	/**
+	 * Returns the log of the probability of a vector of values from this distribution.
+	 */
+	public double getLogProb(List args, Object childValue) {
+		if (!(childValue instanceof MatrixLib)) {
+			throw new IllegalArgumentException("Dirichlet distribution" +
+					"requires MatrixLib as argument, not " + childValue.getClass() + ".");
+		}
+		
+		MatrixLib mat = (MatrixLib)childValue;
+		if (mat.numRows() != 1 || mat.numCols() == 0) {
+			throw new IllegalArgumentException("Dirichlet distribution" +
+					"requires nonempty vector of Numbers as argument.");
+		}
+		
+		double prob = 0.0;
+		for (int i = 0; i < mat.numCols(); i++) {
+			double count_term = (args.size() != 0) ? alpha[0] : alpha[i];
+			double x = mat.elementAt(0, i);
+			prob += Math.log(x) * (count_term - 1);
+		}
+		prob -= Math.log(normalize(alpha));
+		
+		return prob;
+	}
+
+	/**
+	 * Returns a list of doubles sampled from this distribution.
+	 */
+	public Object sampleVal(List args) {
+		double sum = 0.0;
+		int vec_size = alpha.length;
+		if (args.size() != 0) {
+			vec_size = (Integer) args.get(0);
+			reconstructGammas(vec_size);
+		}
+		
+		double[][] samples = new double[1][vec_size];
+		List<Object> dummy = new ArrayList<Object>();
+		for (int i = 0; i < vec_size; i++) {
+			Gamma component = gammas[i];
+			double sample = (Double) component.sampleVal(dummy);
+			sum += sample;
+			samples[0][i] = sample;
+		}
+		
+		for (int i = 0; i < vec_size; i++) {
+			samples[0][i] /= sum;
+		}
+		return MatrixFactory.fromArray(samples);
+	}
+	
+	/**
+	 * Computes the normalization constant for a Dirichlet distribution with
+	 * the given parameters.
+	 * 
+	 * @param a list of parameters of a Dirichlet distribution
+	 * @return the normalization constant for such a distribution
+	 */
+	public static final double normalize(Double[] params) {
+		double denom = 0.0;
+		double numer = 1.0;
+		
+		for (double param: params) {
+			numer *= Gamma.gamma(param);
+			denom += param;
+		}
+		denom = Gamma.gamma(denom);
+		
+		return numer / denom;
+	}
+	
+	public String toString() {
+		return getClass().getName();
+	}
+}
---
+++ src/blog/distrib/TabularCPD.java
@@ -0,0 +1,182 @@
+/*
+ * Copyright (c) 2005, 2006, Regents of the University of California
+ *
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in
+ *   the documentation and/or other materials provided with the
+ *   distribution.  
+ *
+ * * Neither the name of the University of California, Berkeley nor
+ *   the names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior 
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package blog.distrib;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import blog.model.ArgSpec;
+import blog.model.BuiltInFunctions;
+import blog.model.BuiltInTypes;
+import blog.model.Clause;
+import blog.model.EnumeratedObject;
+import blog.model.FuncAppTerm;
+import blog.model.ListSpec;
+import blog.model.Model;
+import blog.model.SymbolTerm;
+
+/**
+ * @author rbharath
+ * 
+ *         CPD which maps objects to distributions. The CPD can have any number
+ *         of arguments, which are the parents. The Distributions must all be
+ *         over the desired child type and must not accept any arguments (that
+ *         is,
+ *         they cannot be conditionally defined). The child type can also be
+ *         Integer or NaturalNum, in which case the distribution is over some
+ *         prefix of the natural numbers.
+ * 
+ *         <p>
+ *         Internally, this cpd maintains a map which sends ArgSpec Objects to
+ *         Distributions. Currently, ArgSpec objects represent single parent
+ *         values. Once a Tuple class is added, ArgSpecs may represent more
+ *         parent values The ordering of the parents in the arguments must match
+ *         the order in which the parents were specified in the TabularCPD
+ *         declaration.
+ * 
+ *         <p>
+ *         Currently, all values of the parents must be specified in the map.
+ *         This should change in the future.
+ * 
+ *         <p>
+ *         When one or more the parent values are <code>Model.NULL</code>, the
+ *         child distribution is also concentrated on <code>Model.NULL</code>
+ */
+public class TabularCPD extends AbstractCondProbDistrib {
+  /**
+   * Creates a new tabular CPD from the given parameter list.
+   * Params should be of length one, containing only one element of MapSpec.
+   * This map will be stored and used internally.
+   */
+  public TabularCPD(List params) {
+    if (params.isEmpty()) {
+      throw new IllegalArgumentException("Tabular CPD must have "
+          + "at least one distribution.");
+    }
+
+    if (params.size() == 1) {
+      Object param = params.get(0);
+      if (param instanceof Map) {
+        this.map = (Map<ArgSpec, Object>) param;
+      }
+    } else {
+      throw new IllegalArgumentException("TabularCPD may only be "
+          + "specified with a map");
+    }
+
+  }
+
+  public double getProb(List args, Object value) {
+    if (args.contains(Model.NULL)) {
+      return ((value == Model.NULL) ? 1 : 0);
+    }
+
+    ArgSpec parents = getArgSpec(args);
+    Clause c = (Clause) this.map.get(parents);
+    CondProbDistrib distrib = (CondProbDistrib) c.getCPD();
+    if (distrib == null) {
+      throw new IllegalArgumentException("TabularCPD args don't "
+          + "specify a distribution");
+    }
+
+    return distrib.getProb(value);
+  }
+
+  public Object sampleVal(List args) {
+    if (args.contains(Model.NULL)) {
+      return Model.NULL;
+    }
+
+    ArgSpec parents = getArgSpec(args);
+    Clause c = (Clause) this.map.get(parents);
+    CondProbDistrib distrib = c.getCPD();
+    if (distrib == null) {
+      throw new IllegalArgumentException("TabularCPD args don't "
+          + "specify a distribution");
+    }
+
+    return distrib.sampleVal();
+  }
+
+  // TODO: Implement this correctly
+  public String toString() {
+    StringBuffer buffer = new StringBuffer("[");
+    for (ArgSpec as : this.map.keySet()) {
+      buffer.append(as);
+    }
+    buffer.append("]");
+    return buffer.toString();
+  }
+
+  private ArgSpec getArgSpec(List params) {
+    if (params.size() == 1) {
+      Object o = params.get(0);
+      if (o instanceof EnumeratedObject) {
+        EnumeratedObject e = (EnumeratedObject) o;
+        return new SymbolTerm(e.getName());
+      }
+      // TODO: add more types as they are required by getArgSpec,
+      // or find a better way to handle built-in types
+      else if (o instanceof Boolean) {
+        Boolean b = (Boolean) o;
+        return new FuncAppTerm(BuiltInFunctions.getLiteral(String.valueOf(b),
+            BuiltInTypes.BOOLEAN, b));
+      } else if (o instanceof List) {
+        List<Object> arg = (List) o;
+        List<ArgSpec> terms = new ArrayList<ArgSpec>();
+        for (Object obj : arg) {
+          List<Object> wrapper = new LinkedList<Object>();
+          wrapper.add(obj);
+          terms.add(this.getArgSpec(wrapper));
+        }
+
+        ListSpec multi = new ListSpec(terms);
+        return multi;
+      } else {
+        throw new IllegalArgumentException("TabularCPD: parameters to "
+            + "sampleVal must be EnumeratedObjects, built-in types, or lists");
+      }
+    } else {
+      throw new IllegalArgumentException("Arguments to TabularCPD must "
+          + "be instance of ArgSpec");
+    }
+  }
+
+  Map<ArgSpec, Object> map;
+}
