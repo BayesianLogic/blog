d2a13d7020b468e5c914960ac80709a492c2174f
Constantin Berzan <cberzan@gmail.com>
2014-05-29 15:25:02 -0700

    cleanup

--- src/blog/distrib/UniformReal.java
+++ src/blog/distrib/UniformReal.java
@@ -1,38 +1,3 @@
-/*
- * Copyright (c) 2005, Regents of the University of California
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * * Redistributions of source code must retain the above copyright
- *   notice, this list of conditions and the following disclaimer.
- *
- * * Redistributions in binary form must reproduce the above copyright
- *   notice, this list of conditions and the following disclaimer in
- *   the documentation and/or other materials provided with the
- *   distribution.  
- *
- * * Neither the name of the University of California, Berkeley nor
- *   the names of its contributors may be used to endorse or promote
- *   products derived from this software without specific prior 
- *   written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package blog.distrib;
 
 import java.util.List;
@@ -40,8 +5,7 @@ import java.util.List;
 import blog.common.Util;
 
 /**
- * Uniform distribution over a range of real numbers [lower, upper). The range
- * is open at the upper end for consistency with Random.nextDouble().
+ * Uniform distribution over a range of real numbers [lower, upper).
  */
 public class UniformReal implements CondProbDistrib {
   public UniformReal() {
@@ -49,6 +13,13 @@ public class UniformReal implements CondProbDistrib {
 
   public UniformReal(double lower, double upper) {
     setParams(lower, upper);
+  }
+
+  public void setParams(List<Object> params) {
+    if (params.size() != 2) {
+      throw new IllegalArgumentException("expected two params: lower and upper");
+    }
+    setParams((Double) params.get(0), (Double) params.get(1));
   }
 
   public void setParams(Double lower, Double upper) {
@@ -63,13 +34,6 @@ public class UniformReal implements CondProbDistrib {
     if (this.hasLower && this.hasUpper && this.lower >= this.upper) {
       throw new IllegalArgumentException("lower >= upper");
     }
-  }
-
-  public void setParams(List<Object> params) {
-    if (params.size() != 2) {
-      throw new IllegalArgumentException("expected two params: lower and upper");
-    }
-    setParams((Double) params.get(0), (Double) params.get(1));
   }
 
   private void checkHasParams() {
@@ -118,7 +82,7 @@ public class UniformReal implements CondProbDistrib {
   }
 
   public String toString() {
-    return "U[" + lower + ", " + upper + "]";
+    return "Uniform(" + lower + ", " + upper + ")";
   }
 
   private boolean hasLower;
