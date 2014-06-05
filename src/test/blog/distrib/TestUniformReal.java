2baef220f8c6614a6e8b32f97da059d5f3380c12
Christopher Gioia <christophergioia64@gmail.com>
2014-06-04 17:02:02 -0700

    refactored unit test for Uniform Real

--- src/test/blog/distrib/TestUniformReal.java
+++ src/test/blog/distrib/TestUniformReal.java
@@ -12,37 +12,73 @@ import blog.distrib.UniformReal;
 /**
  * Unit Tests for Uniform Real
  */
-public class TestUniformReal extends TestDistribution {
+public class TestUniformReal implements TestDistributions {
+  private final double ERROR = 1e-10;
+  private final double EPSILON = 1e-2;
 
+  @Override
   @Test
-  public void testGetProb() {
-    constructParams.add(1.0);
-    constructParams.add(3.0);
-    UniformReal unif = new UniformReal(constructParams);
-    assertEquals(0.0, unif.getProb(args, 0.99), ERROR_BOUND);
-    assertEquals(0.5, unif.getProb(args, 1.0), ERROR_BOUND);
-    assertEquals(0.5, unif.getProb(args, 2.0), ERROR_BOUND);
-    assertEquals(0.5, unif.getProb(args, 2.5), ERROR_BOUND);
-    assertEquals(0.5, unif.getProb(args, 2.99), ERROR_BOUND);
-    assertEquals(0.0, unif.getProb(args, 3.0), ERROR_BOUND);
-    assertEquals(0.0, unif.getProb(args, 3.01), ERROR_BOUND);
+  public void testProbabilityViaConstructor() {
+    // TODO Auto-generated method stub
+    UniformReal unif = new UniformReal(1.0, 3.0);
+    testDistribution(unif);
   }
 
+  @Override
   @Test
-  public void testGetLogProb() {
-    constructParams.add(1.0);
-    constructParams.add(3.0);
-    UniformReal unif = new UniformReal(constructParams);
-    assertEquals(Double.NEGATIVE_INFINITY, unif.getLogProb(args, 0.99),
-        ERROR_BOUND);
-    assertEquals(Math.log(0.5), unif.getLogProb(args, 1.0), ERROR_BOUND);
-    assertEquals(Math.log(0.5), unif.getLogProb(args, 2.0), ERROR_BOUND);
-    assertEquals(Math.log(0.5), unif.getLogProb(args, 2.5), ERROR_BOUND);
-    assertEquals(Math.log(0.5), unif.getLogProb(args, 2.9), ERROR_BOUND);
-    assertEquals(Double.NEGATIVE_INFINITY, unif.getLogProb(args, 3.0),
-        ERROR_BOUND);
-    assertEquals(Double.NEGATIVE_INFINITY, unif.getLogProb(args, 3.01),
-        ERROR_BOUND);
+  public void testProbabilityViaSetParams() {
+    UniformReal unif = new UniformReal();
+    Double[] ary = new Double[2];
+    ary[0] = 1.0;
+    ary[1] = 3.0;
+    unif.setParams(ary);
+    testDistribution(unif);
+  }
+
+  @Override
+  @Test(expected = IllegalArgumentException.class)
+  public void testInsufficientArguments() {
+    UniformReal unif = new UniformReal();
+    unif.setParams(1.0, null);
+    testDistribution(unif);
+  }
+
+  @Override
+  @Test(expected = IllegalArgumentException.class)
+  public void testIncorrectArguments() {
+    UniformReal unif = new UniformReal();
+    Double[] ary = new Double[2];
+    unif.setParams(2.0, 1.9);
+    testDistribution(unif);
+  }
+
+  @Override
+  @Test
+  public void testDoubleSet() {
+    // TODO Auto-generated method stub
+    UniformReal unif = new UniformReal();
+    unif.setParams(null, 1.0);
+    unif.setParams(0.0, null);
+    testDistribution(unif);
+  }
+
+  public void testDistribution(UniformReal unif) {
+    double lower = unif.getLower();
+    double upper = unif.getUpper();
+    double prob = (1.0 / (upper - lower));
+    assertEquals(0, unif.getProb(lower - EPSILON), ERROR);
+    assertEquals(prob, unif.getProb(lower), ERROR);
+    assertEquals(prob, unif.getProb((lower + upper) / 2.0), ERROR);
+    assertEquals(0, unif.getProb(upper), ERROR);
+    assertEquals(0, unif.getProb(upper + EPSILON), ERROR);
+
+    assertEquals(Double.NEGATIVE_INFINITY, unif.getLogProb(lower - EPSILON),
+        ERROR);
+    assertEquals(Math.log(prob), unif.getLogProb(lower), ERROR);
+    assertEquals(Math.log(prob), unif.getLogProb((lower + upper) / 2.0), ERROR);
+    assertEquals(Double.NEGATIVE_INFINITY, unif.getLogProb(upper), ERROR);
+    assertEquals(Double.NEGATIVE_INFINITY, unif.getLogProb(upper + EPSILON),
+        ERROR);
   }
 
 }
