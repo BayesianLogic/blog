01e145c25dd9cb990335bbc664a6a654c694bca1
Christopher Gioia <christophergioia64@gmail.com>
2014-06-06 09:18:39 -0700

    refactored exponential

--- src/test/blog/distrib/TestExponential.java
+++ src/test/blog/distrib/TestExponential.java
@@ -7,45 +7,126 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import blog.common.Util;
 import blog.distrib.Exponential;
 
 /**
  * Unit Tests for Exponential Distribution
  */
-public class TestExponential extends TestDistribution {
+public class TestExponential implements TestDistributions {
+  private final double ERROR = 10e-5;
 
-  @Test
-  public void testGetProb() {
-    constructParams.add(1.0);
-    Exponential exp = new Exponential(constructParams);
-    assertEquals(1.0, exp.getProb(args, 0), ERROR_BOUND);
-    assertEquals(0.36787944117144233, exp.getProb(args, 1), ERROR_BOUND);
-    assertEquals(0.1353352832366127, exp.getProb(args, 2), ERROR_BOUND);
-    assertEquals(0.006737946999085467, exp.getProb(args, 5), ERROR_BOUND);
+  void testDistributionRun(Exponential dist) {
+    dist.getProb(-1);
+    dist.getProb(0);
+    dist.getProb(10);
+    dist.getProb(1000);
+  }
 
-    constructParams.clear();
-    constructParams.add(25.0);
-    exp = new Exponential(constructParams);
-    assertEquals(25.0, exp.getProb(args, 0), ERROR_BOUND);
-    assertEquals(7.1626199215047519, exp.getProb(args, 0.05), ERROR_BOUND);
-    assertEquals(3.4719859662410053e-10, exp.getProb(args, 1), ERROR_BOUND);
+  /** Exponential, lambda = 1. */
+  public void testExponential1(Exponential exp) {
+    assertEquals(1, exp.getLambda(), ERROR);
+
+    assertEquals(0.0, exp.getProb(-20), ERROR);
+    assertEquals(0.0, exp.getProb(-0.01), ERROR);
+    assertEquals(1.0, exp.getProb(0.0), ERROR);
+    assertEquals(0.36787944117144233, exp.getProb(1.0), ERROR);
+    assertEquals(0.1353352832366127, exp.getProb(2.0), ERROR);
+    assertEquals(0.00408677143846, exp.getProb(5.5), ERROR);
+
+    assertEquals(Double.NEGATIVE_INFINITY, exp.getLogProb(-20), ERROR);
+    assertEquals(Double.NEGATIVE_INFINITY, exp.getLogProb(-0.01), ERROR);
+    assertEquals(0.0, exp.getLogProb(0.0), ERROR);
+    assertEquals(-1, exp.getLogProb(1.0), ERROR);
+    assertEquals(-2, exp.getLogProb(2.0), ERROR);
+    assertEquals(-5.5, exp.getLogProb(5.5), ERROR);
+  }
+
+  /** Exponential, lambda = 5.5. */
+
+  public void testExponential2(Exponential exp) {
+    assertEquals(5.5, exp.getLambda(), ERROR);
+
+    assertEquals(0.0, exp.getProb(-20), ERROR);
+    assertEquals(0.0, exp.getProb(-0.01), ERROR);
+    assertEquals(5.5, exp.getProb(0.0), ERROR);
+    assertEquals(3.1732239570926764, exp.getProb(0.1), ERROR);
+    assertEquals(0.022477242911552366, exp.getProb(1.0), ERROR);
+    assertEquals(2.399904239119036e-08, exp.getProb(3.5), ERROR);
+    assertEquals(4.0082482527008306e-13, exp.getProb(5.5), ERROR);
+
+    assertEquals(Double.NEGATIVE_INFINITY, exp.getLogProb(-20), ERROR);
+    assertEquals(Double.NEGATIVE_INFINITY, exp.getLogProb(-0.01), ERROR);
+    assertEquals(1.7047480922384253, exp.getLogProb(0.0), 10e-2);
+    assertEquals(1.154748092238425, exp.getLogProb(0.1), 10e-2);
+    assertEquals(-3.7952519077615747, exp.getLogProb(1.0), 10e-2);
+    assertEquals(-17.545251907761575, exp.getLogProb(3.5), 10e-2);
+    assertEquals(-28.545251907761575, exp.getLogProb(5.5), 10e-2);
   }
 
   @Test
-  public void testGetLogProb() {
-    constructParams.add(1.0);
-    Exponential exp = new Exponential(constructParams);
-    assertEquals(0.0, exp.getLogProb(args, 0), ERROR_BOUND);
-    assertEquals(-1.0, exp.getLogProb(args, 1), ERROR_BOUND);
-    assertEquals(-2.0, exp.getLogProb(args, 2), ERROR_BOUND);
-    assertEquals(-5.0, exp.getLogProb(args, 5), ERROR_BOUND);
+  public void testProbabilityViaConstructor() {
+    Exponential exp = new Exponential(1);
+    testExponential1(exp);
+    exp = new Exponential(5.5);
+    testExponential2(exp);
+  }
 
-    constructParams.clear();
-    constructParams.add(25.0);
-    exp = new Exponential(constructParams);
-    assertEquals(3.2188758248682006, exp.getLogProb(args, 0), ERROR_BOUND);
-    assertEquals(1.9688758248682006, exp.getLogProb(args, 0.05), ERROR_BOUND);
-    assertEquals(-21.7811241751318, exp.getLogProb(args, 1), ERROR_BOUND);
+  @Test
+  public void testProbabilityViaSetParams() {
+    Exponential exp = new Exponential();
+    exp.setParams(Util.array(1.0));
+    testExponential1(exp);
+    exp.setParams(Util.array(5.5));
+    testExponential2(exp);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInsufficientArguments() {
+    Exponential exp = new Exponential();
+    exp.setParams(Util.array(null));
+    testDistributionRun(exp);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInsufficientArguments2() {
+    Exponential exp = new Exponential();
+    Object[] obj = new Object[0];
+    exp.setParams(obj);
+    testDistributionRun(exp);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testExtraneousArguments() {
+    Exponential exp = new Exponential();
+    exp.setParams(Util.array(1, 2));
+    testDistributionRun(exp);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testIncorrectArguments() {
+    Exponential exp = new Exponential();
+    exp.setParams(Util.array(0.0));
+    testDistributionRun(exp);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testIncorrectArguments2() {
+    Exponential exp = new Exponential();
+    exp.setParams(Util.array(-1.0));
+    testDistributionRun(exp);
+  }
+
+  @Test
+  public void testDoubleSet() {
+    Exponential exp = new Exponential();
+    exp.setParams(Util.array(1.0));
+    exp.setParams(Util.array(null));
+    testExponential1(exp);
+    exp.setParams(Util.array(2.0));
+    exp.setParams(Util.array(5.5));
+    testExponential2(exp);
+
   }
 
 }
