90ae7173a21342fea755ad86b1b1e2ed5da379da
Christopher Gioia <christophergioia64@gmail.com>
2014-06-10 12:04:15 -0700

    ported getProb and getLogProb

--- src/blog/distrib/Dirichlet.java
+++ src/blog/distrib/Dirichlet.java
@@ -36,53 +36,26 @@
 package blog.distrib;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import blog.common.numerical.MatrixFactory;
 import blog.common.numerical.MatrixLib;
 
 /**
- * A Dirichlet distribution with shape parameter vector a, defined by
- * f(x1, x2, ... xn) = 1/Z * (x1^(a1-1) * x2^(a2-1) * ... * xn^(an-1),
- * where \sum(xi) = 1 and Z = \prod(Gamma(ai)) / Gamma(\sum(ai)).
+ * A Dirichlet distribution with shape parameter vector alpha, defined by
+ * f(x1, x2, ... xn) = 1/Z * (x1^(alpha_1-1) * x2^(alpha_2-1) * ... *
+ * xn^(alpha_n-1),
+ * where \sum(xi) = 1 and Z = \prod(Gamma(alpha_i)) / Gamma(\sum(alpha_i)).
  * 
  * Note that this is a generalization of the Beta distribution: while
  * the Beta generates a parameter for the Bernoulli process (Bernoulli
  * and Binomial distributions), the Dirichlet generates parameters for
  * the Categorical process (Categorical and Multinomial distributions).
  */
-public class Dirichlet extends AbstractCondProbDistrib {
+public class Dirichlet implements CondProbDistrib {
 
+  private final double TOLERANCE = 10e-2;
   private Double[] alpha;
-  private Gamma[] gammas;
-
-  /**
-   * Constructs a Dirichlet distribution with the given parameters.
-   * 
-   * @param params
-   *          a list of parameters for the distribution
-   */
-  public Dirichlet(List<Double> params) {
-    alpha = new Double[params.size()];
-    params.toArray(alpha);
-    initGammas();
-  }
-
-  /**
-   * Constructs a Dirichlet distribution with the given dimension and
-   * parameter value for all dimensions
-   * 
-   * @param dimension
-   *          the dimension of the distribution
-   * @param paramVal
-   *          the value for all parameters of this distribution
-   */
-  public Dirichlet(int dimension, double paramVal) {
-    alpha = new Double[dimension];
-    Arrays.fill(alpha, paramVal);
-    initGammas();
-  }
 
   /**
    * Helper for constructor to construct needed gamma functions
@@ -104,63 +77,6 @@ public class Dirichlet extends AbstractCondProbDistrib {
     for (int i = 0; i < numParams; i++) {
       gammas[i] = new Gamma(alpha[0], 1);
     }
-  }
-
-  /**
-   * Returns the probability of a vector of values from this distribution.
-   */
-  public double getProb(List args, Object childValue) {
-    if (!(childValue instanceof MatrixLib)) {
-      throw new IllegalArgumentException("Dirichlet distribution"
-          + "requires MatrixLib as argument, not " + childValue.getClass()
-          + ".");
-    }
-
-    MatrixLib mat = (MatrixLib) childValue;
-    if (mat.numRows() != 1 || mat.numCols() == 0) {
-      throw new IllegalArgumentException("Dirichlet distribution"
-          + "requires nonempty vector of Numbers as argument.");
-    }
-
-    double prob = 1.0;
-    for (int i = 0; i < mat.numCols(); i++) {
-      double count_term = (args.size() != 0) ? alpha[0] : alpha[i];
-      double x = mat.elementAt(0, i);
-      prob *= Math.pow(x, count_term - 1);
-    }
-    prob /= normalize(alpha);
-
-    return prob;
-  }
-
-  /**
-   * Returns the log of the probability of a vector of values from this
-   * distribution.
-   */
-  public double getLogProb(List args, Object childValue) {
-    if (!(childValue instanceof MatrixLib)) {
-      throw new IllegalArgumentException("Dirichlet distribution"
-          + "requires MatrixLib as argument, not " + childValue.getClass()
-          + ".");
-    }
-
-    MatrixLib mat = (MatrixLib) childValue;
-    if (mat.numRows() != 1 || mat.numCols() == 0) {
-      throw new IllegalArgumentException("Dirichlet distribution"
-          + "requires nonempty vector of Numbers as argument.");
-    }
-
-    double prob = 0.0;
-    for (int i = 0; i < mat.numCols(); i++) {
-      double count_term = (args.size() != 0) ? alpha[0] : alpha[i];
-      double x = mat.elementAt(0, i);
-      if (count_term != 1) {
-        prob += Math.log(x) * (count_term - 1);
-      }
-    }
-    prob -= Math.log(normalize(alpha));
-
-    return prob;
   }
 
   /**
@@ -189,6 +105,158 @@ public class Dirichlet extends AbstractCondProbDistrib {
     return MatrixFactory.fromArray(samples);
   }
 
+  @Override
+  /**
+   * params[0] -> MatrixLib, which is a row-vector representing the parameter 
+   *              vector alpha, as defined in the class description
+   */
+  public void setParams(Object[] params) {
+    if (params.length != 1) {
+      throw new IllegalArgumentException("expected one parameter");
+    }
+    setParams((MatrixLib) params[0]);
+  }
+
+  /**
+   * If matrix is non-null, sets the alpha vector parameter of the Dirichlet to
+   * matrix.
+   * 
+   * @param matrix
+   *          A row vector representing the parameter vector alpha, as
+   *          defined in the class description
+   */
+  public void setParams(MatrixLib matrix) {
+    if (matrix != null) {
+      if (matrix.numRows() != 1 || matrix.numCols() == 0) {
+        throw new IllegalArgumentException(
+            "Dirichlet distribution requires nonempty vector of numbers as argument.");
+      }
+      int cols = matrix.numCols();
+      this.alphas = new Double[cols];
+      this.gammas = new Gamma[cols];
+      for (int i = 0; i < cols; i++) {
+        double elementValue = matrix.elementAt(0, i);
+        alphas[i] = elementValue;
+        gammas[i] = new Gamma(elementValue, 1);
+      }
+      this.normalizationConstant = Dirichlet.normalize(alphas);
+      this.hasAlpha = true;
+    }
+  }
+
+  private void checkHasParams() {
+    if (!this.hasAlpha) {
+      throw new IllegalArgumentException("parameter alpha not provided");
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see blog.distrib.CondProbDistrib#getProb(java.lang.Object)
+   */
+  @Override
+  public double getProb(Object value) {
+    return getProb(((MatrixLib) value));
+  }
+
+  /**
+   * Returns the pdf of row vector x in a Dirichlet distribution.
+   * 
+   * @param x
+   *          row vector
+   */
+  public double getProb(MatrixLib x) {
+    checkHasParams();
+    if (x == null) {
+      throw new IllegalArgumentException("The random outcome vector is null");
+    }
+    if (x.numRows() != 1 || x.numCols() != this.alphas.length) {
+      throw new IllegalArgumentException(
+          "Incorrect dimensions given for row vector of Dirichlet. should be a 1 by "
+              + alphas.length + " matrix, but is a " + x.numRows() + " by "
+              + x.numCols() + " matrix");
+    }
+    if (!checkSupport(x)) {
+      throw new IllegalArgumentException(
+          "The elements of the random outcome row vector for a Dirichlet should sum to 1.");
+    }
+    double prob = 1.0;
+    for (int i = 0; i < x.numCols(); i++) {
+      double val = x.elementAt(0, i);
+      prob *= Math.pow(val, alpha[i] - 1);
+    }
+    return (prob / normalizationConstant);
+  }
+
+  /**
+   * Returns whether all the elements in the row vector of x sum to 1
+   * Precondition: x is a row vector with # columns >= 1
+   * 
+   * @param x
+   *          row vector
+   */
+  private boolean checkSupport(MatrixLib x) {
+    double sum = 0.0;
+    for (int i = 0; i < x.numCols(); i++) {
+      sum += x.elementAt(0, i);
+    }
+    return (sum <= (1 + TOLERANCE)) && (sum >= (1 - TOLERANCE));
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see blog.distrib.CondProbDistrib#getLogProb(java.lang.Object)
+   */
+  @Override
+  public double getLogProb(Object value) {
+    return getLogProb((MatrixLib) value);
+  }
+
+  /**
+   * Returns the pdf of row vector x in a Dirichlet distribution.
+   * 
+   * @param x
+   *          row vector
+   */
+  public double getLogProb(MatrixLib x) {
+    checkHasParams();
+    if (x == null) {
+      throw new IllegalArgumentException("The random outcome vector is null");
+    }
+    if (x.numRows() != 1 || x.numCols() != this.alphas.length) {
+      throw new IllegalArgumentException(
+          "Incorrect dimensions given for row vector of Dirichlet. should be a 1 by "
+              + alphas.length + " matrix, but is a " + x.numRows() + " by "
+              + x.numCols() + " matrix");
+    }
+    if (!checkSupport(x)) {
+      throw new IllegalArgumentException(
+          "The elements of the random outcome row vector for a Dirichlet should sum to 1.");
+    }
+
+    double prob = 0.0;
+    for (int i = 0; i < x.numCols(); i++) {
+      double value = x.elementAt(0, i);
+      if (alphas[i] != 1) {
+        prob += Math.log(value) * (alphas[i] - 1);
+      }
+    }
+    return (prob - Math.log(normalizationConstant));
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see blog.distrib.CondProbDistrib#sampleVal()
+   */
+  @Override
+  public Object sampleVal() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
   /**
    * Computes the normalization constant for a Dirichlet distribution with
    * the given parameters.
@@ -214,47 +282,8 @@ public class Dirichlet extends AbstractCondProbDistrib {
     return getClass().getName();
   }
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see blog.distrib.CondProbDistrib#setParams(java.util.List)
-   */
-  @Override
-  public void setParams(Object[] params) {
-    // TODO Auto-generated method stub
-
-  }
-
-  /*
-   * (non-Javadoc)
-   * 
-   * @see blog.distrib.CondProbDistrib#getProb(java.lang.Object)
-   */
-  @Override
-  public double getProb(Object value) {
-    // TODO Auto-generated method stub
-    return 0;
-  }
-
-  /*
-   * (non-Javadoc)
-   * 
-   * @see blog.distrib.CondProbDistrib#getLogProb(java.lang.Object)
-   */
-  @Override
-  public double getLogProb(Object value) {
-    // TODO Auto-generated method stub
-    return 0;
-  }
-
-  /*
-   * (non-Javadoc)
-   * 
-   * @see blog.distrib.CondProbDistrib#sampleVal()
-   */
-  @Override
-  public Object sampleVal() {
-    // TODO Auto-generated method stub
-    return null;
-  }
+  private Double[] alphas;
+  private boolean hasAlpha;
+  private Gamma[] gammas;
+  private double normalizationConstant;
 }
