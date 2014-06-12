877b382abeb45bb013d350d5a3a050d7b24591f7
Constantin Berzan <cberzan@gmail.com>
2014-06-12 12:43:40 -0700

    remove cruft from pelicanconf.py

--- docs/pelicanconf.py
+++ docs/pelicanconf.py
@@ -15,16 +15,6 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-# Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
