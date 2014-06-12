a5a655e288e152221c87eda5f6f44f2ed5aef846
Christopher Gioia <christophergioia64@gmail.com>
2014-06-12 16:26:24 -0700

    javadoc modifications

--- src/blog/distrib/UniformReal.java
+++ src/blog/distrib/UniformReal.java
@@ -3,10 +3,20 @@ package blog.distrib;
 import blog.common.Util;
 
 /**
- * Uniform distribution over a range of real numbers [lower, upper).
+ * Uniform distribution over a range of real numbers [<code>lower</code>,
+ * <code>upper</code>).
  */
 public class UniformReal implements CondProbDistrib {
 
+  /**
+   * mapping for <code>params</code>:
+   * <ul>
+   * <li>params[0]:<code>lower</code></li>
+   * <li>params[1]:<code>upper</code></li>
+   * </ul>
+   * 
+   * @see blog.distrib.CondProbDistrib#setParams(java.lang.Object[])
+   */
   public void setParams(Object[] params) {
     if (params.length != 2) {
       throw new IllegalArgumentException("expected two params: lower and upper");
@@ -14,6 +24,12 @@ public class UniformReal implements CondProbDistrib {
     setParams((Double) params[0], (Double) params[1]);
   }
 
+  /**
+   * If the method parameter lower is non-null, sets the distribution parameter
+   * <code>lower</code> to lower. Similarly for upper. Once both parameters
+   * <code>lower</code> and <code>upper</code> have been set, checks to see that
+   * <code>lower</code> < <code>upper</code>.
+   */
   public void setParams(Double lower, Double upper) {
     if (lower != null) {
       this.lower = lower;
@@ -37,10 +53,18 @@ public class UniformReal implements CondProbDistrib {
     }
   }
 
+  /*
+   * (non-Javadoc)
+   * 
+   * @see blog.distrib.CondProbDistrib#getProb(java.lang.Object)
+   */
   public double getProb(Object value) {
     return getProb(((Double) value).doubleValue());
   }
 
+  /**
+   * Returns the probability of <code>value</code>.
+   */
   public double getProb(double value) {
     checkHasParams();
     if ((value >= lower) && (value < upper)) {
@@ -50,35 +74,51 @@ public class UniformReal implements CondProbDistrib {
     }
   }
 
+  /*
+   * (non-Javadoc)
+   * 
+   * @see blog.distrib.CondProbDistrib#getLogProb(java.lang.Object)
+   */
   public double getLogProb(Object value) {
     return getLogProb(((Double) value).doubleValue());
   }
 
+  /**
+   * Returns the log probability of <code>value</code>.
+   */
   public double getLogProb(double value) {
     return Math.log(getProb(value));
   }
 
+  /*
+   * (non-Javadoc)
+   * 
+   * @see blog.distrib.CondProbDistrib#sampleVal()
+   */
   public Object sampleVal() {
-    return sampleValue();
-  }
-
-  public double sampleValue() {
     checkHasParams();
     // rely on the fact that Util.random() returns a value in [0, 1)
     double x = lower + (Util.random() * (upper - lower));
     return x;
   }
 
+  /**
+   * Returns the parameter <code>lower</code>.
+   */
   public double getLower() {
     return lower;
   }
 
+  /**
+   * Returns the parameter <code>upper</code>.
+   */
   public double getUpper() {
     return upper;
   }
 
+  @Override
   public String toString() {
-    return "Uniform(" + lower + ", " + upper + ")";
+    return getClass().getName();
   }
 
   private boolean hasLower;
