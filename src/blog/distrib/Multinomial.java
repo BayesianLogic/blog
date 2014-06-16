2d27a7c2c83fb939394f210b432b6c0a6d1bb864
Christopher Gioia <christophergioia64@gmail.com>
2014-06-16 15:54:16 -0700

    partial refactoring of multinomial

--- src/blog/distrib/Multinomial.java
+++ src/blog/distrib/Multinomial.java
@@ -41,48 +41,28 @@ import java.util.List;
 import blog.common.Util;
 import blog.common.numerical.MatrixFactory;
 import blog.common.numerical.MatrixLib;
-import blog.model.MatrixSpec;
 
 /**
- * Multinomial distribution.
- * Multinomial distribution accepts two arguments,
- * <num of trials>, <weight matrix/list>, either or both can be random.
+ * The multinomial distribution accepts two parameters, an integer
+ * <code>N</code> representing the number of trials, and <code>P</code>, an
+ * unnormalized row vector representing the unnormalized probabilities for each
+ * of the categories.
+ * 
  * See https://en.wikipedia.org/wiki/Multinomial_distribution
+ * 
+ * @author cgioia
+ * @since June 16, 2014
  */
-public class Multinomial extends AbstractCondProbDistrib {
+public class Multinomial implements CondProbDistrib {
 
-  /**
-   * Multinomial distribution accepts two arguments,
-   * <num of trials>, <weight matrix/list>, either or both can be random.
-   * 
-   * @param params
-   */
-  public Multinomial(List params) {
-    if (params.size() == 2) {
-      expectTrialsAsArg = true;
-      expectWeightAsArg = true;
-      initParams(params);
-      expectTrialsAsArg = false;
-      expectWeightAsArg = false;
-    } else if (params.size() == 1) {
-      Object obj = params.get(0);
-      if (obj instanceof Integer) {
-        expectTrialsAsArg = true;
-        expectWeightAsArg = false;
-        initParams(params);
-        expectTrialsAsArg = false;
-        expectWeightAsArg = true;
-      } else {
-        expectTrialsAsArg = false;
-        expectWeightAsArg = true;
-        initParams(params);
-        expectTrialsAsArg = true;
-        expectWeightAsArg = false;
-      }
-    } else {
-      expectTrialsAsArg = true;
-      expectWeightAsArg = true;
-    }
+  /** Returns the distribution parameter <code>N</code>. */
+  public int getN() {
+    return n;
+  }
+
+  /** Returns the distribution parameter <code>P</code>. */
+  public double[] getP() {
+    return p;
   }
 
   MatrixLib ensureValueFormat(Object value) {
@@ -196,9 +176,6 @@ public class Multinomial extends AbstractCondProbDistrib {
     if (expectWeightAsArg) {
       Object objectPi = args.get(argidx);
       double[] nativePi;
-      if (objectPi instanceof MatrixSpec) {
-        objectPi = ((MatrixSpec) objectPi).getValueIfNonRandom();
-      }
       if (objectPi instanceof MatrixLib) {
         MatrixLib pi = (MatrixLib) objectPi;
         if (pi.numCols() == 1) {
@@ -260,8 +237,84 @@ public class Multinomial extends AbstractCondProbDistrib {
    */
   @Override
   public void setParams(Object[] params) {
-    // TODO Auto-generated method stub
+    if (params.length != 2) {
+      throw new IllegalArgumentException("expected two parameters");
+    }
+    setParams((Integer) params[0], (MatrixLib) params[1]);
+  }
 
+  /**
+   * If method parameter n is non-null, then attempt to set distribution
+   * parameter <code>N</code> to n if n is a nonnegative integer.
+   * If method parameter p is non-null, then set distribution parameter
+   * <code>P</code> to method parameter p if distribution parameter
+   * <code>P</code> has not previously been set.
+   * 
+   * @param n
+   *          distribution parameter <code>N</code>, representing the number of
+   *          categories. Must be nonnegative. Can be set multiple times.
+   * 
+   * @param p
+   *          distribution parameter <code>P</code>, representing the row vector
+   *          of unnormalized probabilities. Can only be set once.
+   */
+  public void setParams(Integer n, MatrixLib p) {
+    if (n != null) {
+      if (n < 0) {
+        throw new IllegalArgumentException(
+            "The number of trials 'n' for a Multinomial Distribution must be nonnegative");
+      }
+      this.n = n;
+      this.hasN = true;
+    }
+    if (p != null) {
+      if (this.hasP) {
+        throw new IllegalArgumentException(
+            "Can only set the row vector of probabilities for a multinomial distribution once");
+      }
+      if (p.numRows() != 1 || p.numCols() == 0) {
+        throw new IllegalArgumentException(
+            "The row vector p passed in is not a row vector");
+      }
+      this.p = initializeProbabilityVector(p);
+      this.k = this.p.length;
+      this.hasP = true;
+    }
+  }
+
+  /**
+   * Precondition: p is a row vector
+   * 
+   * @param p
+   * @returns an array of normalized probabilities from the row vector p.
+   */
+  private static double[] initializeProbabilityVector(MatrixLib p) {
+    double[] pi = new double[p.numCols()];
+    double sum = 0.0;
+    for (int i = 0; i < p.numCols(); i++) {
+      double ele = p.elementAt(0, i);
+      if (ele < 0) {
+        throw new IllegalArgumentException("Probability " + ele
+            + " for element " + i + " is negative.");
+      }
+      sum += p.elementAt(0, i);
+    }
+    if (sum < 1e-9) {
+      throw new IllegalArgumentException("Probabilities sum to approx zero");
+    }
+    for (int i = 0; i < p.numCols(); i++) {
+      pi[i] = p.elementAt(0, i) / sum;
+    }
+    return pi;
+  }
+
+  private void checkHasParams() {
+    if (!this.hasN) {
+      throw new IllegalArgumentException("parameter N not provided");
+    }
+    if (!this.hasP) {
+      throw new IllegalArgumentException("parameter P not provided");
+    }
   }
 
   /*
@@ -271,8 +324,43 @@ public class Multinomial extends AbstractCondProbDistrib {
    */
   @Override
   public double getProb(Object value) {
-    // TODO Auto-generated method stub
-    return 0;
+    return getProb((MatrixLib) value);
+  }
+
+  /**
+   * Returns the probability of value outcomes, where value is a row vector
+   * representing the number of times each category occurred.
+   * 
+   * @param value
+   */
+  public double getProb(MatrixLib value) {
+    checkHasParams();
+    if (value.numRows() != 1 || value.numCols() != this.k) {
+      throw new IllegalArgumentException(
+          "The matrix provided is of the incorrect dimensions. Expecting a 1 by "
+              + this.k + " row vector but instead got a matrix of dimension "
+              + value.numRows() + " by " + value.numCols());
+    }
+    double sum = 0.0;
+    for (int i = 0; i < k; i++) {
+      double element = value.elementAt(0, i);
+      if (element < 0 || element % 1 != 0.0) {
+        return 0.0; // Number of successes for a particular category is negative
+                    // or not an integer
+      }
+      sum += element;
+    }
+    if (sum != n) {
+      return 0.0; // N != the sum of values
+    }
+    double prob = Util.factorial(n);
+    for (int i = 0; i < k; i++) {
+      prob *= Math.pow(p[i], value.elementAt(i, 0));
+      prob /= Util.factorial((int) Math.round(value.elementAt(i, 0)));
+      // FIXME: It would be better if we could take the param as an array
+      // of ints, so we don't have to worry about rounding.
+    }
+    return prob;
   }
 
   /*
@@ -282,8 +370,36 @@ public class Multinomial extends AbstractCondProbDistrib {
    */
   @Override
   public double getLogProb(Object value) {
-    // TODO Auto-generated method stub
-    return 0;
+    return getLogProb((MatrixLib) value);
+  }
+
+  /**
+   * Returns the log probability of value outcomes, where value is a row vector
+   * representing the number of times each category occurred.
+   * 
+   * @param value
+   */
+  public double getLogProb(MatrixLib value) {
+    checkHasParams();
+    if (value.numRows() != 1 || value.numCols() != this.k) {
+      throw new IllegalArgumentException(
+          "The matrix provided is of the incorrect dimensions. Expecting a 1 by "
+              + this.k + " row vector but instead got a matrix of dimension "
+              + value.numRows() + " by " + value.numCols());
+    }
+    double sum = 0.0;
+    for (int i = 0; i < k; i++) {
+      double element = value.elementAt(0, i);
+      if (element < 0 || element % 1 != 0.0) {
+        return Double.NEGATIVE_INFINITY; // Number of successes for a particular
+                                         // category is negative or not an
+                                         // integer
+      }
+      sum += element;
+    }
+    if (sum != n) {
+      return 0.0; // N != the sum of values
+    }
   }
 
   /*
@@ -296,4 +412,10 @@ public class Multinomial extends AbstractCondProbDistrib {
     // TODO Auto-generated method stub
     return null;
   }
+
+  private int n;
+  private boolean hasN;
+  private double[] p;
+  private boolean hasP;
+  private int k; // the number of categories; dimension of p
 }
