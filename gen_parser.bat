0962408359afcbf861ea20ab9c3f397c6c9bc4b4
lileicc <lileicc@gmail.com>
2014-06-15 22:14:01 -0700

    fixed parser script

--- gen_parser.bat
+++ gen_parser.bat
@@ -1,5 +1,5 @@
 
-java -cp lib\jflex-1.5.1.jar JFlex.Main src\blog\parse\BLOGLexer.flex
+java -cp lib\jflex-1.5.1.jar jflex.Main src\blog\parse\BLOGLexer.flex
 cd src\blog\parse
 java -cp ..\..\..\lib\java-cup-11b.jar java_cup.Main -locations -symbols BLOGTokenConstants -parser BLOGParser BLOGParser.cup
 cd ..\..\..\
