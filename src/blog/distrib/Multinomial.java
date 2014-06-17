ec971b2b22b62c004763e0c0fc3a421b0372dcc2
Christopher Gioia <christophergioia64@gmail.com>
2014-06-16 17:50:56 -0700

    refactored Multinomial

--- src/blog/distrib/Multinomial.java
+++ src/blog/distrib/Multinomial.java
@@ -35,9 +35,6 @@
 
 package blog.distrib;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import blog.common.Util;
 import blog.common.numerical.MatrixFactory;
 import blog.common.numerical.MatrixLib;
@@ -65,171 +62,6 @@ public class Multinomial implements CondProbDistrib {
     return p;
   }
 
-  MatrixLib ensureValueFormat(Object value) {
-    if (!(value instanceof MatrixLib)) {
-      throw new IllegalArgumentException("expected vector value");
-    }
-    final int numBuckets = pi.length;
-    MatrixLib valueVector = (MatrixLib) value;
-    if (valueVector.numRows() == 1 && (valueVector.numCols() != 1)) {
-      valueVector = valueVector.transpose();
-    }
-    if (valueVector.numRows() != numBuckets) {
-      throw new IllegalArgumentException("value has wrong dimension");
-    }
-    return valueVector;
-  }
-
-  /**
-   * Returns the probability of given vector.
-   */
-  public double getProb(List args, Object value) {
-    initParams(args);
-    final int numBuckets = pi.length;
-    MatrixLib valueVector = ensureValueFormat(value);
-    int sum = (int) valueVector.columnSum().elementAt(0, 0);
-    if (sum != numTrials) {
-      return 0;
-    }
-    double prob = Util.factorial(numTrials);
-    for (int i = 0; i < numBuckets; i++) {
-      prob *= Math.pow(pi[i], valueVector.elementAt(i, 0));
-      prob /= Util.factorial((int) Math.round(valueVector.elementAt(i, 0)));
-      // FIXME: It would be better if we could take the param as an array
-      // of ints, so we don't have to worry about rounding.
-    }
-    return prob;
-  }
-
-  /**
-   * Returns the log probability of given vector.
-   */
-  public double getLogProb(List args, Object value) {
-    initParams(args);
-    final int numBuckets = pi.length;
-    MatrixLib valueVector = ensureValueFormat(value);
-    int sum = (int) valueVector.columnSum().elementAt(0, 0);
-    if (sum != numTrials) {
-      return 0;
-    }
-    double logProb = Util.logFactorial(numTrials);
-    for (int i = 0; i < numBuckets; i++) {
-      logProb += valueVector.elementAt(i, 0) * Math.log(pi[i]);
-      logProb -= Util
-          .logFactorial((int) Math.round(valueVector.elementAt(i, 0)));
-    }
-    return logProb;
-  }
-
-  /**
-   * Returns a vector chosen at random according to this distribution.
-   */
-  public MatrixLib sampleVal(List args) {
-    initParams(args);
-
-    final int numBuckets = pi.length;
-    double[] cdf = new double[numBuckets];
-    cdf[0] = pi[0];
-    for (int i = 1; i < numBuckets; i++) {
-      cdf[i] = cdf[i - 1] + pi[i];
-    }
-
-    int[] result = new int[numBuckets];
-    for (int i = 0; i < numBuckets; i++) {
-      result[i] = 0;
-    }
-
-    for (int trial = 0; trial < numTrials; trial++) {
-      double val = Util.random();
-      int bucket;
-      for (bucket = 0; bucket < numBuckets; bucket++) {
-        if (val <= cdf[bucket]) {
-          break;
-        }
-      }
-      result[bucket] += 1;
-    }
-
-    // Convert to Jama (nasty).
-    double[][] doubleResult = new double[numBuckets][1];
-    for (int i = 0; i < numBuckets; i++) {
-      doubleResult[i][0] = result[i];
-    }
-    return MatrixFactory.fromArray(doubleResult);
-  }
-
-  private void initParams(List args) {
-    int argidx = 0;
-    if (expectTrialsAsArg) {
-      Object obj = args.get(argidx);
-      if (!(obj instanceof Number)) {
-        throw new IllegalArgumentException(
-            "expected first arg to be number numTrials");
-      }
-      this.numTrials = ((Number) obj).intValue();
-      if (numTrials < 0) {
-        throw new IllegalArgumentException(
-            "Multinomial expects non-negative integer as the numTrial argument.");
-      }
-      argidx++;
-    }
-    if (expectWeightAsArg) {
-      Object objectPi = args.get(argidx);
-      double[] nativePi;
-      if (objectPi instanceof MatrixLib) {
-        MatrixLib pi = (MatrixLib) objectPi;
-        if (pi.numCols() == 1) {
-          nativePi = new double[pi.numRows()];
-          for (int i = 0; i < pi.numRows(); i++) {
-            nativePi[i] = pi.elementAt(i, 0);
-          }
-        } else if (pi.numRows() == 1) {
-          nativePi = new double[pi.numCols()];
-          for (int i = 0; i < pi.numCols(); i++) {
-            nativePi[i] = pi.elementAt(0, i);
-          }
-        } else {
-          throw new IllegalArgumentException(
-              "expect either a row vector or column vector");
-        }
-      } else if (objectPi instanceof ArrayList) {
-        ArrayList<?> arrayPi = (ArrayList<?>) objectPi;
-        int size = arrayPi.size();
-        nativePi = new double[size];
-        for (int i = 0; i < size; i++)
-          nativePi[i] = (Double) arrayPi.get(i);
-      } else {
-        throw new IllegalArgumentException(
-            "expected second arg to be array of reals; got " + objectPi
-                + " instead, which is of type " + objectPi.getClass().getName());
-      }
-      normalizeWeight(nativePi);
-    }
-  }
-
-  private void normalizeWeight(double[] pi) {
-    this.pi = pi;
-    double sum = 0;
-    for (int i = 0; i < pi.length; i++) {
-      if (pi[i] < 0) {
-        throw new IllegalArgumentException("Probability " + pi[i]
-            + " for element " + i + " is negative.");
-      }
-      sum += pi[i];
-    }
-    if (sum < 1e-9) {
-      throw new IllegalArgumentException("Probabilities sum to approx zero");
-    }
-    for (int i = 0; i < pi.length; i++) {
-      this.pi[i] /= sum;
-    }
-  }
-
-  private int numTrials;
-  private double[] pi;
-  private boolean expectTrialsAsArg;
-  private boolean expectWeightAsArg;
-
   /*
    * (non-Javadoc)
    * 
@@ -247,8 +79,7 @@ public class Multinomial implements CondProbDistrib {
    * If method parameter n is non-null, then attempt to set distribution
    * parameter <code>N</code> to n if n is a nonnegative integer.
    * If method parameter p is non-null, then set distribution parameter
-   * <code>P</code> to method parameter p if distribution parameter
-   * <code>P</code> has not previously been set.
+   * <code>P</code> to method parameter p.
    * 
    * @param n
    *          distribution parameter <code>N</code>, representing the number of
@@ -256,7 +87,7 @@ public class Multinomial implements CondProbDistrib {
    * 
    * @param p
    *          distribution parameter <code>P</code>, representing the row vector
-   *          of unnormalized probabilities. Can only be set once.
+   *          of unnormalized probabilities.
    */
   public void setParams(Integer n, MatrixLib p) {
     if (n != null) {
@@ -268,16 +99,11 @@ public class Multinomial implements CondProbDistrib {
       this.hasN = true;
     }
     if (p != null) {
-      if (this.hasP) {
-        throw new IllegalArgumentException(
-            "Can only set the row vector of probabilities for a multinomial distribution once");
-      }
       if (p.numRows() != 1 || p.numCols() == 0) {
         throw new IllegalArgumentException(
             "The row vector p passed in is not a row vector");
       }
-      this.p = initializeProbabilityVector(p);
-      this.k = this.p.length;
+      initializeProbabilityVector(p);
       this.hasP = true;
     }
   }
@@ -285,10 +111,12 @@ public class Multinomial implements CondProbDistrib {
   /**
    * Precondition: p is a row vector
    * 
+   * Sets instance variable p to a normalized array of probabilities
+   * Sets pCDF to the CDF of p
+   * 
    * @param p
-   * @returns an array of normalized probabilities from the row vector p.
    */
-  private static double[] initializeProbabilityVector(MatrixLib p) {
+  private void initializeProbabilityVector(MatrixLib p) {
     double[] pi = new double[p.numCols()];
     double sum = 0.0;
     for (int i = 0; i < p.numCols(); i++) {
@@ -305,7 +133,15 @@ public class Multinomial implements CondProbDistrib {
     for (int i = 0; i < p.numCols(); i++) {
       pi[i] = p.elementAt(0, i) / sum;
     }
-    return pi;
+    this.p = pi;
+    this.k = this.p.length;
+
+    // CDF of p -- precompute it for sampling
+    this.pCDF = new double[k];
+    pCDF[0] = this.p[0];
+    for (int i = 1; i < k; i++) {
+      this.pCDF[i] = pCDF[i - 1] + this.p[i];
+    }
   }
 
   private void checkHasParams() {
@@ -335,7 +171,7 @@ public class Multinomial implements CondProbDistrib {
    */
   public double getProb(MatrixLib value) {
     checkHasParams();
-    if (value.numRows() != 1 || value.numCols() != this.k) {
+    if (value.numRows() != 1 || value.numCols() != k) {
       throw new IllegalArgumentException(
           "The matrix provided is of the incorrect dimensions. Expecting a 1 by "
               + this.k + " row vector but instead got a matrix of dimension "
@@ -355,8 +191,8 @@ public class Multinomial implements CondProbDistrib {
     }
     double prob = Util.factorial(n);
     for (int i = 0; i < k; i++) {
-      prob *= Math.pow(p[i], value.elementAt(i, 0));
-      prob /= Util.factorial((int) Math.round(value.elementAt(i, 0)));
+      prob *= Math.pow(p[i], value.elementAt(0, i));
+      prob /= Util.factorial((int) Math.round(value.elementAt(0, i)));
       // FIXME: It would be better if we could take the param as an array
       // of ints, so we don't have to worry about rounding.
     }
@@ -398,8 +234,14 @@ public class Multinomial implements CondProbDistrib {
       sum += element;
     }
     if (sum != n) {
-      return 0.0; // N != the sum of values
+      return Double.NEGATIVE_INFINITY; // N != the sum of values
     }
+    double logProb = Util.logFactorial(n);
+    for (int i = 0; i < k; i++) {
+      logProb += value.elementAt(0, i) * Math.log(p[i]);
+      logProb -= Util.logFactorial((int) Math.round(value.elementAt(0, i)));
+    }
+    return logProb;
   }
 
   /*
@@ -409,13 +251,40 @@ public class Multinomial implements CondProbDistrib {
    */
   @Override
   public Object sampleVal() {
-    // TODO Auto-generated method stub
-    return null;
+    return sample_value();
+  }
+
+  /** Samples a value from the multinomial. */
+  public MatrixLib sample_value() {
+    checkHasParams();
+    int[] result = new int[k];
+    for (int i = 0; i < k; i++) {
+      result[i] = 0;
+    }
+
+    for (int trial = 0; trial < n; trial++) {
+      double val = Util.random();
+      int bucket;
+      for (bucket = 0; bucket < k; bucket++) {
+        if (val <= pCDF[bucket]) {
+          break;
+        }
+      }
+      result[bucket] += 1;
+    }
+
+    // Convert to Jama (nasty).
+    double[][] doubleResult = new double[n][1];
+    for (int i = 0; i < n; i++) {
+      doubleResult[i][0] = result[i];
+    }
+    return MatrixFactory.fromArray(doubleResult);
   }
 
   private int n;
   private boolean hasN;
   private double[] p;
+  private double[] pCDF;
   private boolean hasP;
   private int k; // the number of categories; dimension of p
 }
---
+++ src/test/blog/distrib/TestMultinomial.java
@@ -0,0 +1,129 @@
+/**
+ * 
+ */
+package test.blog.distrib;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import blog.common.numerical.MatrixFactory;
+import blog.common.numerical.MatrixLib;
+import blog.distrib.Multinomial;
+
+/**
+ * @author cgioia
+ * @since Jun 16, 2014
+ * 
+ */
+public class TestMultinomial implements TestDistributions {
+  private final double ERROR = 10e-5;
+
+  /** Multinomial. n = 3, p = [0.5, 0.25, 0.25]. */
+  public void testMultinomial1(Multinomial mult) {
+    assertEquals(3, mult.getN(), ERROR);
+
+    assertEquals(0.1875, mult.getProb(MatrixFactory.createRowVector(1, 1, 1)),
+        ERROR);
+    assertEquals(Math.log(0.1875),
+        mult.getLogProb(MatrixFactory.createRowVector(1, 1, 1)), ERROR);
+    assertEquals(0.1875, mult.getProb(MatrixFactory.createRowVector(2, 1, 0)),
+        ERROR);
+    assertEquals(Math.log(0.1875),
+        mult.getLogProb(MatrixFactory.createRowVector(2, 1, 0)), ERROR);
+  }
+
+  /** Multinomial. n = 4, p = [0.25, 0.25, 0.25, 0.25]. */
+  public void testMultinomial2(Multinomial mult) {
+    assertEquals(4, mult.getN(), ERROR);
+    double[] p = mult.getP();
+    assertEquals(4, p.length, ERROR);
+    assertEquals(0.25, p[0], ERROR);
+    assertEquals(0.25, p[1], ERROR);
+    assertEquals(0.25, p[2], ERROR);
+    assertEquals(0.25, p[3], ERROR);
+    assertEquals(0.09375,
+        mult.getProb(MatrixFactory.createRowVector(1, 1, 1, 1)), ERROR);
+    assertEquals(Math.log(0.09375),
+        mult.getLogProb(MatrixFactory.createRowVector(1, 1, 1, 1)), ERROR);
+    assertEquals(3.90625e-3,
+        mult.getProb(MatrixFactory.createRowVector(4, 0, 0, 0)), ERROR);
+    assertEquals(Math.log(3.90625e-3),
+        mult.getLogProb(MatrixFactory.createRowVector(4, 0, 0, 0)), ERROR);
+    assertEquals(0, mult.getProb(MatrixFactory.createRowVector(2, 1, 1, 1)),
+        ERROR);
+    assertEquals(Double.NEGATIVE_INFINITY,
+        mult.getLogProb(MatrixFactory.createRowVector(2, 1, 1, 1)), ERROR);
+    assertEquals(0, mult.getProb(MatrixFactory.createRowVector(2, -1, 1, 2)),
+        ERROR);
+    assertEquals(Double.NEGATIVE_INFINITY,
+        mult.getLogProb(MatrixFactory.createRowVector(2, -1, 1, 2)), ERROR);
+  }
+
+  @Test
+  public void testProbabilityViaConstructor() {
+    // no longer needed. will be removed
+  }
+
+  @Test
+  public void testProbabilityViaSetParams() {
+    Multinomial mult = new Multinomial();
+    mult.setParams(new Object[] { 3,
+        MatrixFactory.createRowVector(0.5, 0.25, 0.25) });
+    testMultinomial1(mult);
+    mult.setParams(new Object[] { 4,
+        MatrixFactory.createRowVector(0.25, 0.25, 0.25, 0.25) });
+    testMultinomial2(mult);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInsufficientArguments() {
+    Multinomial mult = new Multinomial();
+    mult.setParams(new Object[] { null,
+        MatrixFactory.createRowVector(0.5, 0.25, 0.25) });
+    mult.sampleVal();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInsufficientArguments2() {
+    Multinomial mult = new Multinomial();
+    mult.setParams(new Object[] { 2, null });
+    mult.sampleVal();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testIncorrectArguments3() {
+    Multinomial mult = new Multinomial();
+    MatrixLib lib = MatrixFactory.ones(2, 3);
+    mult.setParams(new Object[] { 2, lib });
+    mult.sampleVal();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testIncorrectArguments4() {
+    Multinomial mult = new Multinomial();
+    mult.setParams(new Object[] { -1, MatrixFactory.createRowVector(1, 1) });
+    mult.sampleVal();
+  }
+
+  @Test
+  public void testNormalization() {
+    Multinomial mult = new Multinomial();
+    mult.setParams(new Object[] { 4, MatrixFactory.createRowVector(1, 1, 1, 1) });
+    testMultinomial2(mult);
+  }
+
+  @Test
+  public void testDoubleSet() {
+    Multinomial mult = new Multinomial();
+    mult.setParams(new Object[] { null, null });
+    mult.setParams(new Object[] { null, MatrixFactory.createRowVector(2, 1, 1) });
+    mult.setParams(new Object[] { 3, null });
+    testMultinomial1(mult);
+    mult.setParams(new Object[] { 4, null });
+    mult.setParams(new Object[] { null,
+        MatrixFactory.createRowVector(1, 1, 1, 1) });
+    testMultinomial2(mult);
+  }
+
+}
