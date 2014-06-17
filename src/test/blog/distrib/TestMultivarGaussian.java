5d0996279e6990633788c24553f376a8085b0545
Christopher Gioia <christophergioia64@gmail.com>
2014-06-17 15:59:21 -0700

    cleanup; refactored unit test

--- src/test/blog/distrib/TestMultivarGaussian.java
+++ src/test/blog/distrib/TestMultivarGaussian.java
@@ -3,7 +3,6 @@ package test.blog.distrib;
 import static org.junit.Assert.assertEquals;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Set;
 
 import org.junit.Test;
@@ -18,10 +17,11 @@ import blog.distrib.MultivarGaussian;
  * Unit tests for MultivarGaussian.
  */
 @RunWith(JUnit4.class)
-public class TestMultivarGaussian extends TestDistribution {
+public class TestMultivarGaussian implements TestDistributions {
   private MatrixLib mean;
   private MatrixLib variance;
   private HashMap<MatrixLib, Double> probVals;
+  private final double ERROR = 10e-5;
 
   public TestMultivarGaussian() {
     // Create a Gaussian with a mean and covariance matrix
@@ -48,107 +48,12 @@ public class TestMultivarGaussian extends TestDistribution {
 
   }
 
-  @Test
-  public void testFixedMeanFixedVariance() {
-    constructParams.add(mean);
-    constructParams.add(variance);
-    testGaussian(constructParams, args);
-  }
-
-  @Test
-  public void testRandomMeanFixedVariance() {
-    constructParams.add(variance);
-    args.add(mean);
-    testGaussian(constructParams, args);
-  }
-
-  @Test
-  public void testRandomMeanRandomVariance() {
-    args.add(mean);
-    args.add(variance);
-    testGaussian(constructParams, args);
-  }
-
-  /**
-   * Error Case 1: Fixed variance and fixed mean. Extraneous random argument.
-   */
-  @Test(expected = IllegalArgumentException.class)
-  public void errorCase1() {
-    constructParams.add(mean);
-    constructParams.add(variance);
-    args.add(mean);
-    testGaussian(constructParams, args);
-  }
-
-  /**
-   * Error Case 2: No variance provided.
-   */
-  @Test(expected = IllegalArgumentException.class)
-  public void errorCase2() {
-    constructParams.add(mean);
-    testGaussian(constructParams, args);
-  }
-
-  /**
-   * Error Case 3: Variance provided as the first fixed parameter,
-   * and mean provided as the second fixed parameter.
-   */
-  @Test(expected = IllegalArgumentException.class)
-  public void errorCase3() {
-    constructParams.add(variance);
-    constructParams.add(mean);
-    testGaussian(constructParams, args);
-  }
-
-  /**
-   * Error Case 4: Mean provided as a fixed argument,
-   * Covariance as a random argument.
-   */
-  @Test(expected = IllegalArgumentException.class)
-  public void errorCase4() {
-    constructParams.add(mean);
-    args.add(variance);
-    testGaussian(constructParams, args);
-  }
-
-  /**
-   * Error Case 5: No fixed parameters, more than 2 random parameters.
-   */
-  @Test(expected = IllegalArgumentException.class)
-  public void errorCase5() {
-    args.add(mean);
-    args.add(variance);
-    args.add(mean);
-    testGaussian(constructParams, args);
-  }
-
-  /**
-   * Error Case 6: No fixed parameters, Only 1 random parameter.
-   */
-  @Test(expected = IllegalArgumentException.class)
-  public void errorCase6() {
-    args.add(mean);
-    testGaussian(constructParams, args);
-  }
-
-  /**
-   * Error Case 7: No fixed parameters. Covariance provided
-   * as the first argument and the mean as the argument
-   */
-  @Test(expected = IllegalArgumentException.class)
-  public void errorCase7() {
-    args.add(variance);
-    args.add(mean);
-    testGaussian(constructParams, args);
-  }
-
-  public void testGaussian(List<Object> constructParams, List<Object> args) {
-    MultivarGaussian mvg = new MultivarGaussian(constructParams);
+  public void testGaussian(MultivarGaussian gauss) {
     Set<MatrixLib> points = probVals.keySet();
     for (MatrixLib point : points) {
-      assertEquals(probVals.get(point), mvg.getProb(args, point), ERROR_BOUND);
-      assertEquals(Math.log(probVals.get(point)), mvg.getLogProb(args, point),
-          ERROR_BOUND);
+      assertEquals(probVals.get(point), gauss.getProb(point), ERROR);
+      assertEquals(Math.log(probVals.get(point)), gauss.getLogProb(point),
+          ERROR);
     }
   }
 
@@ -165,10 +70,76 @@ public class TestMultivarGaussian extends TestDistribution {
 
     // MultivarGaussian works nonetheless.
     // (The values here were NOT verified against an external implementation.)
-    MultivarGaussian dist = new MultivarGaussian(mean, sigma);
+    MultivarGaussian dist = new MultivarGaussian();
+    dist.setParams(mean, sigma);
     assertEquals(dist.getLogProb(MatrixFactory.zeros(400, 1)),
         1013.9756425145674, 1e-10);
     assertEquals(dist.getLogProb(MatrixFactory.ones(400, 1).timesScale(0.2)),
         -6986.024357485432, 1e-10);
+  }
+
+  @Test
+  public void testProbabilityViaConstructor() {
+    // no longer needed. will be removed.
+  }
+
+  @Test
+  public void testProbabilityViaSetParams() {
+    MultivarGaussian gauss = new MultivarGaussian();
+    gauss.setParams(mean, variance);
+    testGaussian(gauss);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInsufficientArguments() {
+    MultivarGaussian gauss = new MultivarGaussian();
+    gauss.setParams(mean, null);
+    gauss.sampleVal();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInsufficientArguments2() {
+    MultivarGaussian gauss = new MultivarGaussian();
+    gauss.setParams(null, variance);
+    gauss.sampleVal();
+  }
+
+  // covariance matrix not symmetric
+  @Test(expected = IllegalArgumentException.class)
+  public void testIncorrectArguments() {
+    MultivarGaussian gauss = new MultivarGaussian();
+    MatrixLib matrix = MatrixFactory.zeros(2, 2);
+    matrix.setElement(0, 1, 1.5);
+    matrix.setElement(1, 0, 1.7);
+    gauss.setParams(mean, matrix);
+    gauss.sampleVal();
+  }
+
+  // mean vector and covariance matrix of different dimensions
+  @Test(expected = IllegalArgumentException.class)
+  public void testIncorrectArguments2() {
+    MultivarGaussian gauss = new MultivarGaussian();
+    MatrixLib sigma = MatrixFactory.eye(2);
+    double[][] ary = new double[3][1];
+    MatrixLib mean = MatrixFactory.fromArray(ary);
+    gauss.setParams(mean, sigma);
+    gauss.sampleVal();
+  }
+
+  // covariance matrix is not square
+  @Test(expected = IllegalArgumentException.class)
+  public void testIncorrectArguments3() {
+    MultivarGaussian gauss = new MultivarGaussian();
+    MatrixLib sigma = MatrixFactory.zeros(2, 3);
+    double[][] ary = new double[3][1];
+    MatrixLib mean = MatrixFactory.fromArray(ary);
+    gauss.setParams(mean, sigma);
+    gauss.sampleVal();
+  }
+
+  @Test
+  public void testDoubleSet() {
+    // TODO Auto-generated method stub
+
   }
 }
