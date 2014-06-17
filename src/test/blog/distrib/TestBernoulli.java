0e1fea4af777e2f7f97bf9e89dd9f5d8bdc3901f
Christopher Gioia <christophergioia64@gmail.com>
2014-06-17 11:03:52 -0700

    refactored unit tests

--- src/test/blog/distrib/TestBernoulli.java
+++ src/test/blog/distrib/TestBernoulli.java
@@ -24,36 +24,40 @@ public class TestBernoulli implements TestDistributions {
   public void testProbabilityViaSetParams() {
     Bernoulli b = new Bernoulli();
     b.setParams(new Object[] { 0.4 });
-    assertEquals(0.4, b.getP(), ERROR);
-    testDistribution(b);
+    assertEquals(0.4, b.getProb(1), ERROR);
+    assertEquals(0.6, b.getProb(0), ERROR);
+    assertEquals(0.0, b.getProb(2), ERROR);
+    assertEquals(Math.log(0.4), b.getLogProb(1), ERROR);
+    assertEquals(Math.log(0.6), b.getLogProb(0), ERROR);
+    assertEquals(Double.NEGATIVE_INFINITY, b.getLogProb(2), ERROR);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testInsufficientArguments() {
     Bernoulli b = new Bernoulli();
     b.setParams(new Object[] { null });
-    testDistribution(b);
+    b.sampleVal();
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testExtraneousArgs() {
     Bernoulli b = new Bernoulli();
     b.setParams(new Object[] { 1.0, 0.5 });
-    testDistribution(b);
+    b.sampleVal();
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testIncorrectArguments() {
     Bernoulli b = new Bernoulli();
     b.setParams(new Object[] { 1.1 });
-    testDistribution(b);
+    b.sampleVal();
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testIncorrectArguments2() {
     Bernoulli b = new Bernoulli();
     b.setParams(new Object[] { -0.1 });
-    testDistribution(b);
+    b.sampleVal();
   }
 
   @Test
@@ -61,18 +65,11 @@ public class TestBernoulli implements TestDistributions {
     Bernoulli b = new Bernoulli();
     b.setParams(new Object[] { null });
     b.setParams(new Object[] { 0.5 });
-    assertEquals(0.5, b.getP(), ERROR);
+    assertEquals(0.5, b.getProb(1), ERROR);
+    assertEquals(0.5, b.getProb(0), ERROR);
     b.setParams(new Object[] { 0.7 });
-    assertEquals(0.7, b.getP(), ERROR);
-    testDistribution(b);
-  }
-
-  public void testDistribution(Bernoulli bernoulli) {
-    double p = bernoulli.getP();
-    assertEquals(p, bernoulli.getProb(1), ERROR);
-    assertEquals(Math.log(p), bernoulli.getLogProb(1), ERROR);
-    assertEquals(1 - p, bernoulli.getProb(0), ERROR);
-    assertEquals(Math.log(1 - p), bernoulli.getLogProb(0), ERROR);
+    assertEquals(0.7, b.getProb(1), ERROR);
+    assertEquals(0.3, b.getProb(0), ERROR);
   }
 
 }
