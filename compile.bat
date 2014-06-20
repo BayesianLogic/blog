72336332447a76203375a8f53439b7ed378ed697
tejasvsrinivasan <tejasvsrinivasan@gmail.com>
2014-06-20 01:40:39 -0700

    Using ComplexSymbolFactory for location of errors

--- compile.bat
+++ compile.bat
@@ -2,6 +2,6 @@ mkdir bin
 
 dir /s /B src\*.java > javafiles
 
-javac -verbose -cp lib\gson-2.2.4.jar;lib\java-cup-11b.jar;lib\jflex-1.5.1.jar;lib\commons-math3-3.0.jar;lib\commons-math-2.2.jar;lib\junit-4.10.jar;lib\Jama.jar;bin\ -d bin\ @javafiles
+javac -cp lib\gson-2.2.4.jar;lib\java-cup-11b.jar;lib\jflex-1.5.1.jar;lib\commons-math3-3.0.jar;lib\commons-math-2.2.jar;lib\junit-4.10.jar;lib\Jama.jar;bin\ -d bin\ @javafiles
 
 del javafiles
