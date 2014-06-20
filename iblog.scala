b3081110db2c35edd8d3923aef99f1aaa8682a8d
Constantin Berzan <cberzan@gmail.com>
2014-06-20 16:35:53 -0700

    import handy stuff automatically when launching iblog

---
+++ iblog.scala
@@ -0,0 +1,7 @@
+// This code runs automatically when launching "iblog".
+
+import blog.debug.LWDebugger
+import blog.model.Evidence
+import blog.model.Model
+import blog.model.Queries
+import blog.world.PartialWorld
