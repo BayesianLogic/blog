c00b974ae2a70a7625dcf442e868508d6c10c931
Constantin Berzan <cberzan@gmail.com>
2014-06-19 18:01:40 -0700

    proof-of-concept debugging magic
    Defined a function "n" that takes no args. It computes the next sample,
    writes it to stdout, and saves it to a global variable for inspection.

--- tmp/script.scala
+++ tmp/script.scala
@@ -1,5 +1,6 @@
 import java.util.Properties
 
+import blog.common.Util
 import blog.engine.SamplingEngine
 import blog.model.Evidence
 import blog.model.Model
@@ -15,7 +16,26 @@ queries.addFromFile("tmp/burglary.q")
 model.compile()
 evidence.compile()
 queries.compile()
+Util.initRandom(false)
 
 var sampler = new LWSampler(model, new Properties())
 sampler.initialize(evidence, queries)
-var engine = new SamplingEngine(sampler)
+/*
+for (i <- 1 to 1000) {
+    var oneSample = sampler.sampleOne()
+    println(oneSample.world)
+    println(oneSample.logWeight)
+}
+*/
+// var samples = sampler.sample(1000)
+
+
+// Debugging magic:
+var sample: blog.sample.LWSample = null;
+
+// Compute next sample, print it out, and save it for inspection.
+// (Uses global vars: sample, sampler.)
+def n = {
+    sample = sampler.sampleOne()
+    println(s"logWeight: ${sample.logWeight}  world: ${sample.world}")
+}
