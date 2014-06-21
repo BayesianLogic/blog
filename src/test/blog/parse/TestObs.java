c21f43d340efc8126c5a8ccc1fa583c88119c952
lileicc <lileicc@gmail.com>
2014-06-21 15:34:36 -0700

    remove precedence of ID

--- src/test/blog/parse/TestObs.java
+++ src/test/blog/parse/TestObs.java
@@ -7,34 +7,27 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import blog.absyn.BooleanExpr;
-import blog.absyn.DistinctSymbolDec;
-import blog.absyn.DoubleExpr;
-import blog.absyn.FieldList;
-import blog.absyn.FixedFuncDec;
 import blog.absyn.FuncCallExpr;
-import blog.absyn.NameTy;
-import blog.absyn.OpExpr;
-import blog.absyn.Stmt;
-import blog.absyn.StringExpr;
-import blog.absyn.SymbolArray;
-import blog.absyn.SymbolArrayList;
-import blog.symbol.Symbol;
+import blog.absyn.SymbolExpr;
+import blog.absyn.ValueEvidence;
 
 /**
  * @author amatsukawa
  * @date Apr 19, 2012
+ * @author leili
+ * @date June 21, 2014
  */
-
-//TODO: These still need testing, but this helps me see the tree structure
 
 public class TestObs extends TestParse {
 
-	@Test
-	public void testSimpleDecl() {
-		String toParse = "obs ObsColor(Draw1) = Blue;";
-		String parsed = TestParse.parseToRepr(toParse);
-		System.out.println(parsed);
-	}
+  @Test
+  public void testSimpleDecl() {
+    String toParse = "obs ObsColor(Draw1) = Blue;";
+    String parsed = parseToRepr(toParse);
+    String generated = toRepr(Stmts(new ValueEvidence(0, new FuncCallExpr(0,
+        Symbol("ObsColor"), Exprs(new SymbolExpr(Symbol("Draw1")))),
+        Expr(Symbol("Blue")))));
+    assertEquals(parsed, generated);
+  }
 
 }
