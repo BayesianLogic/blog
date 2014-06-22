e2f124d85f022f0d062053c136c2e5ad71b032a5
lileicc <lileicc@gmail.com>
2014-06-21 18:00:05 -0700

    fix the conflicts

--- src/blog/absyn/DistributionDec.java
+++ src/blog/absyn/DistributionDec.java
@@ -8,30 +8,30 @@ import blog.symbol.Symbol;
  * 
  */
 public class DistributionDec extends Dec {
-	public Symbol name;
-	public ClassName classname;
-	public ExprList params;
+  public Symbol name;
+  public Symbol classname;
+  public ExprList params;
 
-	public DistributionDec(int p, Symbol n, ClassName cn, ExprList a) {
-		this(0, p, n, cn, a);
-	}
+  public DistributionDec(int p, Symbol n, Symbol cn, ExprList a) {
+    this(0, p, n, cn, a);
+  }
 
-	public DistributionDec(int line, int col, Symbol n, ClassName cn, ExprList a) {
-		super(line, col);
-		name = n;
-		classname = cn;
-		params = a;
-	}
+  public DistributionDec(int line, int col, Symbol n, Symbol cn, ExprList a) {
+    super(line, col);
+    name = n;
+    classname = cn;
+    params = a;
+  }
 
-	@Override
-	public void printTree(Printer pr, int d) {
-		pr.indent(d);
-		pr.sayln("DistributionDec(");
-		pr.indent(d + 1);
-		pr.say(name.toString());
-		pr.sayln(",");
-		classname.printTree(pr, d + 1);
-		pr.sayln(",");
-		params.printTree(pr, d + 1);
-	}
+  @Override
+  public void printTree(Printer pr, int d) {
+    pr.indent(d);
+    pr.sayln("DistributionDec(");
+    pr.indent(d + 1);
+    pr.say(name.toString());
+    pr.sayln(",");
+    pr.say(classname.toString());
+    pr.sayln(",");
+    params.printTree(pr, d + 1);
+  }
 }
