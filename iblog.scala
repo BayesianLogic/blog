45bb1a2b827ac3b130462da5f7a2d88218025bc6
Constantin Berzan <cberzan@gmail.com>
2014-06-23 16:11:08 -0700

    first pass at MHDebugger

--- iblog.scala
+++ iblog.scala
@@ -1,6 +1,7 @@
 // This code runs automatically when launching "iblog".
 
 import blog.debug.LWDebugger
+import blog.debug.MHDebugger
 import blog.model.Evidence
 import blog.model.Model
 import blog.model.Queries
