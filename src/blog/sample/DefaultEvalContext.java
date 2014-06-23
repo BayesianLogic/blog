713b5668a485ca2ab66976730ce53f63fb8bd519
Constantin Berzan <cberzan@gmail.com>
2014-06-23 16:27:37 -0700

    bugfix: keep behavior constant regardless of verbose setting

--- src/blog/sample/DefaultEvalContext.java
+++ src/blog/sample/DefaultEvalContext.java
@@ -63,183 +63,185 @@ import blog.world.PartialWorld;
  * <code>errorIfUndet</code> flag set to true.
  */
 public class DefaultEvalContext implements EvalContext {
-	/**
-	 * Creates a new evaluation context using the given partial world. This
-	 * DefaultEvalContext instance will return null from access methods if the
-	 * world is not complete enough to determine the correct value.
-	 */
-	public DefaultEvalContext(PartialWorld world) {
-		this.world = world;
-	}
+  /**
+   * Creates a new evaluation context using the given partial world. This
+   * DefaultEvalContext instance will return null from access methods if the
+   * world is not complete enough to determine the correct value.
+   */
+  public DefaultEvalContext(PartialWorld world) {
+    this.world = world;
+  }
 
-	/**
-	 * Creates a new evaluation context using the given partial world. If the
-	 * <code>errorIfUndet</code> flag is true, the access methods on this instance
-	 * will print error messages and exit the program if the world is not complete
-	 * enough to determine the correct return value.
-	 */
-	public DefaultEvalContext(PartialWorld world, boolean errorIfUndet) {
-		this.world = world;
-		this.errorIfUndet = errorIfUndet;
-	}
+  /**
+   * Creates a new evaluation context using the given partial world. If the
+   * <code>errorIfUndet</code> flag is true, the access methods on this instance
+   * will print error messages and exit the program if the world is not complete
+   * enough to determine the correct return value.
+   */
+  public DefaultEvalContext(PartialWorld world, boolean errorIfUndet) {
+    this.world = world;
+    this.errorIfUndet = errorIfUndet;
+  }
 
-	public Object getValue(BasicVar var) {
-		Object value = world.getValue(var);
-		if (value == null) {
-			handleMissingVar(var);
-		}
-		return value;
-	}
+  public Object getValue(BasicVar var) {
+    Object value = world.getValue(var);
+    if (value == null) {
+      handleMissingVar(var);
+    }
+    return value;
+  }
 
-	public ObjectSet getSatisfiers(NumberVar popApp) {
-		if (getValue(popApp) != null) { // note that we use our getValue method
-			return world.getSatisfiers(popApp);
-		}
+  public ObjectSet getSatisfiers(NumberVar popApp) {
+    if (getValue(popApp) != null) { // note that we use our getValue method
+      return world.getSatisfiers(popApp);
+    }
 
-		handleMissingVar(popApp);
-		return null;
-	}
+    handleMissingVar(popApp);
+    return null;
+  }
 
-	public NumberVar getPOPAppSatisfied(Object obj) {
-		// might want to report invalid objects
-		return world.getPOPAppSatisfied(obj);
-	}
+  public NumberVar getPOPAppSatisfied(Object obj) {
+    // might want to report invalid objects
+    return world.getPOPAppSatisfied(obj);
+  }
 
-	public boolean usesIdentifiers(Type type) {
-		return world.getIdTypes().contains(type);
-	}
+  public boolean usesIdentifiers(Type type) {
+    return world.getIdTypes().contains(type);
+  }
 
-	public Boolean objectExists(Object obj) {
-		if (obj instanceof NonGuaranteedObject) {
-			NonGuaranteedObject ngObj = (NonGuaranteedObject) obj;
-			NumberVar nv = ngObj.getNumberVar();
-			Integer varValue = (Integer) getValue(nv);
-			if (varValue == null) {
-				return null;
-			}
-			return Boolean.valueOf(ngObj.getNumber() <= varValue.intValue());
-		}
+  public Boolean objectExists(Object obj) {
+    if (obj instanceof NonGuaranteedObject) {
+      NonGuaranteedObject ngObj = (NonGuaranteedObject) obj;
+      NumberVar nv = ngObj.getNumberVar();
+      Integer varValue = (Integer) getValue(nv);
+      if (varValue == null) {
+        return null;
+      }
+      return Boolean.valueOf(ngObj.getNumber() <= varValue.intValue());
+    }
 
-		return Boolean.TRUE;
-	}
+    return Boolean.TRUE;
+  }
 
-	public Object getLogicalVarValue(LogicalVar var) {
-		if (assignment == null) {
-			return null;
-		}
-		return assignment.get(var);
-	}
+  public Object getLogicalVarValue(LogicalVar var) {
+    if (assignment == null) {
+      return null;
+    }
+    return assignment.get(var);
+  }
 
-	public Set getLogicalVarValues() {
-		if (assignment == null) {
-			return Collections.EMPTY_SET;
-		}
-		return assignment.valueSet();
-	}
+  public Set getLogicalVarValues() {
+    if (assignment == null) {
+      return Collections.EMPTY_SET;
+    }
+    return assignment.valueSet();
+  }
 
-	public String getAssignmentStr() {
-		if (assignment == null) {
-			return Collections.EMPTY_MAP.toString();
-		}
-		return assignment.toString();
-	}
+  public String getAssignmentStr() {
+    if (assignment == null) {
+      return Collections.EMPTY_MAP.toString();
+    }
+    return assignment.toString();
+  }
 
-	public void assign(LogicalVar var, Object value) {
-		if (assignment == null) {
-			assignment = new HashMapWithPreimages();
-		}
-		assignment.put(var, value);
-	}
+  public void assign(LogicalVar var, Object value) {
+    if (assignment == null) {
+      assignment = new HashMapWithPreimages();
+    }
+    assignment.put(var, value);
+  }
 
-	public void assignTuple(LogicalVar[] vars, Object[] values) {
-		if (assignment == null) {
-			assignment = new HashMapWithPreimages();
-		}
-		for (int i = 0; i < vars.length; ++i) {
-			assignment.put(vars[i], values[i]);
-		}
-	}
+  public void assignTuple(LogicalVar[] vars, Object[] values) {
+    if (assignment == null) {
+      assignment = new HashMapWithPreimages();
+    }
+    for (int i = 0; i < vars.length; ++i) {
+      assignment.put(vars[i], values[i]);
+    }
+  }
 
-	public void unassign(LogicalVar var) {
-		if (assignment != null) {
-			assignment.remove(var);
-		}
-	}
+  public void unassign(LogicalVar var) {
+    if (assignment != null) {
+      assignment.remove(var);
+    }
+  }
 
-	public void unassignTuple(LogicalVar[] vars) {
-		if (assignment != null) {
-			for (int i = 0; i < vars.length; ++i) {
-				assignment.remove(vars[i]);
-			}
-		}
-	}
+  public void unassignTuple(LogicalVar[] vars) {
+    if (assignment != null) {
+      for (int i = 0; i < vars.length; ++i) {
+        assignment.remove(vars[i]);
+      }
+    }
+  }
 
-	/**
-	 * Pushes the given object onto the stack of objects being evaluated. This
-	 * stack is maintained purely so that the EvalContext can print more
-	 * informative error messages.
-	 * 
-	 */
-	public void pushEvaluee(Object evaluee) {
-		if (evaluees == null) {
-			evaluees = new LinkedList();
-		}
-		evaluees.addLast(evaluee);
-	}
+  /**
+   * Pushes the given object onto the stack of objects being evaluated. This
+   * stack is maintained purely so that the EvalContext can print more
+   * informative error messages.
+   * 
+   */
+  public void pushEvaluee(Object evaluee) {
+    if (evaluees == null) {
+      evaluees = new LinkedList();
+    }
+    evaluees.addLast(evaluee);
+  }
 
-	/**
-	 * Pops the top object from the stack of objects being evaluated.
-	 * 
-	 * @throws IllegalStateException
-	 *           if the stack of objects being evaluated is empty
-	 */
-	public void popEvaluee() {
-		if ((evaluees == null) || evaluees.isEmpty()) {
-			throw new IllegalStateException("Evaluee stack is empty.");
-		}
-		evaluees.removeLast();
-	}
+  /**
+   * Pops the top object from the stack of objects being evaluated.
+   * 
+   * @throws IllegalStateException
+   *           if the stack of objects being evaluated is empty
+   */
+  public void popEvaluee() {
+    if ((evaluees == null) || evaluees.isEmpty()) {
+      throw new IllegalStateException("Evaluee stack is empty.");
+    }
+    evaluees.removeLast();
+  }
 
-	/**
-	 * Prints the sequence of objects being evaluated, in the order they were
-	 * added to the stack, to the given stream.
-	 */
-	public void printEvalTrace(PrintStream s) {
-		if (evaluees != null) {
-			for (Iterator iter = evaluees.iterator(); iter.hasNext();) {
-				s.println(iter.next());
-			}
-		}
-	}
+  /**
+   * Prints the sequence of objects being evaluated, in the order they were
+   * added to the stack, to the given stream.
+   */
+  public void printEvalTrace(PrintStream s) {
+    if (evaluees != null) {
+      for (Iterator iter = evaluees.iterator(); iter.hasNext();) {
+        s.println(iter.next());
+      }
+    }
+  }
 
-	/**
-	 * Handle the situation where an access method needs the value of the given
-	 * variable, but it is not instantiated. This method either prints an error
-	 * message and exits the program, or does nothing, depending on the value of
-	 * the <code>errorIfUndet</code> field.
-	 */
-	protected void handleMissingVar(BayesNetVar var) {
-		if (errorIfUndet & Util.verbose()) {
-			System.err.println("Error with evaluation trace:");
-			printEvalTrace(System.err);
-			Util.fatalError("Variable " + var + " is not instantiated.");
-		}
-	}
+  /**
+   * Handle the situation where an access method needs the value of the given
+   * variable, but it is not instantiated. This method either prints an error
+   * message and exits the program, or does nothing, depending on the value of
+   * the <code>errorIfUndet</code> field.
+   */
+  protected void handleMissingVar(BayesNetVar var) {
+    if (errorIfUndet) {
+      if (Util.verbose()) {
+        System.err.println("Error with evaluation trace:");
+        printEvalTrace(System.err);
+      }
+      Util.fatalError("Variable " + var + " is not instantiated.");
+    }
+  }
 
-	public/* protected -- for debugging */PartialWorld world;
+  public/* protected -- for debugging */PartialWorld world;
 
-	protected boolean errorIfUndet = false;
+  protected boolean errorIfUndet = false;
 
-	private MapWithPreimages assignment;
-	private LinkedList evaluees;
+  private MapWithPreimages assignment;
+  private LinkedList evaluees;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see blog.sample.EvalContext#getPartialWorld()
-	 */
-	@Override
-	public PartialWorld getPartialWorld() {
-		return world;
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see blog.sample.EvalContext#getPartialWorld()
+   */
+  @Override
+  public PartialWorld getPartialWorld() {
+    return world;
+  }
 }
