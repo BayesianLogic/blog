880b21059b059f6559734efa20cae756ce8c317f
Constantin Berzan <cberzan@gmail.com>
2014-06-25 18:25:01 -0700

    cleanup

--- debug-demo-lw.scala
+++
@@ -1,6 +0,0 @@
-val d = LWDebugger.make("example/burglary.blog")
-import d._
-n
-n
-n
-s.eval("Burglary | Alarm")
--- debug-demo-mh.scala
+++
@@ -1,6 +0,0 @@
-val d = MHDebugger.make("example/aircraft-static.blog")
-import d._
-n
-n
-n
-s.dump
