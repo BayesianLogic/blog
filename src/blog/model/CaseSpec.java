d6ab5227fc88cc0dc7fc788b508e4623a11c190d
jxwuyi <jxwuyi@gmail.com>
2014-06-26 14:48:14 +0800

    add CaseSpec

---
+++ src/blog/model/CaseSpec.java
@@ -0,0 +1,119 @@
+/**
+ * 
+ */
+package blog.model;
+
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+import blog.common.UnaryProcedure;
+import blog.sample.EvalContext;
+
+/**
+ * @author Yi Wu
+ * @since 2014/6/26/
+ * 
+ */
+public class CaseSpec extends ArgSpec {
+  ArgSpec test;
+  MapSpec clause;
+  boolean compiled = false;
+
+  public CaseSpec(ArgSpec test, MapSpec clause) {
+    this.test = test;
+    this.clause = clause;
+    compiled = false;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see blog.model.ArgSpec#evaluate(blog.sample.EvalContext)
+   */
+  @Override
+  public Object evaluate(EvalContext context) {
+    Object t = test.evaluate(context);
+    Map<Object, Object> mp = clause.evaluate(context);
+    if (mp.containsKey(t)) {
+      return mp.get(t);
+    } else
+      return Model.NULL;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see blog.model.ArgSpec#containsRandomSymbol()
+   */
+  @Override
+  public boolean containsRandomSymbol() {
+    return test.containsRandomSymbol() || clause.containsRandomSymbol();
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see blog.model.ArgSpec#checkTypesAndScope(blog.model.Model, java.util.Map)
+   */
+  @Override
+  public boolean checkTypesAndScope(Model model, Map scope) {
+    return test.checkTypesAndScope(model, scope)
+        && clause.checkTypesAndScope(model, scope);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see blog.model.ArgSpec#find(blog.model.Term)
+   */
+  @Override
+  public ArgSpec find(Term t) {
+    if ((test instanceof Term) && test.equals(t))
+      return test;
+    ArgSpec ret = test.find(t);
+    if (ret != null)
+      return ret;
+    return clause.find(t);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see blog.model.ArgSpec#applyToTerms(blog.common.UnaryProcedure)
+   */
+  @Override
+  public void applyToTerms(UnaryProcedure procedure) {
+    test.applyToTerms(procedure);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see blog.model.ArgSpec#replace(blog.model.Term, blog.model.ArgSpec)
+   */
+  @Override
+  public ArgSpec replace(Term t, ArgSpec another) {
+    ArgSpec newTest = test.replace(t, another);
+    MapSpec newClause = (MapSpec) clause.replace(t, another);
+    if (newTest == test && newClause == clause)
+      return this;
+    CaseSpec newCase = new CaseSpec(newTest, newClause);
+    if (compiled)
+      newCase.compile(new LinkedHashSet());
+    return newCase;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see blog.model.ArgSpec#getSubstResult(blog.model.Substitution,
+   * java.util.Set)
+   */
+  @Override
+  public ArgSpec getSubstResult(Substitution subst, Set<LogicalVar> boundVars) {
+    return new CaseSpec(test.getSubstResult(subst, boundVars),
+        (MapSpec) clause.getSubstResult(subst, boundVars));
+  }
+
+}
