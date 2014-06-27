3948acbf5a6045f4b3d1e904077696c0672aacec
Constantin Berzan <cberzan@gmail.com>
2014-06-27 14:58:23 -0700

    docs for scala PF code

--- src/blog/debug/FilterFeeder.scala
+++ src/blog/debug/FilterFeeder.scala
@@ -4,6 +4,16 @@ import blog.model.Evidence
 import blog.model.Queries
 
 /**
+ * Feeds evidence and queries to a ParticleFilter.
+ *
+ * If you have all the evidence and queries ahead of time, use
+ * OfflineFilterFeeder. If your filter is online, subclass FilterFeeder
+ * to provide the new evidence and queries as time progresses.
+ *
+ * Each item in the iterator is a (timestep, evidence, queries) triple.
+ * The timesteps have to be monotonically increasing. The evidence or
+ * queries may be null.
+ *
  * @author cberzan
  * @since Jun 23, 2014
  */
