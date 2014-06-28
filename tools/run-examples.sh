7dec5805a911576151f2572ae5932b79a2239532
Constantin Berzan <cberzan@gmail.com>
2014-06-27 17:23:20 -0700

    consolidate testing script and remove output from repo

--- tools/run-examples.sh
+++ tools/run-examples.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Runs all examples and produces the following files:
+#
+# status.txt
+# - A CSV of all Blog Examples alongside their status
+# 
+# successExamples.txt
+# - A list of all BLOG examples that run successfully
+# 
+# errorExamples.txt
+# - A list of all BLOG examples that throw exceptions
+# 
+# errors/*
+# - A list of files that threw a Java Exception (non-empty Standard Error)
+# - Each file as its content contains the Stack Trace
+
 # A list of all BLOG examples that throw exceptions
 errorFiles='testing/errorExamples.txt'
 echo "Error Examples" > $errorFiles
@@ -18,7 +33,7 @@ echo "FileName,Status" > $statusFiles
 
 for f in $(find ../example -name '*.blog'); do
     echo "Running $f"
-    ../dblog -n 100 $f 2> testing/errors > testing/output
+    ../blog -n 100 $f 2> testing/errors > testing/output
     errors=`cat testing/errors | wc -l`
     if [ "$errors" == "0" ]; then
         echo "$f,Pass" >> $statusFiles
@@ -33,3 +48,19 @@ for f in $(find ../example -name '*.blog'); do
 done
 rm testing/errors testing/output 
 
+for f in $(find ../example -name '*.dblog'); do
+    echo "Running $f"
+    ../dblog -n 100 $f 2> testing/errors > testing/output
+    errors=`cat testing/errors | wc -l`
+    if [ "$errors" == "0" ]; then
+        echo "$f,Pass" >> $statusFiles
+        echo "$f" >> $successFiles
+    else
+        echo "$f,Fail" >> $statusFiles
+        echo "$f" >> $errorFiles
+        echo "$f" >> $fileErrors
+        echo $(cat testing/errors) >> $fileErrors
+        echo "" >> $fileErrors
+    fi
+done
+rm testing/errors testing/output 
