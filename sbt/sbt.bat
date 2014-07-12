902cbe6b5c567ee851736e992e5a5223d188d08a
td11 <qltedxtc@sina.com>
2014-07-13 02:37:43 +0800

    Remove the heap memory settings

--- sbt/sbt.bat
+++ sbt/sbt.bat
@@ -1,2 +1,2 @@
 set SCRIPT_DIR=%~dp0
-java -Xms512M -Xmx1024M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M -jar "%SCRIPT_DIR%sbt-launch.jar" %*
+java -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M -jar "%SCRIPT_DIR%sbt-launch.jar" %*
