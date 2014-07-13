3862d6e04f71e6a10fb171c358ce8f685b30a487
jxwuyi <jxwuyi@gmail.com>
2014-07-12 18:03:41 -0700

    update

--- iblog.bat
+++ iblog.bat
@@ -7,6 +7,6 @@ if exist "%BLOG_HOME%\bin\iblog.scala" (
   set CPATH="%BLOG_HOME%\lib\java-cup-11b.jar;%BLOG_HOME%\lib\*"
   scala -cp %CPATH% -i "%BLOG_HOME%\bin\iblog.scala" -J%JAVA_OPTS% -J%*
 ) else (
-	set CPATH="%BLOG_HOME%\lib\java-cup-11b.jar":"%BLOG_HOME%\lib\*"
-  scala -cp %CPATH% -i "%BLOG_HOME%\src\main\scala\iblog.scala" -J%JAVA_OPTS% -J%*  
+	set CPATH="%BLOG_HOME%\blog\lib\java-cup-11b.jar;%BLOG_HOME%\blog\lib\*"
+  scala -cp %CPATH% -i "%BLOG_HOME%\blog\src\main\scala\iblog.scala" -J%JAVA_OPTS% -J%*  
 )
