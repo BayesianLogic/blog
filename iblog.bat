b5adf9be470afcc647cd8bfd83dafe5508608dba
jxwuyi <jxwuyi@gmail.com>
2014-07-13 16:40:43 -0700

    update iblog.bat for windows

--- iblog.bat
+++ iblog.bat
@@ -1,5 +1,4 @@
 @echo off
-set JAVA_OPTS=
 if "%1"=="" set JAVA_OPTS=-Xmx4096M
 set BLOG_HOME=%~dp0..
 echo Starting Interative Shell for BLOG
@@ -7,6 +6,8 @@ if exist "%BLOG_HOME%\bin\iblog.scala" (
   set CPATH="%BLOG_HOME%\lib\java-cup-11b.jar;%BLOG_HOME%\lib\*"
   scala -cp %CPATH% -i "%BLOG_HOME%\bin\iblog.scala" -J%JAVA_OPTS% %*
 ) else (
-	set CPATH="%BLOG_HOME%\lib\java-cup-11b.jar;%BLOG_HOME%\lib\*"
-  scala -cp %CPATH% -i "%BLOG_HOME%\src\main\scala\iblog.scala" -J%JAVA_OPTS% %*  
+  set BLOG_DEVELOP_HOME=%~dp0
+	if NOT exist "%BLOG_DEVELOP_HOME%target\universal\stage\" (sbt\sbt.bat stage)
+  set CPATH="%BLOG_DEVELOP_HOME%target\universal\stage\lib\java-cup-11b.jar;%BLOG_DEVELOP_HOME%target\universal\stage\lib\*"
+  scala -cp %CPATH% -i "%BLOG_DEVELOP_HOME%src\main\scala\iblog.scala" -J%JAVA_OPTS% %*
 )
