16ab5b925979c46dde55c361c732b463e9e0f51f
lileicc <lileicc@gmail.com>
2014-10-06 15:22:41 -0700

    fix typo in import

--- src/test/java/test/blog/distrib/TestCategorical.java
+++ src/test/java/test/blog/distrib/TestCategorical.java
@@ -6,12 +6,12 @@ package test.blog.distrib;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 
 import org.junit.Test;
 
-import scala.actors.threadpool.Arrays;
 import blog.distrib.Categorical;
 
 /**
