a088def7f47a67b8fee8053dbc62428ba094eea4
zth667 <946966490@qq.com>
2017-12-01 16:28:47 -0800

    stable web

--- web/app.py
+++ web/app.py
@@ -11,12 +11,12 @@ import time
 import os
 import subprocess
 import re
-
+from operator import itemgetter
 BLOG_EXTENSION = ".blog"
 USER_STORE = "static/user_query/"
 DEFAULT_GRAPH = "static/images/BerkeleyLogo.png"
 EXAMPLE_BLOG_PATH = 'example.blog'
-
+EXAMPLES_PATH = 'static/example/'
 urls = ('/', 'blog_web_ui')
 render = web.template.render('templates/')
 
@@ -25,8 +25,9 @@ app = web.application(urls, globals())
 with open(EXAMPLE_BLOG_PATH) as f:
     example_blog_code = f.read()
 
-def run_process(script_name):
-    command = ["../blog", "--displaycbn", script_name]
+def run_process(script_name,base,eng,alg):
+    command = ["../blog", script_name,base,eng,alg]#"--displaycbn"
+    #print command
     p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output = os.linesep.join(p.communicate())
     return output
@@ -47,7 +48,7 @@ def store_script(prefix, script):
     input_handler.close()
     return script_name
 
-def execute_script(script):
+def execute_script(script,base,eng,alg):
     #Define name of the script
     current = str(time.time())
     filename = "tmp_%s" % current
@@ -55,22 +56,58 @@ def execute_script(script):
     prefix = USER_STORE + filename
 
     script_name = store_script(prefix, script)
-    return run_process(script_name)
+    return run_process(script_name,base,eng,alg)
 
 class blog_web_ui:
     def GET(self):
         return render.index(example_blog_code)
 
     def POST(self):
+        if hasattr(web.input(),'sel_file'):
+            newpath = EXAMPLES_PATH+web.input().sel_file
+            #print newpath
+            with open(newpath) as f:
+                example_blog_code = f.read()
+            return example_blog_code
         s = web.input().textfield
-        raw_data = execute_script(s)
+        base = '-n '+web.input().base
+        eng = '-e blog.engine.'+web.input().eng
+        alg = '-s blog.sample.'+web.input().alg
+        raw_data = execute_script(s,base,eng,alg)
         parsed_results = parse_query_results(raw_data)
-        return render.data(raw_data, parsed_results)
+        if not parsed_results:
+            print "error occurred"
+            return "error occurred"
+        #return render.data(raw_data, parsed_results)
+        return [parsed_results,raw_data]
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+    return False
+
+def kdeit(dist):
+    import numpy as np
+    from pyqt_fit import kde
+    #print dist
+    samples,weights = zip(*dist)
+    samples = list(samples)
+    weights = list(weights)
+    est = kde.KDE1D(samples,weights = weights)
+    xmin = min(samples)
+    xmax = max(samples)
+    x = np.linspace(xmin,xmax, 200)
+    y = est(x)
+    return [list(i) for i in zip(x,y)]
 
 def parse_query_results(s):
     results = []
-    regex = re.compile(r'Query Results\s*=*\n(([^=].*\n)*)\s*=*\s*Done\s*=*')
+    #print s
+    regex = re.compile(r'Query Results\s*=*\n(([^=].*\n)*)\s*=*')
     for match in regex.finditer(s):
+        #print match.groups()
         lines = match.groups()[0].split('\n')
         match = re.match('Number of samples: (\d+)', lines[0])
         if match:
@@ -79,21 +116,48 @@ def parse_query_results(s):
                 'samples': samples,
                 'queries': []
             })
+            #print results
+        dist = []
+        checkden = False
         for line in lines[1:]:
             query_match = re.match('Distribution of values for (.*)', line)
             result_match = re.match(r'\s*(\S*)\s+([0-9]*\.?[0-9]+[eE]?[-+]?[0-9]*)', line)
             if query_match:
                 query = query_match.group(1)
+                if(len(dist)>0):
+                    dist=sorted(dist, key=itemgetter(0))
+                    if checkden:
+                        checkden = False
+                        dist = kdeit(dist)
+                    results[-1]['queries'][-1]['distribution'].extend(dist)
+                    dist=[]
                 results[-1]['queries'].append({
-                    'query': query,
-                    'distribution': []
+                    'query': 'Distribution of values for '+query,
+                    'distribution': [['value','probability']]
                 })
+                checkden = False
             elif result_match:
                 value, probability = result_match.groups()
-                results[-1]['queries'][-1]['distribution'].append({
-                    'value': value,
-                    'probability': 100 * float(probability)
-                })
+                if is_number(value):
+                    if(value.count('.')==1):
+                        checkden = True
+                    dist.append([
+                        float(value),
+                        100 * float(probability)
+                    ])
+                else:
+                    dist.append([
+                        value,
+                        100 * float(probability)
+                    ])
+        if(len(dist)>0):
+            dist=sorted(dist, key=itemgetter(0))
+            if checkden:
+                checkden = False
+                dist = kdeit(dist)
+            results[-1]['queries'][-1]['distribution'].extend(dist)
+            dist=[]
+    #print results
     return results
 
 
--- web/static/blog/blog.js
+++ web/static/blog/blog.js
@@ -1,4 +1,8 @@
 $(function() {
+  //document.getElementById('viewres').style.visibility='hidden';
+document.getElementById('viewres').style.display='none';
+document.getElementById('viewlog').style.display='none';
+document.getElementById('computing').style.display='none';
   CodeMirror.defineMode("diff", function() {
     var keywords = wordRegexp(['extern','import','fixed','distinct','random','origin','param','type', 'obs', 'query', 'for', 'forall','exists', 'if', 'then', 'else', 'null']);
     var types = wordRegexp(['Integer', 'Real', 'Boolean', 'NaturalNum', 'String', 'List', 'Map', 'RealMatrix', 'IntMatrix', 'TabularCPD','Categorical']);
@@ -21,20 +25,202 @@ $(function() {
 
   CodeMirror.defineMIME("text/x-diff", "diff");
 
-  var editor = CodeMirror.fromTextArea($('#textfield')[0], {
+ editor = CodeMirror.fromTextArea($('#textfield')[0], {
+
     lineNumbers: true,
     matchBrackets: true
   });
 
+ num=0;
+
+//editor.setOption("theme", "railscasts");
   $(".button").click(function() {
+    document.getElementById('viewres').style.display='none';
+      document.getElementById('viewlog').style.display='none';
+    document.getElementById('computing').style.display='initial';
     var input_string = editor.getValue();
+    var samp_base = $('input[name=samp_base]:checked').val();
+    var samp_eng = $('input[name=samp_eng]:checked').val();
+    var samp_alg = $('input[name=samp_alg]:checked').val();
+    var input_data = {"textfield" : input_string, "base" : samp_base, "eng" : samp_eng, "alg" : samp_alg};
+    re = /@/i;
+    var found = input_string.match(re);
+    if(found==null&&samp_eng=="ParticleFilter"){
+      alert(" ParticleFilter can only be used to dynamic models!");
+      return false;
+    }
+    /*
+    var i;
+    for (i=1;i<=num;i++ ){
+      var chartname='#chardiv'+i.toString();
+      $("#chartdiv"+i.toString()).css({"height"		: "0px"});
+      //console.log(AmCharts.charts);
+      for(key in AmCharts.charts)
+        AmCharts.charts[key].clear();
+    }*/
+    num=0;
+    glb_title = [];
+    glb_dist = [];
+    glb_samples = [];
     $.ajax({
       type: "POST",
-      data: {textfield : input_string},
+      data: input_data,
       success: function(data) {
-        $('#results').html($(data));
+        //console.log(data);
+        //console.log(eval(data));
+        //console.log(eval(data));
+        if (data=="error occurred"){
+          alert(data);
+          return false;
+        }
+        var real_data= eval(data);
+        $('#logtext').val(real_data[1]);
+        real_data=real_data[0];
+        glb_title = [];
+        glb_dist = [];
+        glb_samples = [];
+        num=0;
+        $("#choosedata").empty();
+        //console.log(data);
+        for (key1 in real_data)
+          if(real_data.hasOwnProperty(key1)){
+          var samples = real_data[key1]['samples']
+          var queries = real_data[key1]['queries']
+          for(key in queries){
+            if(queries.hasOwnProperty(key)){
+              num+=1;
+              var title = queries[key]['query'];
+              var dist = queries[key]['distribution'];
+              glb_dist.push(dist);
+              glb_title.push(title);
+              glb_samples.push(samples);
+              $("#choosedata").append($("<option />").val(num-1).text(title));
+            }
+          }
+        }
+        google.visualization.events.addOneTimeListener(ChartC, 'ready',function(){
+          document.getElementById('viewres').style.display='initial';
+            document.getElementById('viewlog').style.display='initial';
+          document.getElementById('computing').style.display='none';}
+              );
+        showchart(glb_samples[0],glb_title[0], glb_dist[0]);
+        //$('#results').html($(data));
       },
     });
+
     return false;
   });
 });
+google.charts.load('44', {'packages':['corechart']});
+google.charts.setOnLoadCallback(showchart);
+
+var days;
+var report='activity';
+movie_flag=false;
+num_flag=0;
+function getChart() {
+  return google.visualization.ColumnChart;
+}
+function isNumber (value) {
+return typeof value === 'number' && isFinite(value);
+};
+function drawChart(Chart, jsonData, title, samples) {
+  if (jsonData==null)
+    jsonData=[];
+  //  console.log(jsonData[1][0]);
+  var dataTable = new google.visualization.arrayToDataTable(jsonData,false);
+
+  var e = document.getElementById("DelayDiv");
+  var delay = 1000;
+  var total = 0;
+  if(jsonData.length>1&&isNumber(jsonData[1][0])){
+    Chart.setChartType('LineChart');
+  }else
+  Chart.setChartType('ColumnChart');
+  Chart.setDataTable(dataTable);
+  Chart.setOptions({
+      title: title+" w/ "+samples+" samples",
+      titleFontSize:15,
+      vAxis: {minValue:0, maxValue:100},
+        height: 400,
+        width: 600,
+      animation: {
+        duration: delay,
+        easing: 'linear',
+        startup: true
+      },
+      legend: {position: 'none'}
+  });
+  if (movie_flag){
+    movie_flag=false;
+    //console.log('got here'+num_flag.toString());
+    google.visualization.events.addOneTimeListener(Chart.getChart(), 'animationfinish',function(){movie(num_flag);});}
+  Chart.draw();
+
+}
+function showhide() {
+    var x = document.getElementById("usrtext");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function getval(selbox){
+  $.ajax({
+    type: "POST",
+    data: {sel_file : selbox.value},
+    success: function(data) {
+      editor.setValue(data);
+      //console.log(eval(data));
+      //$('#results').html($(data));
+    },
+  });
+}
+function handlebase(myradio){
+//  $("#sampno").slider("destroy");
+  $("#sampno").prop({"max":parseInt(myradio.value)*2});
+  $("#sampno").val(parseInt(myradio.value));
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+}
+var ChartC;
+function showchart(samples,title, dist){
+
+if ( ChartC == null ) {
+			//gv = getChart();
+			ChartC = new google.visualization.ChartWrapper({
+     containerId: 'chartdiv'
+ });
+		}
+		drawChart(ChartC, dist, title,samples);
+
+}
+function refresh(){
+  var e = document.getElementById("choosedata");
+	var chtid = e.options[e.selectedIndex].value;
+  showchart(glb_samples[chtid],glb_title[chtid],glb_dist[chtid])
+}
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+function movie(i){
+    if (i==0) return;
+    console.log(num-i);
+    $("#choosedata").val(num-i);
+    movie_flag=true;
+    num_flag=i-1;
+    showchart(glb_samples[num-i],glb_title[num-i],glb_dist[num-i]);
+}
+var slider = document.getElementById("sampno");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
